0149a421cb17826d5ebced2e819c47db4a1b1417
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-15 17:32:57 +0545

    users page

--- apps/rahat-ui/src/app/user/page.tsx
+++ apps/rahat-ui/src/app/user/page.tsx
@@ -1,7 +1,48 @@
-export default function UserPage() {
+import ProjectNav from '../../components/projects/nav';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@rahat-ui/shadcn/components/resizable';
+import { Tabs } from '@rahat-ui/shadcn/components/tabs';
+import UsersTable from '../../components/users/usersTable';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@rahat-ui/shadcn/components/dailog';
+
+export default function UsersPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mt-8">Users</h1>
+    <div className="mb-5">
+      <Tabs defaultValue="grid">
+        <div className="flex items-center justify-between mb-9 mt-8">
+          <h1 className="text-3xl font-semibold">Users List</h1>
+        </div>
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="min-h-max border"
+        >
+          <ResizablePanel
+            minSize={20}
+            defaultSize={20}
+            maxSize={20}
+            className="h-full"
+          >
+            <ProjectNav />
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel>
+            <div className="p-4">
+              <UsersTable />
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </Tabs>
     </div>
   );
 }

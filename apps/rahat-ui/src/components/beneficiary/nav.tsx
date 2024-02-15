6e4146dc75665cdb27520df28cd041d7f583bd95
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-15 21:13:24 +0545

    beneficiary side nav updates

--- apps/rahat-ui/src/components/beneficiary/nav.tsx
+++ apps/rahat-ui/src/components/beneficiary/nav.tsx
@@ -5,35 +5,57 @@ import { ScrollArea } from '@rahat-ui/shadcn/components/scroll-area';
 export default function Nav() {
   return (
     <>
-      <ScrollArea>
-        <div className="p-4">
-          <h1 className="font-semibold text-xl text-slate-600 mb-4">
-            Navigation Items
-          </h1>
-          <nav>
-            <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
-              <div className="flex gap-3">
-                <Eye />
-                <p>Active beneficiaries</p>
+      <div>
+        <h1 className="p-4 font-semibold text-xl text-slate-600">
+          Navigation Items
+        </h1>
+        <ScrollArea className="h-44">
+          <div className="px-4 pb-4">
+            <nav>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <Eye />
+                  <p>Active beneficiaries</p>
+                </div>
+                <p>128</p>
               </div>
-              <p>128</p>
-            </div>
-            <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
-              <div className="flex gap-3">
-                <EyeOff />
-                <p>Inactive beneficiaries</p>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <EyeOff />
+                  <p>Inactive beneficiaries</p>
+                </div>
+                <p>32</p>
               </div>
-              <p>32</p>
-            </div>
-            <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
-              <div className="flex gap-3">
-                <ScreenShareOff /> <p>Disabled/ Deleted</p>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <ScreenShareOff /> <p>Disabled/ Deleted</p>
+                </div>
+                <p>9</p>
               </div>
-              <p>9</p>
-            </div>
-          </nav>
-        </div>
-      </ScrollArea>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <Eye />
+                  <p>Active beneficiaries</p>
+                </div>
+                <p>128</p>
+              </div>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <EyeOff />
+                  <p>Inactive beneficiaries</p>
+                </div>
+                <p>32</p>
+              </div>
+              <div className="flex justify-between p-4 rounded-md cursor-pointer hover:bg-primary hover:text-white">
+                <div className="flex gap-3">
+                  <ScreenShareOff /> <p>Disabled/ Deleted</p>
+                </div>
+                <p>9</p>
+              </div>
+            </nav>
+          </div>
+        </ScrollArea>
+      </div>
       <Separator />
       <ScrollArea>
         <div className="p-4">

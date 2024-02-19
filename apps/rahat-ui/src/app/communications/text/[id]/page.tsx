58aaa0a976f6e2fa82f62b77004f52c65f536391
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-19 13:59:56 +0545

    text detail page

---
+++ apps/rahat-ui/src/app/communications/text/[id]/page.tsx
@@ -0,0 +1,9 @@
+import TextDetailView from 'apps/rahat-ui/src/components/communications/text/textDetailView';
+
+export const metadata = {
+  title: 'Text: Detail',
+};
+
+export default function TextCampaignDetail() {
+  return <TextDetailView />;
+}

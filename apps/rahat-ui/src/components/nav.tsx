5ba0a9e3c61835f83202727275524869d65e128e
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-19 14:49:46 +0545

    dropdown menu on avatar

--- apps/rahat-ui/src/components/nav.tsx
+++ apps/rahat-ui/src/components/nav.tsx
@@ -13,6 +13,12 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from '@rahat-ui/shadcn/components/hover-card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuGroup,
+} from '@rahat-ui/shadcn/src/components/ui/dropdown-menu';
 import { ChevronDown } from 'lucide-react';
 
 import { useNavData } from '../app/config-nav';
@@ -70,16 +76,49 @@ export function Nav() {
           )}
         </nav>
       </div>
-      <div className="flex gap-8">
+      <div className="flex gap-8 items-center">
         <Input
           className="text-slate-500 font-medium w-96"
           type="text"
           placeholder="Search..."
         />
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage
+                src="https://github.com/shadcn.png"
+                alt="profile-icon"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-5" side="bottom">
+            <DropdownMenuGroup className="p-2 flex flex-col">
+              <div className="p-2 flex flex-col">
+                <span className="font-bold">John Doe</span>
+                <span>john.doe@rahat.io</span>
+              </div>
+              <Link
+                className="p-2 hover:bg-secondary"
+                href={paths.profile.root}
+              >
+                Profile
+              </Link>
+              <Link
+                className="p-2 hover:bg-secondary"
+                href={paths.dashboard.root}
+              >
+                Home
+              </Link>
+              <Link
+                className="text-red-500 p-2 hover:bg-secondary w-full"
+                href={paths.auth.login}
+              >
+                Logout
+              </Link>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );

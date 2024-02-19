d2811d45c21c002d02a32949ed2d9775947997c8
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-19 13:37:05 +0545

    log card

--- apps/rahat-ui/src/components/communications/voice/voiceDetailView.tsx
+++ apps/rahat-ui/src/components/communications/voice/voiceDetailView.tsx
@@ -3,8 +3,14 @@
 import VoiceDetailTable from './voiceDetailTable';
 import VoiceInfoCard from '../infoCard';
 import { Card, CardContent } from '@rahat-ui/shadcn/src/components/ui/card';
+import LogCard from '../logCard';
 
 export default function VoiceDetailView() {
+  const logCardData = [
+    { total: 4, title: 'Total IVR sent' },
+    { total: 1, title: 'Successfull IVR' },
+    { total: 3, title: 'Failed IVR' },
+  ];
   return (
     <>
       <div className="mt-8">
@@ -13,6 +19,11 @@ export default function VoiceDetailView() {
       <div className="mt-5">
         <Card>
           <CardContent>
+            <div className="flex justify-between gap-4 flex-wrap mt-8 mb-4">
+              {logCardData.map((item) => (
+                <LogCard title={item.title} total={item.total} />
+              ))}
+            </div>
             <VoiceDetailTable />
           </CardContent>
         </Card>

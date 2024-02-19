04636ce407611e0e564c3934db6a9e408f9db2ad
Amrit Pandey <pandeyamrit714@gmail.com>
2024-02-19 12:49:06 +0545

    Voice detail page

---
+++ apps/rahat-ui/src/app/communications/voice/[id]/page.tsx
@@ -0,0 +1,9 @@
+import VoiceDetailView from 'apps/rahat-ui/src/components/communications/voiceDetailView';
+
+export const metadata = {
+  title: 'Voice: Detail',
+};
+
+export default function VoiceCampaignDetail() {
+  return <VoiceDetailView />;
+}
---
+++ apps/rahat-ui/src/components/communications/voiceDetailView.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import VoiceDetailTable from './voiceDetailTable';
+import VoiceInfoCard from './infoCard';
+import { Card, CardContent } from '@rahat-ui/shadcn/src/components/ui/card';
+
+export default function VoiceDetailView() {
+  return (
+    <>
+      <div className="mt-8">
+        <VoiceInfoCard />
+      </div>
+      <div className="mt-5">
+        <Card>
+          <CardContent>
+            <VoiceDetailTable />
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}

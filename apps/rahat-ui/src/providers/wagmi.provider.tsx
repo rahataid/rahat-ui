e4f5a7cda49f3fa1697a22941f364e38564e7767
Raghav Kattel <arg.ahv7@gmail.com>
2024-02-22 11:48:43 +0545

    Update wagmi.config path

--- apps/rahat-ui/src/providers/wagmi.provider.tsx
+++ apps/rahat-ui/src/providers/wagmi.provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { WagmiProvider } from 'wagmi';
-import { config } from '../wagmi.config';
+import { config } from '../../wagmi.config';
 
 export const Wagmi = ({ children }) => {
   return <WagmiProvider config={config}>{children}=</WagmiProvider>;

03cc87e0490a84afc148a1f933e3072b5fe8cd9e
bipinparajuli <bipinprjl@gmail.com>
2024-02-27 09:28:17 +0545

    fix:remove unused props

--- apps/rahat-ui/src/app/user/page.tsx
+++ apps/rahat-ui/src/app/user/page.tsx
@@ -1,26 +1,24 @@
 'use client';
+import { useState } from 'react';
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from '@rahat-ui/shadcn/components/resizable';
 import { Tabs } from '@rahat-ui/shadcn/components/tabs';
+
 import UserNav from '../../components/users/nav';
-import UsersTable from '../../components/users/usersTable';
-import UserDetails from '../../components/users/viewUser';
-import RoleDetails from '../../components/users/role/roleDetail';
-import { IRoleItem, IUserItem } from '../../types/user';
-import { useState } from 'react';
-import { USER_NAV_ROUTE } from '../../const/user.const';
-import RoleTable from '../../components/users/role/roleTable';
-import AddRole from '../../components/users/role/addRole';
 import AddUser from '../../components/users/addUser';
+import { IRoleItem, IUserItem } from '../../types/user';
+import { USER_NAV_ROUTE } from '../../const/user.const';
+import UserDetails from '../../components/users/viewUser';
+import AddRole from '../../components/users/role/addRole';
+import UsersTable from '../../components/users/usersTable';
+import RoleTable from '../../components/users/role/roleTable';
+import RoleDetails from '../../components/users/role/roleDetail';
 
-type IProps = {
-  onTabChange: (tab: string) => void;
-};
-
-export default function UsersPage({ onTabChange }: IProps) {
+export default function UsersPage() {
   const [selectedUserData, setSelectedUserData] = useState<IUserItem>();
   const [selectedRoleData, setSelectedRoleData] = useState<IRoleItem>();
   const [addUser, setAddUser] = useState<boolean>(false);

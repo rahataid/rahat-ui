cf118cc42edf863621681caf6ffac511c53ed4e0
Raghav Kattel <arg.ahv7@gmail.com>
2024-02-28 13:04:47 +0545

    Update import paths and refactor beneficiary service

--- apps/rahat-ui/src/app/communications/text/edit/[id]/page.tsx
+++ apps/rahat-ui/src/app/communications/text/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-import EditTextCampaign from 'apps/rahat-ui/src/components/communications/text/editCampaign';
+import EditTextCampaign from 'apps/rahat-ui/src/sections/communications/text/editCampaign';
 
 export const metadata = {
   title: 'Text: Edit',

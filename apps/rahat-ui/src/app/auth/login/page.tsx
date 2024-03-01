fa8da982b2264f10d4db2cb6fd9225a6cbe1a620
Amrit Pandey <pandeyamrit714@gmail.com>
2024-03-01 09:18:05 +0545

    update

--- apps/rahat-ui/src/app/auth/login/page.tsx
+++ apps/rahat-ui/src/app/auth/login/page.tsx
@@ -93,12 +93,7 @@ export default function AuthPage() {
                     {error?.response?.data?.message}
                   </p>
                 )}
-                <Button
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
-                  type="submit"
-                >
-                  Send OTP
-                </Button>
+                <Button type="submit">Send OTP</Button>
               </div>
             </form>
           ) : (

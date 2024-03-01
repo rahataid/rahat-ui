62e68ca86c7b12294a13351612a625cd065a0bee
Amrit Pandey <pandeyamrit714@gmail.com>
2024-03-01 12:37:46 +0545

    users in, communicatios out

--- apps/rahat-ui/src/components/nav.tsx
+++ apps/rahat-ui/src/components/nav.tsx
@@ -54,48 +54,69 @@ export function Nav() {
           />
         </Link>
         <nav className="hidden md:flex items-center">
-          {data.map((item) => (
-            <Link key={item.title} href={item.path}>
-              <p
-                className={`py-2 px-4 font-light rounded ${
-                  currentPath === item.path && 'bg-secondary'
-                }`}
-              >
-                {item.title}
-              </p>
-            </Link>
-          ))}
+          {data.map((item) =>
+            item.children ? (
+              <DropdownMenu key={item.title}>
+                <DropdownMenuTrigger className="py-2 px-4 font-light cursor-pointer border:none">
+                  {item.title}
+                </DropdownMenuTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuContent>
+                    {item.children.map((child) => (
+                      <Link key={child.title} href={child.path}>
+                        <DropdownMenuItem className="cursor-pointer">
+                          {child.title}
+                        </DropdownMenuItem>
+                      </Link>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenuPortal>
+              </DropdownMenu>
+            ) : (
+              <Link key={item.title} href={item.path}>
+                <p
+                  className={`py-2 px-4 font-light rounded ${
+                    currentPath === item.path && 'bg-secondary'
+                  }`}
+                >
+                  {item.title}
+                </p>
+              </Link>
+            )
+          )}
           <DropdownMenu>
             <DropdownMenuTrigger className="py-2 px-4 font-light rounded">
               More...
             </DropdownMenuTrigger>
             <DropdownMenuContent className="ml-12">
-              {subData.map((item) =>
-                item.children ? (
-                  <DropdownMenuSub key={item.title}>
-                    <DropdownMenuSubTrigger>
-                      {item.title}
-                    </DropdownMenuSubTrigger>
-                    <DropdownMenuPortal>
-                      <DropdownMenuSubContent>
-                        {item.children.map((child) => (
-                          <Link key={child.title} href={child.path}>
-                            <DropdownMenuItem className="cursor-pointer">
-                              {child.title}
-                            </DropdownMenuItem>
-                          </Link>
-                        ))}
-                      </DropdownMenuSubContent>
-                    </DropdownMenuPortal>
-                  </DropdownMenuSub>
-                ) : (
+              {
+                subData.map((item) => (
+                  // item?.children ? (
+                  //   <DropdownMenuSub key={item.title}>
+                  //     <DropdownMenuSubTrigger>
+                  //       {item.title}
+                  //     </DropdownMenuSubTrigger>
+                  //     <DropdownMenuPortal>
+                  //       <DropdownMenuSubContent>
+                  //         {item.children.map((child) => (
+                  //           <Link key={child.title} href={child.path}>
+                  //             <DropdownMenuItem className="cursor-pointer">
+                  //               {child.title}
+                  //             </DropdownMenuItem>
+                  //           </Link>
+                  //         ))}
+                  //       </DropdownMenuSubContent>
+                  //     </DropdownMenuPortal>
+                  //   </DropdownMenuSub>
+                  // ) : (
                   <Link key={item.title} href={item.path}>
                     <DropdownMenuItem className="cursor-pointer">
                       {item.title}
                     </DropdownMenuItem>
                   </Link>
-                )
-              )}
+                ))
+                // )
+              }
             </DropdownMenuContent>
           </DropdownMenu>
         </nav>

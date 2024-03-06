32da8c1f131e1d2cb1610de2e7285e579a6e14d8
Binod Chaudhary <thebinod7@gmail.com>
2024-03-06 18:40:02 +0545

    load and update benef details

--- apps/rahat-ui/src/sections/beneficiary/beneficiaryDetail.tsx
+++ apps/rahat-ui/src/sections/beneficiary/beneficiaryDetail.tsx
@@ -22,6 +22,7 @@ import { paths } from '../../routes/paths';
 import { IBeneficiaryItem } from '../../types/beneficiary';
 import EditBeneficiary from './editBeneficiary';
 import InfoCards from './infoCards';
+import { useRumsanService } from '../../providers/service.provider';
 
 type IProps = {
   data: IBeneficiaryItem;
@@ -29,12 +30,10 @@ type IProps = {
   handleClose: VoidFunction;
 };
 
-export default function BeneficiaryDetail({
-  data,
-  handleDefault,
-  handleClose,
-}: IProps) {
+export default function BeneficiaryDetail({ data, handleClose }: IProps) {
   const router = useRouter();
+  const { beneficiaryQuery } = useRumsanService();
+  let beneficiary = null;
 
   const changedDate = new Date(data?.updatedAt);
   const formattedDate = changedDate.toLocaleDateString('en-US', {
@@ -42,6 +41,14 @@ export default function BeneficiaryDetail({
     month: 'long',
     day: 'numeric',
   });
+
+  if (data.uuid) {
+    const response = beneficiaryQuery.useBeneficiaryGet(data.uuid);
+    beneficiary = response.data?.data;
+  }
+  console.log('BEN', beneficiary);
+
+  const piData = { name: 'John Doe', phone: '1234567890' };
   return (
     <>
       <Tabs defaultValue="detail">
@@ -73,7 +80,7 @@ export default function BeneficiaryDetail({
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
-            <TooltipProvider delayDuration={100}>
+            {/* <TooltipProvider delayDuration={100}>
               <Tooltip>
                 <TooltipTrigger>
                   <FilePenLine size={20} strokeWidth={1.5} />
@@ -82,7 +89,7 @@ export default function BeneficiaryDetail({
                   <p className="text-xs font-medium">Edit</p>
                 </TooltipContent>
               </Tooltip>
-            </TooltipProvider>
+            </TooltipProvider> */}
             <TooltipProvider delayDuration={100}>
               <Tooltip>
                 <TooltipTrigger>
@@ -118,111 +125,16 @@ export default function BeneficiaryDetail({
             <TabsTrigger value="edit">Edit</TabsTrigger>
           </TabsList>
         </div>
-        {/* <div className="flex justify-between items-center p-4">
-          <div className="flex gap-4">
-            <Image
-              className="rounded-full"
-              src="/svg/funny-cat.svg"
-              alt="cat"
-              height={80}
-              width={80}
-            />
-            <div className="my-auto">
-              <h1 className="font-semibold text-xl mb-2">
-                {data.walletAddress}
-              </h1>
-              <div className="flex justify-between">
-                <p>Edit</p>
-                <p>Delete</p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="text-slate-500">
-              {formattedDate}
-              <br />
-              Last updated
-            </p>
-          </div>
-        </div> */}
+
         <TabsContent value="detail">
-          {/* <div className="grid grid-cols-2 border-y font-light">
-            <div className="border-r p-4 flex flex-col gap-2 ">
-              <p>Name</p>
-              <p>Verified</p>
-              <p>Updated Date</p>
-            </div>
-            <div className="p-4 flex flex-col gap-2">
-              <p>{data.walletAddress}</p>
-              <p>{data.verified ? 'True' : 'False'}</p>
-              <p>{formattedDate}</p>
-            </div>
-          </div> */}
-          {/* <Card className="shadow-md m-2">
-            <CardHeader>
-              <div className="flex justify-between">
-                <p>Name</p>
-                <Badge variant="outline" color="red">
-                  Not Approved
-                </Badge>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <div className="grid grid-cols-2 gap-8">
-                <div>
-                  <p>{data.walletAddress ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Wallet Address
-                  </p>
-                </div>
-                <div>
-                  <p>{data.gender ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Gender
-                  </p>
-                </div>
-                <div>
-                  <p>{data.bankStatus ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Bank Status
-                  </p>
-                </div>
-                <div>
-                  <p>{data.location ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Location
-                  </p>
-                </div>
-                <div>
-                  <p>{data.internetStatus ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Internet Status
-                  </p>
-                </div>
-                <div>
-                  <p>{data.phoneStatus ?? 'test'}</p>
-                  <p className="text-sm font-normal text-muted-foreground">
-                    Phone Status
-                  </p>
-                </div>
-              </div>
-            </CardContent>
-          </Card> */}
           <InfoCards data={data} />
         </TabsContent>
         <TabsContent value="transaction-history">
           <div className="p-4 border-y">Transaction History View</div>
         </TabsContent>
         <TabsContent value="edit">
-          <EditBeneficiary />
+          {beneficiary && <EditBeneficiary beneficiary={beneficiary} />}
         </TabsContent>
-        {/* <div className="p-6 flex justify-between">
-          <div className="flex items-center space-x-2">
-            <Switch id="disable-user" />
-            <Label htmlFor="disable-user">Disable this user</Label>
-          </div>
-          <Button>Confirm</Button>
-        </div> */}
       </Tabs>
     </>
   );

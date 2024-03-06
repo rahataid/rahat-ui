138cb6a2527a334fa7bfca8ba5dd9929506a8d19
Amrit Pandey <pandeyamrit714@gmail.com>
2024-03-06 22:48:49 +0545

    ui feedback

--- apps/rahat-ui/src/sections/beneficiary/infoCards.jsx
+++ apps/rahat-ui/src/sections/beneficiary/infoCards.jsx
@@ -38,7 +38,6 @@ import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
 import { MoreVertical } from 'lucide-react';
 import { Input } from '@rahat-ui/shadcn/src/components/ui/input';
 import { truncateEthAddress } from '@rumsan/sdk/utils';
-// import data from '../../app/beneficiary/beneficiaryData.json';
 
 export default function InfoCards({ data }) {
   return (
@@ -47,22 +46,22 @@ export default function InfoCards({ data }) {
         <CardHeader>
           <div className="flex justify-between">
             <p>Beneficiary Name</p>
-            <Badge variant="outline" color="red">
+            <Badge variant="outline" className="bg-secondary">
               Not Approved
             </Badge>
           </div>
         </CardHeader>
         <CardContent>
-          <div className="mb-2">
-            <p className="text-xs">
-              {truncateEthAddress(data?.walletAddress) ?? 'N/A'}
-            </p>
-            <p className="text-sm font-normal text-muted-foreground">
-              Wallet Address
-            </p>
-          </div>
           <div className="flex justify-between gap-8">
             <div className="flex flex-col gap-2">
+              <div>
+                <p className="text-xs">
+                  {truncateEthAddress(data?.walletAddress) ?? 'N/A'}
+                </p>
+                <p className="text-sm font-normal text-muted-foreground">
+                  Wallet Address
+                </p>
+              </div>
               <div>
                 <p>{data?.bankStatus ?? 'test'}</p>
                 <p className="text-sm font-normal text-muted-foreground">
@@ -73,12 +72,6 @@ export default function InfoCards({ data }) {
                 <p>{data?.internetStatus ?? 'test'}</p>
                 <p className="text-sm font-normal text-muted-foreground">
                   Internet Status
-                </p>
-              </div>
-              <div>
-                <p>{data?.phoneStatus ?? 'test'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Phone Status
                 </p>
               </div>
             </div>
@@ -94,6 +87,12 @@ export default function InfoCards({ data }) {
                 <p>{data?.location ?? 'test'}</p>
                 <p className="text-sm font-normal text-muted-foreground">
                   Location
+                </p>
+              </div>
+              <div>
+                <p>{data?.phoneStatus ?? 'test'}</p>
+                <p className="text-sm font-normal text-muted-foreground">
+                  Phone Status
                 </p>
               </div>
             </div>

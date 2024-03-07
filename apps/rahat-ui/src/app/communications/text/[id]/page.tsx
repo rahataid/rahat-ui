c68ae14b5f4b1adab25516d40c952f06fd582720
bipinparajuli <bipinprjl@gmail.com>
2024-03-07 15:11:19 +0545

    fix:communication integration

--- apps/rahat-ui/src/app/communications/text/[id]/page.tsx
+++ apps/rahat-ui/src/app/communications/text/[id]/page.tsx
@@ -1,4 +1,4 @@
-import TextDetailView from '../../../../sections/communications/text/textDetailTable';
+import TextDetailView from '../../../../sections/communications/text/textDetailView';
 
 export const metadata = {
   title: 'Text: Detail',

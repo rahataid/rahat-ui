4ffe2e4b019b6fc544efd8f73e4b98df7d5f4310
myanzik <myanz.470shrestha@gmail.com>
2024-03-07 19:07:53 +0545

    fix merge conflict

--- apps/rahat-ui/src/sections/communications/voice/voiceView.tsx
+++ apps/rahat-ui/src/sections/communications/voice/voiceView.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState } from 'react';
 
-import { COMMUNICATION_NAV_ROUTE } from '@/apps/rahat-ui/src/constants/communication.const';
 import {
   ResizableHandle,
   ResizablePanel,

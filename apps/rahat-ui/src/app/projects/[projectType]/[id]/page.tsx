377defc6b5e8cb7f6674c8773bcf55f00f3366b0
Raghav Kattel <arg.ahv7@gmail.com>
2024-03-07 12:07:35 +0545

    Refactor dynamic imports and update component type in pages object

--- apps/rahat-ui/src/app/projects/[projectType]/[id]/page.tsx
+++ apps/rahat-ui/src/app/projects/[projectType]/[id]/page.tsx
@@ -1,20 +1,36 @@
 'use client';
 
-import { AAProjectView } from 'apps/rahat-ui/src/sections/projects/aa';
-import { CVAProjectView } from 'apps/rahat-ui/src/sections/projects/cva';
-import { ELProjectView } from 'apps/rahat-ui/src/sections/projects/el';
+import dynamic from 'next/dynamic';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import { ComponentType } from 'react'; // Import ComponentType
 
-const pages = {
-  cva: <CVAProjectView />,
-  aa: <AAProjectView />,
-  el: <ELProjectView />,
+const AAProjectView = dynamic(
+  () => import('../../../../sections/projects/aa/project-details.view')
+);
+const CVAProjectView = dynamic(
+  () => import('../../../../sections/projects/cva/project-details.view')
+);
+
+const ELProjectView = dynamic(
+  () => import('../../../../sections/projects/el/project-details.view')
+);
+
+const pages: { [key: string]: ComponentType<any> } = {
+  // Update the type of values in the pages object
+  cva: CVAProjectView,
+  aa: AAProjectView,
+  el: ELProjectView,
 };
 
 const ProjectDetail = () => {
-  const { projectType, id } = useParams();
-  return pages[projectType];
+  const { projectType } = useParams();
+  const Page = pages[projectType as string];
+
+  return Page ? (
+    <Page />
+  ) : (
+    <h1 className="text-red-500">The Project Type doesn&apos;t exist.</h1>
+  );
 };
 
 export default ProjectDetail;

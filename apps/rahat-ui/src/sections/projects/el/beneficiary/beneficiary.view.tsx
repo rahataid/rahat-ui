a3070e0ad87c146cf81f33a2488100036af1a766
Manjik Shrestha <myanz.470shrestha@gmail.com>
2024-03-15 19:50:05 +0545

    El refactor dev merge (#91)
    * code refactor for data fetch from subgraph
    
    * closes #63, graph function call re-structured
    
    * update project detials page
    
    * project details
    
    * el navbar pages update
    
    * clean up
    
    * husky update
    
    * update
    
    * beneficiary table update
    
    * el project table updates
    
    * table design updates
    
    * beneficiary details page table update
    
    * Refactor project layout and add create voucher modal
    
    * changes
    
    * dynamic-data-in-beneficiary-page
    
    * temp
    
    * project action sdk created
    
    * formatting changes
    
    ---------
    
    Co-authored-by: anupamakoirala <anukoirala.96@hotmail.com>
    Co-authored-by: sarvesh-karki <sarveshkarki93@gmail.com>
    Co-authored-by: sushant-rumsan <sushanttripathee1154@gmail.com>
    Co-authored-by: Raghav Kattel <arg.ahv7@gmail.com>

--- apps/rahat-ui/src/sections/projects/el/beneficiary/beneficiary.view.tsx
+++ apps/rahat-ui/src/sections/projects/el/beneficiary/beneficiary.view.tsx
@@ -1,21 +1,36 @@
 'use client';
 
+import { useState } from 'react';
 import {
   ResizablePanel,
   ResizablePanelGroup,
 } from '@rahat-ui/shadcn/src/components/ui/resizable';
 import BeneficiaryTable from './beneficiary.table';
+import BeneficiaryDetail from './beneficiary.detail';
 
 export default function BeneficiaryView() {
-  // const handleClose = () => {
-  //   setSelectedBeneficiary(undefined);
-  // };
+  const [selectedBeneficiary, setSelectedBeneficiary] = useState();
 
+  const handleRowClick = (selected) => {
+    setSelectedBeneficiary(selected);
+  };
+
+  const handleClose = () => {
+    setSelectedBeneficiary(undefined);
+  };
   return (
     <ResizablePanelGroup className="bg-secondary" direction="horizontal">
       <ResizablePanel>
         <BeneficiaryTable />
       </ResizablePanel>
+      {selectedBeneficiary && (
+        <ResizablePanel defaultSize={32}>
+          <BeneficiaryDetail
+            data={selectedBeneficiary}
+            handleClose={handleClose}
+          />
+        </ResizablePanel>
+      )}
     </ResizablePanelGroup>
   );
 }

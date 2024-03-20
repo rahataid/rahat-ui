83f7eec73019a650f02b293cb4e6f6e477ea3206
Binod Chaudhary <thebinod7@gmail.com>
2024-03-20 15:23:02 +0545

    update package

---
+++ apps/community-tool-ui/src/app/beneficiary/add/page.tsx
@@ -0,0 +1,10 @@
+import AddBeneficiary from '../../../sections/beneficiary/add';
+import React from 'react';
+
+export const metadata = {
+  title: 'Beneficiary- Add',
+};
+
+export default function page() {
+  return <AddBeneficiary />;
+}
---
+++ apps/community-tool-ui/src/app/beneficiary/import/page.tsx
@@ -0,0 +1,10 @@
+import ImportBeneficiary from '../../../sections/beneficiary/import/';
+import React from 'react';
+
+export const metadata = {
+  title: 'Beneficiary- Import',
+};
+
+export default function page() {
+  return <ImportBeneficiary />;
+}

af3a8cdceca2976acb3da48e54f5079894bf1962
RS21 <rs21@pop-os.localdomain>
2024-03-25 15:02:33 +0545

    changes in community  beneficiary group query

--- libs/community-query/src/beneficiaryGroup/beneficiary.group.ts
+++ libs/community-query/src/beneficiaryGroup/beneficiary.group.ts
@@ -1,20 +1,11 @@
 import { getBeneficiaryGroupClient } from '@rahataid/community-tool-sdk/clients';
-import {
-  BeneficiaryGroupClient,
-  GroupClient,
-} from '@rahataid/community-tool-sdk/types';
+import { BeneficiaryGroupClient } from '@rahataid/community-tool-sdk/types';
 
 import { RumsanService } from '@rumsan/sdk';
-import {
-  QueryClient,
-  useMutation,
-  useQuery,
-  UseQueryResult,
-} from '@tanstack/react-query';
+import { QueryClient, useMutation } from '@tanstack/react-query';
 
 import { TAGS } from '../config';
 import Swal from 'sweetalert2';
-import { BeneficiaryGroup } from '@rahataid/community-tool-sdk/beneficiarygroup';
 
 export class CommunityBeneficiaryGroupQuery {
   private client: BeneficiaryGroupClient;
@@ -41,23 +32,29 @@ export class CommunityBeneficiaryGroupQuery {
         });
 
         const inputData = {
-          beneficiaryId: payload?.selectedData,
+          beneficiariesId: payload?.selectedData,
           groupId: parseInt(value),
         };
 
         return this.client.create(inputData as any);
       },
-      onSuccess: async (data) => {
+      onSuccess: async (data: any) => {
         await this.qc.invalidateQueries({
           queryKey: [TAGS.LIST_COMMUNITY_BENEFICIARY_GROUP],
         });
 
-        data &&
-          Swal.fire({
-            title: 'Beneficiaries Group has been created Sucessfully ',
+        data && data?.data?.info === false
+          ? Swal.fire({
+              text: data?.data?.finalMessage,
+              icon: 'success',
+            })
+          : Swal.fire({
+              title: data?.data?.finalMessage,
+              titleText: data?.data?.finalMessage,
+              text: data?.data?.info,
 
-            icon: 'success',
-          });
+              icon: 'info',
+            });
       },
       onError: (error: any) => {
         Swal.fire({

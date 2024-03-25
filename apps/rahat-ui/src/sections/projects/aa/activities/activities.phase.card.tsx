e3e351cf66b547d64e286d64669440de5aae4e0b
Amrit Pandey <pandeyamrit714@gmail.com>
2024-03-25 18:31:33 +0545

    phase cards ui

---
+++ apps/rahat-ui/src/sections/projects/aa/activities/activities.phase.card.tsx
@@ -0,0 +1,80 @@
+import { Card, CardContent } from '@rahat-ui/shadcn/src/components/ui/card';
+import { RadialChart } from '@rahat-ui/shadcn/src/components/charts';
+import {
+  EllipsisVertical,
+  TriangleAlert,
+  CircleCheck,
+  CircleX,
+} from 'lucide-react';
+
+export default function PhaseCard() {
+  return (
+    <div className="grid grid-cols-3 p-2 gap-2 ">
+      <Card className="h-auto overflow-hidden">
+        <CardContent className="flex p-2">
+          <div>
+            <RadialChart series={[30]} total={15} />
+          </div>
+          <div>
+            <div className="flex justify-between">
+              <p className="font-medium text-xl mb-2">Preparedness</p>
+              <EllipsisVertical size={20} strokeWidth={1.5} />
+            </div>
+            <p className="font-normal text-md pr-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="flex justify-end mt-2">
+              <div className="flex gap-3">
+                <TriangleAlert color="yellow" size={20} strokeWidth={1.5} />
+                <CircleCheck color="green" size={20} strokeWidth={1.5} />
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="h-auto overflow-hidden">
+        <CardContent className="flex p-2">
+          <RadialChart series={[50]} total={5} />
+          <div>
+            <div className="flex justify-between">
+              <p className="font-medium text-xl">Readiness</p>
+              <EllipsisVertical size={20} strokeWidth={1.5} />
+            </div>
+            <p className="font-normal text-md pr-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="flex justify-end mt-2">
+              <div className="flex gap-3">
+                <TriangleAlert color="yellow" size={20} strokeWidth={1.5} />
+                <CircleX color="red" size={20} strokeWidth={1.5} />
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="h-auto overflow-hidden">
+        <CardContent className="flex p-2">
+          <RadialChart series={[70]} total={10} />
+          <div>
+            <div className="flex justify-between">
+              <p className="font-medium text-xl">Activation</p>
+              <EllipsisVertical size={20} strokeWidth={1.5} />
+            </div>
+            <p className="font-normal text-md pr-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <div className="flex justify-end mt-2">
+              <div className="flex gap-3">
+                <TriangleAlert color="yellow" size={20} strokeWidth={1.5} />
+                <CircleCheck color="green" size={20} strokeWidth={1.5} />
+              </div>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}

d6c3a02cefd1b03fffe319bf550ba96d66d4cc65
sarvesh-karki <sarveshkarki93@gmail.com>
2024-03-25 15:45:09 +0545

    vendor referral and transaction table

--- apps/rahat-ui/src/sections/projects/el/vendors/vendors.view.tsx
+++ apps/rahat-ui/src/sections/projects/el/vendors/vendors.view.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/tabs';
 import DataCard from '../../../../components/dataCard';
-import VendorTable from '../../../../sections/vendors/vendorsTable';
+import VendorTable from '../../../vendors/vendors.transaction.table';
+import ReferralTable from '../../../vendors/vendors.referral.table';
 
 export default function VendorsPage() {
   return (
@@ -33,7 +40,20 @@ export default function VendorsPage() {
           subTitle="Referred Beneficiaries"
         />
       </div>
-      <VendorTable />
+      <div className="mt-2 mx-2">
+        <Tabs defaultValue="transactions" className="w-full">
+          <TabsList>
+            <TabsTrigger value="transactions">Transaction History</TabsTrigger>
+            <TabsTrigger value="referrals">Referrals List</TabsTrigger>
+          </TabsList>
+          <TabsContent value="transactions">
+            <VendorTable />
+          </TabsContent>
+          <TabsContent value="referrals">
+            <ReferralTable />
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }

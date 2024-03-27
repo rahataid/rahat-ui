b83222545ccf32888fdefc4d46f75683a6f37654
Raghav Kattel <arg.ahv7@gmail.com>
2024-03-27 08:44:04 +0545

    Refactor code to improve performance and readability

--- apps/rahat-ui/src/sections/beneficiary/infoCards.tsx
+++ apps/rahat-ui/src/sections/beneficiary/infoCards.tsx
@@ -1,4 +1,20 @@
 'use client';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@rahat-ui/shadcn/components/dialog';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@rahat-ui/shadcn/components/select';
 import { Badge } from '@rahat-ui/shadcn/src/components/ui/badge';
 import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
 import {
@@ -8,19 +24,33 @@ import {
 } from '@rahat-ui/shadcn/src/components/ui/card';
 import { MS_ACTIONS } from '@rahataid/sdk';
 import { truncateEthAddress } from '@rumsan/sdk/utils';
+import * as React from 'react';
 import { useProjectAction } from '../../../../../libs/query/src/lib/projects/projects';
+import { useSwal } from '../../components/swal';
+import { useBoolean } from '../../hooks/use-boolean';
+import { useRumsanService } from '../../providers/service.provider';
 
 export default function InfoCards({ data, voucherData }) {
   const addBeneficiary = useProjectAction();
+  const { projectQuery } = useRumsanService();
 
-  const handleAssignClaims = async () => {
-    const walletAddress = data.walletAddress || '';
+  const [selectedProject, setSelectedProject] = React.useState('');
+  const [selectedRow, setSelectedRow] = React.useState(null) as any;
 
-    // Remove fetching uuid from env
-    const uuid = process.env.NEXT_PUBLIC_PROJECT_UUID;
+  const alert = useSwal();
+
+  const projectsList = projectQuery.useProjectList({});
+  const d = projectsList.data;
+  const projectList = d?.data || [];
+
+  const projectModal = useBoolean();
+  const handleProjectChange = (d: string) => setSelectedProject(d);
+
+  const handleAssignProject = async () => {
+    if (!selectedProject) return alert('Please select a project');
 
     const result = await addBeneficiary.mutateAsync({
-      uuid,
+      uuid: selectedProject,
       data: {
         action: MS_ACTIONS.BENEFICIARY.ASSGIN_TO_PROJECT,
         payload: {
@@ -30,87 +60,158 @@ export default function InfoCards({ data, voucherData }) {
     });
   };
 
-  console.log('INFO==>', data);
+  const handleAssignModalClick = (row: any) => {
+    setSelectedRow(row);
+    projectModal.onTrue();
+  };
+
+  React.useEffect(() => {
+    if (!addBeneficiary) return;
+    if (addBeneficiary.isSuccess) {
+      alert.fire({
+        title: 'Beneficiary Assigned Successfully',
+        icon: 'success',
+      });
+      addBeneficiary.reset();
+    }
+    if (addBeneficiary.isError) {
+      alert.fire({
+        title: 'Error while updating Beneficiary',
+        icon: 'error',
+      });
+      addBeneficiary.reset();
+    }
+  }, [addBeneficiary, alert]);
 
   return (
-    <div className="flex flex-col gap-2 py-2 pl-2">
-      <Card className="shadow rounded">
-        <CardHeader>
-          <div className="flex justify-between">
-            <div className="flex flex-col items-start justify-start">
-              <p>{data?.piiData?.name || 'N/A'}</p>
-              <Badge variant="outline" className="bg-secondary">
-                Not Approved
-              </Badge>
+    <>
+      <div className="flex flex-col gap-2 py-2 pl-2">
+        <Card className="shadow rounded">
+          <CardHeader>
+            <div className="flex justify-between">
+              <div className="flex flex-col items-start justify-start">
+                <p>Beneficiary Name</p>
+                <Badge variant="outline" className="bg-secondary">
+                  Not Approved
+                </Badge>
+              </div>
+              <Button onClick={handleAssignModalClick}>
+                Assign To Project
+              </Button>
             </div>
-            <Button onClick={handleAssignClaims}>Assign To Project</Button>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-between gap-8">
-            <div className="flex flex-col gap-2">
-              <div>
-                <p className="text-xs">
-                  {truncateEthAddress(data?.walletAddress) ?? 'N/A'}
-                </p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Wallet Address
-                </p>
+          </CardHeader>
+          <CardContent>
+            <div className="flex justify-between gap-8">
+              <div className="flex flex-col gap-2">
+                <div>
+                  <p className="text-xs">
+                    {truncateEthAddress(data?.walletAddress) ?? 'N/A'}
+                  </p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Wallet Address
+                  </p>
+                </div>
+                <div>
+                  <p>{data?.bankStatus ?? 'test'}</p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Bank Status
+                  </p>
+                </div>
+                <div>
+                  <p>{data?.internetStatus ?? 'test'}</p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Internet Status
+                  </p>
+                </div>
               </div>
-              <div>
-                <p>{data?.bankStatus ?? '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Bank Status
-                </p>
-              </div>
-              <div>
-                <p>{data?.internetStatus ?? '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Internet Status
-                </p>
-              </div>
-              <div>
-                <p>{data?.piiData?.phone || '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Phone
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              <div>
-                <p>{data?.gender ?? '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Gender
-                </p>
-              </div>
+              <div className="flex flex-col gap-2">
+                <div>
+                  <p>{data?.gender ?? 'test'}</p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Gender
+                  </p>
+                </div>
 
-              <div>
-                <p>{data?.location ?? '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Location
-                </p>
-              </div>
-              <div>
-                <p>{data?.phoneStatus ?? '-'}</p>
-                <p className="text-sm font-normal text-muted-foreground">
-                  Phone Status
-                </p>
+                <div>
+                  <p>{data?.location ?? 'test'}</p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Location
+                  </p>
+                </div>
+                <div>
+                  <p>{data?.phoneStatus ?? 'test'}</p>
+                  <p className="text-sm font-normal text-muted-foreground">
+                    Phone Status
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
 
-      <Card className="shadow rounded">
-        <CardHeader>
-          <p className="font-mediun text-md">Projects Involved</p>
-        </CardHeader>
-        <CardContent>
-          <Badge variant="outline" color="secondary" className="rounded">
-            - Project
-          </Badge>
-        </CardContent>
-      </Card>
-    </div>
+        <Card className="shadow rounded">
+          <CardHeader>
+            <p className="font-mediun text-md">Projects Involved</p>
+          </CardHeader>
+          <CardContent>
+            <Badge variant="outline" color="secondary" className="rounded">
+              Test Project
+            </Badge>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="py-2 w-full border-t">
+        <div className="p-4 flex flex-col gap-0.5 text-sm">
+          <Dialog
+            open={projectModal.value}
+            onOpenChange={projectModal.onToggle}
+          >
+            {/* <DialogTrigger className=" hover:bg-muted p-1 rounded text-left">
+              Assign Projects
+            </DialogTrigger> */}
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Assign Project</DialogTitle>
+                <DialogDescription>
+                  Select the project to be assigned to the beneficiary
+                </DialogDescription>
+              </DialogHeader>
+              <div>
+                <Select onValueChange={handleProjectChange}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Projects" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {projectList.length > 0 &&
+                      projectList.map((project: any) => {
+                        return (
+                          <SelectItem key={project.id} value={project.id}>
+                            {project.title}
+                          </SelectItem>
+                        );
+                      })}
+                  </SelectContent>
+                </Select>
+              </div>
+              <DialogFooter className="sm:justify-end">
+                <DialogClose asChild>
+                  <Button type="button" variant="ghost">
+                    Close
+                  </Button>
+                </DialogClose>
+                <Button
+                  onClick={handleAssignProject}
+                  type="button"
+                  variant="ghost"
+                  className="text-primary"
+                >
+                  Assign
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </div>
+    </>
   );
 }

c608afd3ee3c68dfe544d0da5ab31ea29fd82c8c
Amrit Pandey <pandeyamrit714@gmail.com>
2024-03-28 18:41:26 +0545

    update

--- apps/rahat-ui/src/sections/beneficiary/components/assignToProjectModal.tsx
+++ apps/rahat-ui/src/sections/beneficiary/components/assignToProjectModal.tsx
@@ -1,21 +1,21 @@
-'use client'
+'use client';
 
 import * as React from 'react';
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from '@rahat-ui/shadcn/components/dialog';
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from '@rahat-ui/shadcn/components/select';
 import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
 import { useRumsanService } from 'apps/rahat-ui/src/providers/service.provider';
@@ -23,84 +23,84 @@ import { MS_ACTIONS } from '@rahataid/sdk';
 import { useProjectAction } from '../../../../../../libs/query/src/lib/projects/projects';
 
 type ProjectModalType = {
-    value: boolean;
-    onToggle: () => void;
+  value: boolean;
+  onToggle: () => void;
 };
 
 type IProps = {
-    beneficiaryDetail: any,
-    projectModal: ProjectModalType,
-}
+  beneficiaryDetail: any;
+  projectModal: ProjectModalType;
+};
 
-export default function AssignToProjectModal({ beneficiaryDetail, projectModal }: IProps) {
-    const addBeneficiary = useProjectAction();
-    const { projectQuery } = useRumsanService();
-    const projectsList = projectQuery.useProjectList({});
-    const d = projectsList.data;
-    const projectList = d?.data || [];
+export default function AssignToProjectModal({
+  beneficiaryDetail,
+  projectModal,
+}: IProps) {
+  const addBeneficiary = useProjectAction();
+  const { projectQuery } = useRumsanService();
+  const projectsList = projectQuery.useProjectList({});
+  const d = projectsList.data;
+  const projectList = d?.data || [];
 
-    const [selectedProject, setSelectedProject] = React.useState('');
+  const [selectedProject, setSelectedProject] = React.useState('');
 
-    const handleProjectChange = (d: string) => setSelectedProject(d);
+  const handleProjectChange = (d: string) => setSelectedProject(d);
 
-    const handleAssignProject = async () => {
-        if (!selectedProject) return alert('Please select a project');
-        const result = await addBeneficiary.mutateAsync({
-            uuid: selectedProject,
-            data: {
-                action: MS_ACTIONS.BENEFICIARY.ASSGIN_TO_PROJECT,
-                payload: {
-                    beneficiaryId: beneficiaryDetail?.uuid,
-                },
-            },
-        });
-    };
+  const handleAssignProject = async () => {
+    if (!selectedProject) return alert('Please select a project');
+    const result = await addBeneficiary.mutateAsync({
+      uuid: selectedProject,
+      data: {
+        action: MS_ACTIONS.BENEFICIARY.ASSGIN_TO_PROJECT,
+        payload: {
+          beneficiaryId: beneficiaryDetail?.uuid,
+        },
+      },
+    });
+  };
 
-    return (
-        <Dialog
-            open={projectModal.value}
-            onOpenChange={projectModal.onToggle}
-        >
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Assign Project</DialogTitle>
-                    <DialogDescription>
-                        Select the project to be assigned to the beneficiary
-                    </DialogDescription>
-                </DialogHeader>
-                <div>
-                    <Select onValueChange={handleProjectChange}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Projects" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {projectList.length > 0 &&
-                                projectList.map((project: any) => {
-                                    return (
-                                        <SelectItem key={project.id} value={project.id}>
-                                            {project.title}
-                                        </SelectItem>
-                                    );
-                                })}
-                        </SelectContent>
-                    </Select>
-                </div>
-                <DialogFooter className="sm:justify-end">
-                    <DialogClose asChild>
-                        <Button type="button" variant="ghost">
-                            Close
-                        </Button>
-                    </DialogClose>
-                    <Button
-                        onClick={handleAssignProject}
-                        type="button"
-                        variant="ghost"
-                        className="text-primary"
-                    >
-                        Assign
-                    </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
+  return (
+    <Dialog open={projectModal.value} onOpenChange={projectModal.onToggle}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Assign Project</DialogTitle>
+          <DialogDescription>
+            Select the project to be assigned to the beneficiary
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <Select onValueChange={handleProjectChange}>
+            <SelectTrigger>
+              <SelectValue placeholder="Projects" />
+            </SelectTrigger>
+            <SelectContent>
+              {projectList.length > 0 &&
+                projectList.map((project: any) => {
+                  return (
+                    <SelectItem key={project.id} value={project.id}>
+                      {project.title}
+                    </SelectItem>
+                  );
+                })}
+            </SelectContent>
+          </Select>
+        </div>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose asChild>
+            <Button type="button" variant="ghost">
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={handleAssignProject}
+            type="button"
+            variant="ghost"
+            className="text-primary"
+          >
+            Assign
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }

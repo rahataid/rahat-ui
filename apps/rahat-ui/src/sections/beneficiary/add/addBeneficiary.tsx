7efbc97a6a5360b80832f86ed1e6f23b98b5418c
sarvesh-karki <sarveshkarki93@gmail.com>
2024-04-03 16:52:14 +0545

    warning if walletaddress not entered in add beneficiary

--- apps/rahat-ui/src/sections/beneficiary/add/addBeneficiary.tsx
+++ apps/rahat-ui/src/sections/beneficiary/add/addBeneficiary.tsx
@@ -94,18 +94,210 @@ export default function AddBeneficiaryForm() {
     }
   };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleCreateBeneficiary)}>
-        <div className="p-4 h-add">
-          <div className="shadow-md p-4 rounded-sm bg-card">
-            <h1 className="text-lg font-semibold mb-6">Add Beneficiary</h1>
-            <div className="grid grid-cols-2 gap-4 mb-4">
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleCreateBeneficiary)}>
+          <div className="p-4 h-add">
+            <div className="shadow-md p-4 rounded-sm bg-card">
+              <h1 className="text-lg font-semibold mb-6">Add Beneficiary</h1>
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormControl>
+                          <Input type="text" placeholder="Name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormControl>
+                          <PhoneInput placeholder="Phone" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <div className="grid grid-cols-4 gap-4 mb-4">
+                <div className="col-span-3">
+                  <FormField
+                    control={form.control}
+                    name="address"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormControl>
+                            <Input
+                              type="text"
+                              placeholder="Address"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </div>
+                <div className="col-span-1">
+                  <FormField
+                    control={form.control}
+                    name="age"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormControl>
+                            <Input type="text" placeholder="Age" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 gap-4 mb-4">
+                <FormField
+                  control={form.control}
+                  name="bankedStatus"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Banked Status" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="banked">Banked</SelectItem>
+                            <SelectItem value="under_banked">
+                              Under Banked
+                            </SelectItem>
+                            <SelectItem value="unBanked">UnBanked</SelectItem>
+                            <SelectItem value="unknown">Unknown</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="gender"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Gender" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="male">Male</SelectItem>
+                            <SelectItem value="female">Female</SelectItem>
+                            <SelectItem value="other">Other</SelectItem>
+                            <SelectItem value="unknown">Unknown</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="internetStatus"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Internet Status" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="mobile_internet">
+                              Mobile Internet
+                            </SelectItem>
+                            <SelectItem value="no_internet">
+                              No Internet
+                            </SelectItem>
+                            <SelectItem value="home_internet">
+                              Home Internet
+                            </SelectItem>
+                            <SelectItem value="unknown">Unknown</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="phoneStatus"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Phone Status" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="smart_phone">
+                              Smart Phone
+                            </SelectItem>
+                            <SelectItem value="no_phone">No Phone</SelectItem>
+                            <SelectItem value="feature_phone">
+                              Feature Phone
+                            </SelectItem>
+                            <SelectItem value="unknown">Unknown</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
               <FormField
                 control={form.control}
                 name="walletAddress"
                 render={({ field }) => {
                   return (
-                    <FormItem className="col-span-2">
+                    <FormItem className="col-span-2 mb-4">
                       <FormControl>
                         <div className="relative w-full">
                           <Wallet className="absolute right-2 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -114,6 +306,14 @@ export default function AddBeneficiaryForm() {
                             placeholder="Wallet Address"
                             {...field}
                           />
+                          {!field.value ? (
+                            <p className="text-xs text-amber-500 mt-2">
+                              * Wallet address is required. If not entered, it
+                              will be automatically filled.
+                            </p>
+                          ) : (
+                            ''
+                          )}
                         </div>
                       </FormControl>
                       <FormMessage />
@@ -121,200 +321,14 @@ export default function AddBeneficiaryForm() {
                   );
                 }}
               />
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormControl>
-                        <Input type="text" placeholder="Name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
 
-              <FormField
-                control={form.control}
-                name="phone"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <FormControl>
-                        <PhoneInput placeholder="Phone" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-            </div>
-            <div className="grid grid-cols-4 gap-4 mb-4">
-              <div className="col-span-3">
-                <FormField
-                  control={form.control}
-                  name="address"
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormControl>
-                          <Input type="text" placeholder="Address" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
-                  }}
-                />
+              <div className="flex justify-end">
+                <Button>Create Beneficiary</Button>
               </div>
-              <div className="col-span-1">
-                <FormField
-                  control={form.control}
-                  name="age"
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormControl>
-                          <Input type="text" placeholder="Age" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
-                  }}
-                />
-              </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 mb-4">
-              <FormField
-                control={form.control}
-                name="bankedStatus"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Banked Status" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="banked">Banked</SelectItem>
-                          <SelectItem value="under_banked">
-                            Under Banked
-                          </SelectItem>
-                          <SelectItem value="unBanked">UnBanked</SelectItem>
-                          <SelectItem value="unknown">Unknown</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-
-              <FormField
-                control={form.control}
-                name="gender"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Gender" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="male">Male</SelectItem>
-                          <SelectItem value="female">Female</SelectItem>
-                          <SelectItem value="other">Other</SelectItem>
-                          <SelectItem value="unknown">Unknown</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="internetStatus"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Internet Status" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="mobile_internet">
-                            Mobile Internet
-                          </SelectItem>
-                          <SelectItem value="no_internet">
-                            No Internet
-                          </SelectItem>
-                          <SelectItem value="home_internet">
-                            Home Internet
-                          </SelectItem>
-                          <SelectItem value="unknown">Unknown</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-              <FormField
-                control={form.control}
-                name="phoneStatus"
-                render={({ field }) => {
-                  return (
-                    <FormItem>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Phone Status" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="smart_phone">
-                            Smart Phone
-                          </SelectItem>
-                          <SelectItem value="no_phone">No Phone</SelectItem>
-                          <SelectItem value="feature_phone">
-                            Feature Phone
-                          </SelectItem>
-                          <SelectItem value="unknown">Unknown</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  );
-                }}
-              />
-            </div>
-            <div className="flex justify-end">
-              <Button>Create Beneficiary</Button>
             </div>
           </div>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </>
   );
 }

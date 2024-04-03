1c8852ad29dcdc7d956e5c5bc5f1f6d4c908838f
Binod Chaudhary <thebinod7@gmail.com>
2024-04-03 21:08:28 +0545

    fix field definition populate

--- apps/community-tool-ui/src/sections/beneficiary/import/index.tsx
+++ apps/community-tool-ui/src/sections/beneficiary/import/index.tsx
@@ -8,11 +8,11 @@ import { Tabs } from '@rahat-ui/shadcn/src/components/ui/tabs';
 import React from 'react';
 import BeneficiaryNav from '../../../sections/beneficiary/nav';
 import ImportBen from './Beneficiary';
-import { useFieldDefinitionsList } from '@rahat-ui/community-query';
+import { useActiveFieldDefinitionsList } from '@rahat-ui/community-query';
 
 export default function ImportBeneficiary() {
   let extraFields = [];
-  const { data } = useFieldDefinitionsList();
+  const { data } = useActiveFieldDefinitionsList();
   if (data && data.data.length > 0) {
     const myFields = data.data.map((obj: any) => obj.name);
     extraFields = myFields;

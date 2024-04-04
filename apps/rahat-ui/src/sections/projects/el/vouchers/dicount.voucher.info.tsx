1d0fe3a34b0ff821000ab5fbc4acbdf12efbe08f
anupamakoirala <anukoirala.96@hotmail.com>
2024-04-04 12:38:23 +0545

    fixes in voucher pages

--- apps/rahat-ui/src/sections/projects/el/vouchers/dicount.voucher.info.tsx
+++ apps/rahat-ui/src/sections/projects/el/vouchers/dicount.voucher.info.tsx
@@ -1,30 +1,8 @@
-import { useProjectVoucher } from 'apps/rahat-ui/src/hooks/el/subgraph/querycall';
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
 
-const DiscountVoucherInfo = () => {
-  const[contractAddress,setContractAddress] = useState<any>()
+import React from 'react';
 
-  const { id } = useParams();
-
-  const projectSettings = localStorage.getItem('projectSettingsStore');
-
-  useEffect(()=>{
-    if(projectSettings){
-      const settings = JSON.parse(projectSettings)?.state?.settings?.[id]
-      setContractAddress({
-        el:settings?.elproject?.address,
-        eyeVoucher:settings?.eyevoucher?.address,
-        referredVoucher:settings?.referralvoucher?.address
-      })
-    }
-
-  },[projectSettings])
+const DiscountVoucherInfo = ({data}) => {
   
-  const { data: transactionData, error } = useProjectVoucher(
-    contractAddress?.el || '',
-    'Free'
-  );
   return (
     <div className="m-2 rounded bg-card p-4 shadow">
       <div className="flex items-center flex-wrap mt-2 gap-10 md:gap-32">
@@ -33,17 +11,17 @@ const DiscountVoucherInfo = () => {
           <p className="font-light">Voucher Name</p>
         </div>
         <div>
-          <p className="font-medium text-primary">12</p>
-          <p className="font-light">Price in {contractAddress?.referredVoucherCurrency || ''}</p>
+          <p className="font-medium text-primary">{data?.referredVoucherPrice}</p>
+          <p className="font-light">Price in {data?.referredVoucherCurrency || ''}</p>
         </div>
         <div>
-          <p className="font-medium text-primary">{contractAddress?.referredVoucherBudget || 0}</p>
+          <p className="font-medium text-primary">{data?.referredVoucherBudget || 0}</p>
           <p className="font-light">No. of Voucher Minted</p>
         </div>
       </div>
       <div>
         <p className="mt-4 sm:mt-8 sm:w-2/3">
-          Enter discount voucher description here.
+          {data?.referredVoucherDescription}
         </p>
       </div>
     </div>

617f4cd21bb579ece2e7a243b2aecef725855627
Amrit Pandey <pandeyamrit714@gmail.com>
2024-04-05 13:44:22 +0545

    tabs ui fix

--- apps/rahat-ui/src/sections/projects/el/vouchers/voucher.view.tsx
+++ apps/rahat-ui/src/sections/projects/el/vouchers/voucher.view.tsx
@@ -8,7 +8,6 @@ import {
   TabsList,
   TabsTrigger,
 } from '@rahat-ui/shadcn/src/components/ui/tabs';
-import { Card } from '@rahat-ui/shadcn/src/components/ui/card';
 import { FreeTransactionTable } from './free.transactions.table';
 import { FreeHoldersTable } from './free.holder.table';
 import { DiscountTransactionTable } from './discount.transactions.table';
@@ -57,26 +56,22 @@ const VoucherView = () => {
               <FreeVoucherInfo data={projectVoucher} />
               <DiscountVoucherInfo data={projectVoucher} />
 
-              <div className="mt-2 mr-2 ml-2 w-full">
+              <div className="m-2">
                 <Tabs defaultValue="free">
-                  <div className="flex justify-between items-center">
-                    <Card className="rounded h-14 w-full mr-2 flex items-center justify-between">
-                      <TabsList className="gap-2">
-                        <TabsTrigger value="free">
-                          <div>Free Vouchers Transaction History</div>
-                        </TabsTrigger>
-                        <TabsTrigger value="discount">
-                          Discount Vouchers Transaction History
-                        </TabsTrigger>
-                      </TabsList>
-                    </Card>
-                  </div>
+                  <TabsList className="w-full grid grid-cols-2 border h-auto">
+                    <TabsTrigger value="free">
+                      Free Vouchers Transaction History
+                    </TabsTrigger>
+                    <TabsTrigger value="discount">
+                      Discount Vouchers Transaction History
+                    </TabsTrigger>
+                  </TabsList>
                   <TabsContent value="free">
                     <div className="grid grid-cols-3 gap-2">
                       <div className="col-span-2 h-full">
                         <FreeTransactionTable />
                       </div>
-                      <div className="h-full">
+                      <div>
                         <FreeHoldersTable data={data?.eyeVoucherOwners} />
                       </div>
                     </div>

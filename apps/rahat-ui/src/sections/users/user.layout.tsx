f11b8b2a4f556ef314c87569b1e958348cea4888
Amrit Pandey <pandeyamrit714@gmail.com>
2024-04-09 17:31:49 +0545

    panels overlapping issue fix

--- apps/rahat-ui/src/sections/users/user.layout.tsx
+++ apps/rahat-ui/src/sections/users/user.layout.tsx
@@ -5,7 +5,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from '@rahat-ui/shadcn/src/components/ui/resizable';
-import { ScrollArea } from '@rahat-ui/shadcn/src/components/ui/scroll-area';
+// import { ScrollArea } from '@rahat-ui/shadcn/src/components/ui/scroll-area';
 import { FC } from 'react';
 import { NavItem } from './nav-items.types';
 import UserNavView from './user.nav.view';
@@ -18,8 +18,9 @@ type UserLayoutProps = {
 const UserLayout: FC<UserLayoutProps> = ({ children, menuItems }) => {
   const renderResizablePanel = (children: React.ReactNode, index?: number) => {
     return (
-      <ResizablePanel key={index}>
-        <ScrollArea className="h-[calc(100vh-66px)]">{children}</ScrollArea>
+      <ResizablePanel minSize={30} key={index}>
+        {children}
+        {/* <ScrollArea className="h-[calc(100vh-66px)]">{children}</ScrollArea> */}
       </ResizablePanel>
     );
   };

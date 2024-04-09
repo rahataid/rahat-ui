514934c83caf12c2d1b3d687950764ae3a53480d
manjik-rumsan <manjik.shrestha@rumsan.com>
2024-04-09 16:13:06 +0545

    Update chain settings in settings.service.ts and settings.store.ts

--- libs/query/src/lib/settings/settings.store.ts
+++ libs/query/src/lib/settings/settings.store.ts
@@ -16,36 +16,22 @@ export type AppSettings = AppSettingsState & AppSettingsAction;
 
 export const initialAppSettings: AppSettingsState = {
   chainSettings: {
-    id: 80001,
-    name: 'Rahat',
-    nativeCurrency: { name: 'Rahat', symbol: 'Rs.', decimals: 18 },
+    id: Number(process.env['NEXT_PUBLIC_CHAIN_ID']) || 8888,
+    name: process.env['NEXT_PUBLIC_CHAIN_NAME'] || 'Rahat',
+    nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
     rpcUrls: {
       default: {
         http: [
-          'https://polygon-mumbai.infura.io/v3/627efc2e63b5449eaf60728ea083fa9d',
-          // 'wss://billowing-long-ensemble.arbitrum-sepolia.quiknode.pro/e0c76079c7d67ed114812420ba1d4472a30c93fa',
+          process.env['NEXT_PUBLIC_CHAIN_URL'] || 'http://localhost:8888'
         ],
       },
     },
     blockExplorers: {
       default: { name: 'Etherscan', url: 'https://etherscan.io' },
     },
-    contracts: {
-      ensRegistry: {
-        address: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-      },
-      ensUniversalResolver: {
-        address: '0xE4Acdd618deED4e6d2f03b9bf62dc6118FC9A4da',
-        blockCreated: 16773775,
-      },
-      multicall3: {
-        address: '0xca11bde05977b3631167028862be2a173976ca11',
-        blockCreated: 14353601,
-      },
-    },
   },
   subGraphUrl:
-    'https://api.studio.thegraph.com/query/42205/el-dev/version/latest',
+    process.env['NEXT_PUBLIC_SUBGRAPH_URL'] || "http://localhost:8000/subgraphs/name/rahat/el",
 };
 
 export const useSettingsStore = zustandStore<AppSettings>(

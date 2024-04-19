8a073be62f688d9776e6c068a178ed99b8b0d948
Raghav Kattel <raghav.kattel@rumsan.net>
2024-04-19 14:26:40 +0545

    Refactor project layout components and fix project settings issues

--- apps/rahat-ui/src/providers/service.provider.tsx
+++ apps/rahat-ui/src/providers/service.provider.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-import {
-  useChainSettings,
-  useSettingsStore,
-  useSubGraphUrlSettings,
-} from '@rahat-ui/query';
+import { useSettingsStore } from '@rahat-ui/query';
 import { useCommunicationQuery } from '@rumsan/communication-query';
 import { CommunicationService } from '@rumsan/communication/services/communication.client';
 import {
@@ -34,8 +30,6 @@ export function ServiceProvider({ children }: ServiceProviderProps) {
     queryClient: commsQueryClient,
     setQueryClient: setCommsQueryClient,
   } = useCommunicationQuery();
-
-  useSubGraphUrlSettings();
 
   const chainSettings = useSettingsStore((s) => s.chainSettings);
   const rsService = useMemo(

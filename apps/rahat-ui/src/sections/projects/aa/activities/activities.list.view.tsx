8cb390d9a464db999e4d752c4eb320bb060a3caf
Amrit Pandey <amrit@rumsan.net>
2024-04-22 01:50:55 +0545

    list aa->activities->demo data (query & integration)

--- apps/rahat-ui/src/sections/projects/aa/activities/activities.list.view.tsx
+++ apps/rahat-ui/src/sections/projects/aa/activities/activities.list.view.tsx
@@ -8,16 +8,18 @@ import {
 } from '@rahat-ui/shadcn/src/components/ui/resizable';
 import { useSecondPanel } from 'apps/rahat-ui/src/providers/second-panel-provider';
 import ActivitiesPhaseCard from './activities.phase.card';
+import { useActivities, useActivitiesFieldStore } from '@rahat-ui/query';
 
 export default function ActivitiesList() {
   const { secondPanel } = useSecondPanel();
-
+  useActivities('45606343-e6f5-475f-a2b3-f31d6ab10733');
+  const activities = useActivitiesFieldStore((state) => state.demoActivities);
   return (
     <>
       <ActivitiesPhaseCard />
       <ResizablePanelGroup className="bg-secondary" direction="horizontal">
         <ResizablePanel minSize={50}>
-          <ActivitiesTable />
+          <ActivitiesTable activitiesData={activities?.data} />
         </ResizablePanel>
         {secondPanel && (
           <>
--- libs/query/src/lib/aa/activities/activities.field.store.ts
+++ libs/query/src/lib/aa/activities/activities.field.store.ts
@@ -1,46 +1,49 @@
-import { localStore, zustandStore } from "@rumsan/react-query";
+import { localStore, zustandStore } from '@rumsan/react-query';
 
 interface I {
-    id: number;
-    uuid: string;
-    name: string;
-    isDeleted: boolean;
-    createdAt: string;
+  id: number;
+  uuid: string;
+  name: string;
+  isDeleted: boolean;
+  createdAt: string;
 }
 
 const initialStore = {
-    categories: [],
-    phases: [],
-    hazardTypes: [],
-}
+  categories: [],
+  phases: [],
+  hazardTypes: [],
+  demoActivities: [],
+};
 
 type ActivitiesFieldState = {
-    categories: I[];
-    phases: I[];
-    hazardTypes: I[];
-}
+  categories: I[];
+  phases: I[];
+  hazardTypes: I[];
+  demoActivities: any;
+};
 
 type ActivitiesFieldStateAction = {
-    setCategories: (categories: I[]) => void;
-    setPhases: (phases: I[]) => void;
-    setHazardTypes: (hazardTypes: I[]) => void
-
-}
+  setCategories: (categories: I[]) => void;
+  setPhases: (phases: I[]) => void;
+  setHazardTypes: (hazardTypes: I[]) => void;
+  setDemoActivities: (demoActivities: any) => void;
+};
 
 type ActivitiesFieldStore = ActivitiesFieldState & ActivitiesFieldStateAction;
 
 export const useActivitiesFieldStore = zustandStore<ActivitiesFieldStore>(
-    (set) => ({
-        ...initialStore,
-        setCategories: (categories) => set({ categories }),
-        setPhases: (phases) => set({ phases }),
-        setHazardTypes: (hazardTypes) => set({ hazardTypes })
-    }),
-    {
-        devtoolsEnabled: true,
-        persistOptions: {
-            name: 'aaActivitiesFieldStore',
-            storage: localStore,
-        }
-    }
-)
+  (set) => ({
+    ...initialStore,
+    setCategories: (categories) => set({ categories }),
+    setPhases: (phases) => set({ phases }),
+    setHazardTypes: (hazardTypes) => set({ hazardTypes }),
+    setDemoActivities: (demoActivities) => set({ demoActivities }),
+  }),
+  {
+    devtoolsEnabled: true,
+    persistOptions: {
+      name: 'aaActivitiesFieldStore',
+      storage: localStore,
+    },
+  },
+);

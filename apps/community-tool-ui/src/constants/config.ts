9ea60022db5aaa6ef9a8fd7d2d48c1e3c4ea2fcb
karun-rumsan <karun@rumsan.net>
2024-04-24 17:45:17 +0545

    fix:login

--- apps/community-tool-ui/src/constants/config.ts
+++ apps/community-tool-ui/src/constants/config.ts
@@ -2,3 +2,5 @@ export const mapboxBasicConfig = {
   mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '',
   minZoom: 1,
 };
+
+export const DEBUG_MODE = process.env.NEXT_PUBLIC_DEBUG_MODE || false;

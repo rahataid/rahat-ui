bd48b26c7a891bafe4d9643861ddd6809aa6c4dc
Amrit Pandey <amrit@rumsan.net>
2024-04-26 10:20:51 +0545

    ts ignore

--- libs/shadcn/src/components/maps/container/index.tsx
+++ libs/shadcn/src/components/maps/container/index.tsx
@@ -1,4 +1,3 @@
-import { ReactNode } from 'react';
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
@@ -11,19 +10,14 @@ export const THEMES = {
   satelliteStreets: 'mapbox://styles/mapbox/satellite-streets-v11',
 };
 
-interface StyledMapContainerProps {
-  children?: ReactNode;
-}
-
-export const StyledMapContainer: FC<StyledMapContainerProps> = styled('div')(
-  () => ({
-    zIndex: 0,
-    height: 560,
-    overflow: 'hidden',
-    position: 'relative',
-    borderRadius: 8,
-    '& .mapboxgl-ctrl-logo, .mapboxgl-ctrl-bottom-right': {
-      display: 'none',
-    },
-  }),
-);
+// @ts-ignore
+export const StyledMapContainer: FC = styled('div')(() => ({
+  zIndex: 0,
+  height: 560,
+  overflow: 'hidden',
+  position: 'relative',
+  borderRadius: 8,
+  '& .mapboxgl-ctrl-logo, .mapboxgl-ctrl-bottom-right': {
+    display: 'none',
+  },
+}));

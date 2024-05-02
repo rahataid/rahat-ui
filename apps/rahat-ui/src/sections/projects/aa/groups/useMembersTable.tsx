072e98db1a11b8355cc76bd977e0b1680d3b4ba8
Amrit Pandey <amrit@rumsan.net>
2024-05-02 13:12:22 +0545

    add stakeholders groups view page ui

---
+++ apps/rahat-ui/src/sections/projects/aa/groups/useMembersTable.tsx
@@ -0,0 +1,67 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { Checkbox } from '@rahat-ui/shadcn/src/components/ui/checkbox';
+import { IStakeholdersItem } from 'apps/rahat-ui/src/types/stakeholders';
+
+export default function useMembersTableColumn() {
+    const columns: ColumnDef<IStakeholdersItem>[] = [
+        {
+            id: 'select',
+            header: ({ table }) => (
+                <Checkbox
+                    checked={
+                        table.getIsAllPageRowsSelected() ||
+                        (table.getIsSomePageRowsSelected() && 'indeterminate')
+                    }
+                    onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+        },
+        {
+            accessorKey: 'name',
+            header: 'Name',
+            cell: ({ row }) => <div>{row.getValue('name')}</div>,
+        },
+        {
+            accessorKey: 'phone',
+            header: 'Phone',
+            cell: ({ row }) => <div>{row.getValue('phone') || "N/A"}</div>,
+        },
+        {
+            accessorKey: 'email',
+            header: 'Email Address',
+            cell: ({ row }) => <div>{row.getValue('email') || "N/A"}</div>,
+        },
+        {
+            accessorKey: 'designation',
+            header: 'Designation',
+            cell: ({ row }) => <div>{row.getValue('designation')}</div>,
+        },
+        {
+            accessorKey: 'organization',
+            header: 'Organization',
+            cell: ({ row }) => <div>{row.getValue('organization')}</div>,
+        },
+        {
+            accessorKey: 'district',
+            header: 'District',
+            cell: ({ row }) => <div>{row.getValue('district')}</div>,
+        },
+        {
+            accessorKey: 'municipality',
+            header: 'Municipality',
+            cell: ({ row }) => <div>{row.getValue('municipality')}</div>,
+        },
+    ];
+
+    return columns;
+}

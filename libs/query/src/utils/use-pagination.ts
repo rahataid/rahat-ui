0f4fba4297614ce9749944e6a3bacfd8671239be
anupama-rumsan <anupama@rumsan.net>
2024-05-17 12:59:05 +0545

    action: ui fixes

--- libs/query/src/utils/use-pagination.ts
+++ libs/query/src/utils/use-pagination.ts
@@ -40,7 +40,7 @@ type usePaginationReturn = {
 
 export const usePagination = (): usePaginationReturn => {
   const [pagination, setPagination] = useState<Pagination>(
-    hashStorage.getItem('pagination') || { page: 1, perPage: 50 },
+    hashStorage.getItem('pagination') || { page: 1, perPage: 10 },
   );
   const [filters, setFilters] = useState<{ [key: string]: string }>(
     hashStorage.getItem('filters') || {},

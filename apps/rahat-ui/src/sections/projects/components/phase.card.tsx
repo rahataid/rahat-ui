aaf691b8930851b4ebf3b764918cb80f3eadafa4
Amrit Pandey <amrit@rumsan.net>
2024-05-20 18:39:57 +0545

    update

--- apps/rahat-ui/src/sections/projects/components/phase.card.tsx
+++ apps/rahat-ui/src/sections/projects/components/phase.card.tsx
@@ -3,17 +3,16 @@ import { Settings } from "lucide-react"
 
 type IProps = {
     path: string
-    initial: string
     name: string
 }
 
-export default function PhaseCard({ path, initial, name }: IProps) {
+export default function PhaseCard({ path, name }: IProps) {
     return (
         <Link href={path}>
             <div className="relative w-48 bg-card rounded p-4 grid gap-3 place-items-center">
                 <Settings size={20} className="absolute top-4 right-4 text-muted-foreground" />
                 <div className="w-20 h-20 rounded-full bg-secondary grid place-items-center">
-                    <p className="font-semibold text-4xl text-muted-foreground">{initial}</p>
+                    <p className="font-semibold text-4xl text-muted-foreground">{name.charAt(0)}</p>
                 </div>
                 <p className="font-medium">{name}</p>
             </div>

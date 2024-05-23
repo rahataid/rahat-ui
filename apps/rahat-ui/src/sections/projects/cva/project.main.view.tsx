f368cff58fd7e9c0d2aa0dfbbfcbc877e64fcc1b
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-05-23 16:13:25 +0545

    project dashboard update

--- apps/rahat-ui/src/sections/projects/cva/project.main.view.tsx
+++ apps/rahat-ui/src/sections/projects/cva/project.main.view.tsx
@@ -1,9 +1,14 @@
+import { ScrollArea } from '@rahat-ui/shadcn/src/components/ui/scroll-area';
 import ProjectDetails from './project.detail';
 
 const ProjectMainView = () => {
   return (
     <>
-      <ProjectDetails />
+      <div className="p-2 bg-secondary">
+        <ScrollArea className="h-[calc(100vh-80px)]">
+          <ProjectDetails />
+        </ScrollArea>
+      </div>
     </>
   );
 };

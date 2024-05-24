20e3369eaf2006a909f7bf748d07eca3918de441
Amrit Pandey <amrit@rumsan.net>
2024-05-24 15:33:48 +0545

    ui fixes: aa dashboard

--- apps/rahat-ui/src/sections/projects/aa/project.info.tsx
+++ apps/rahat-ui/src/sections/projects/aa/project.info.tsx
@@ -3,27 +3,63 @@ import Image from 'next/image';
 import { Project } from '@rahataid/sdk/project/project.types';
 import EditButton from '../components/edit.btn';
 import DeleteButton from '../components/delete.btn';
+import Back from '../components/back';
 
 type ProjectInfoProps = {
   project: Project;
 };
 
-const ProjectInfo: FC<ProjectInfoProps> = ({ project }) => {
-  const onDelete = () => { }
+const ProjectInfoCard = ({
+  status,
+  type,
+  description,
+}: {
+  status: string;
+  type: string;
+  description: string;
+}) => {
   return (
-    <>
-      <div className='flex justify-between items-center'>
-        <h1 className='font-semibold text-xl'>{project?.name}</h1>
-        <div className="flex gap-4 items-center">
-          <EditButton path='/' />
-          <DeleteButton name='project' handleContinueClick={onDelete} />
+    <div className="w-full rounded bg-card p-4 shadow">
+      <div className="flex flex-wrap gap-10 md:gap-32">
+        <div>
+          <p className="font-medium text-primary">{status}</p>
+          <p className="font-light">Status</p>
+        </div>
+        <div>
+          <p className="font-medium text-primary">{type}</p>
+          <p className="font-light">Type</p>
         </div>
       </div>
-      <div className='grid grid-cols-5'>
-        <p className='col-span-3'>{project?.description}</p>
+      <div>
+        <p className="mt-4">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+const ProjectInfo: FC<ProjectInfoProps> = ({ project }) => {
+  const onDelete = () => {};
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <Back path="/projects" />
+          <h1 className="font-semibold text-xl">{project?.name}</h1>
+        </div>
+        <div className="flex gap-4 items-center">
+          <EditButton path="/" />
+          <DeleteButton name="project" handleContinueClick={onDelete} />
+        </div>
+      </div>
+      <div className="flex gap-4 justify-between">
+        <ProjectInfoCard
+          status={project?.status ?? 'N/A'}
+          type={project?.type ?? 'N/A'}
+          description={project?.description ?? 'N/A'}
+        />
         <Image
-          className="col-span-2 w-full rounded"
-          src='/svg/aa-project.svg'
+          className="rounded"
+          src="/svg/aa-project.svg"
           alt="project"
           height={100}
           width={500}

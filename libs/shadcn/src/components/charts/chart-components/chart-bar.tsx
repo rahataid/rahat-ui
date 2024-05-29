cba8a965676f285d3f7fdeab9e314fa86448c2ce
karun-rumsan <gyawalikarrun@gmail.com>
2024-05-29 14:52:02 +0545

    update bar charts component with props and optional

--- libs/shadcn/src/components/charts/chart-components/chart-bar.tsx
+++ libs/shadcn/src/components/charts/chart-components/chart-bar.tsx
@@ -8,16 +8,61 @@ import useChart from '../use-chart';
 type Props = {
   series: number[];
   categories: string[];
+  width?: number | string;
+  height?: number | string;
+  horizontal?: boolean;
 };
 
-export default function ChartBar({ series, categories = [] }: Props) {
+export default function ChartBar({
+  series,
+  categories,
+  width = 400,
+  height = 400,
+  horizontal = false,
+}: Props) {
   const chartOptions = useChart({
     stroke: { show: false },
+    chart: {
+      type: 'bar',
+      height: 350,
+    },
+
     plotOptions: {
-      bar: { horizontal: false, barHeight: '30%' },
+      bar: {
+        horizontal: horizontal,
+        barHeight: '60%',
+        borderRadius: 4,
+        borderRadiusApplication: 'end',
+      },
     },
     xaxis: {
       categories,
+      labels: {
+        show: true,
+      },
+      axisBorder: {
+        show: true,
+      },
+      axisTicks: {
+        show: false,
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E9FF',
+            colorTo: '#BED2FF',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          },
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: (value: number) => `${value}`,
+      },
     },
   });
 
@@ -31,7 +76,8 @@ export default function ChartBar({ series, categories = [] }: Props) {
         },
       ]}
       options={chartOptions}
-      height={320}
+      height={height}
+      width={width}
     />
   );
 }

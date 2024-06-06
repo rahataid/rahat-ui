fd7a96a8ac9654f565d06161b4cd232fdc762dce
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-06 15:49:46 +0545

    disburse page and table

---
+++ apps/rahat-ui/src/sections/projects/c2c/disbursement/disburse.table.tsx
@@ -0,0 +1,259 @@
+'use client';
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import { Checkbox } from '@rahat-ui/shadcn/src/components/ui/checkbox';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/dropdown-menu';
+import { Input } from '@rahat-ui/shadcn/src/components/ui/input';
+import { ScrollArea } from '@rahat-ui/shadcn/src/components/ui/scroll-area';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@rahat-ui/shadcn/src/components/ui/table';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { ChevronDown, Eye } from 'lucide-react';
+import * as React from 'react';
+import { useDisburseTable } from './useDisburseTable';
+
+const data = [
+  {
+    date: '0x273d...B6Ed',
+    type: 'Project Balance',
+    totalAmount: '30000',
+    status: 'Completed',
+  },
+  {
+    date: '0x273d...B6Ed',
+    type: 'Wallet',
+    totalAmount: '50000',
+    status: 'Completed',
+  },
+  {
+    date: '0x273d...B6Ed',
+    type: 'Multisig Wallet',
+    totalAmount: '100000',
+    status: 'Pending',
+  },
+  {
+    date: '0x374f...A6Fg',
+    type: 'Project Balance',
+    totalAmount: '20000',
+    status: 'Completed',
+  },
+  {
+    date: '0x374f...A6Fg',
+    type: 'Wallet',
+    totalAmount: '45000',
+    status: 'Completed',
+  },
+  {
+    date: '0x374f...A6Fg',
+    type: 'Multisig Wallet',
+    totalAmount: '80000',
+    status: 'Pending',
+  },
+  {
+    date: '0x485g...C7Hh',
+    type: 'Project Balance',
+    totalAmount: '25000',
+    status: 'Completed',
+  },
+  {
+    date: '0x485g...C7Hh',
+    type: 'Wallet',
+    totalAmount: '40000',
+    status: 'Completed',
+  },
+  {
+    date: '0x485g...C7Hh',
+    type: 'Multisig Wallet',
+    totalAmount: '90000',
+    status: 'Pending',
+  },
+  {
+    date: '0x596h...D8Ii',
+    type: 'Project Balance',
+    totalAmount: '35000',
+    status: 'Completed',
+  },
+  {
+    date: '0x596h...D8Ii',
+    type: 'Wallet',
+    totalAmount: '60000',
+    status: 'Completed',
+  },
+  {
+    date: '0x596h...D8Ii',
+    type: 'Multisig Wallet',
+    totalAmount: '110000',
+    status: 'Pending',
+  },
+];
+
+export function DisburseTable() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  // const [data, setData] = React.useState([]);
+  const columns = useDisburseTable();
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter date..."
+          value={(table.getColumn('date')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('date')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded h-[calc(100vh-180px)] bg-card">
+        <Table>
+          <ScrollArea className="h-table1">
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && 'selected'}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </ScrollArea>
+        </Table>
+      </div>
+      <div className="sticky bottom-0 flex items-center justify-end space-x-4 px-4 py-1 border-t-2 bg-card">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/rahat-ui/src/sections/projects/c2c/disbursement/useDisburseTable.tsx
@@ -0,0 +1,102 @@
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import { Checkbox } from '@rahat-ui/shadcn/src/components/ui/checkbox';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/dropdown-menu';
+import { ColumnDef } from '@tanstack/react-table';
+import { Eye } from 'lucide-react';
+
+export type Disbursements = {
+  date: string;
+  type: string;
+  totalAmount: string;
+  status: string;
+};
+// const { id } = useParams();
+// const router = useRouter();
+
+export const useDisburseTable = () => {
+  const columns: ColumnDef<Disbursements>[] = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && 'indeterminate')
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: 'date',
+      header: 'Date',
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue('date')}</div>
+      ),
+    },
+    {
+      accessorKey: 'type',
+      header: 'Type',
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue('type')}</div>
+      ),
+    },
+    {
+      accessorKey: 'totalAmount',
+      header: 'Total Amount',
+      cell: ({ row }) => {
+        return <div>{row.getValue('totalAmount')}</div>;
+      },
+    },
+    {
+      accessorKey: 'status',
+      header: 'Status',
+      cell: ({ row }) => <div>{row.getValue('status')}</div>,
+    },
+    {
+      id: 'actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        const payment = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <Eye className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              {/* <DropdownMenuItem
+                onClick={() =>
+                  router.push(`/projects/c2c/${id}/disbursements/1`)
+                }
+              >
+                View Details
+              </DropdownMenuItem> */}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+  return columns;
+};

304281391917454caed258d027ff94bc09248db3
karun-rumsan <gyawalikarrun@gmail.com>
2024-06-06 11:34:27 +0545

    ui to list imported community benef in rahat ui

---
+++ apps/rahat-ui/src/sections/community-beneficiary/useNavItems.tsx
@@ -0,0 +1,27 @@
+import { usePagination } from '@rahat-ui/query';
+import { KanbanSquare } from 'lucide-react';
+import { paths } from '../../routes/paths';
+import { NavItem } from './nav-items.types';
+
+export const useCommunityBeneficiaryNavItems = () => {
+  const { setFilters } = usePagination();
+
+  const menuItems: NavItem[] = [
+    {
+      title: 'Beneficiaries',
+      component: (
+        <div className="flex justify-between items-center border-6 w-full">
+          <h1 className="font-semibold text-xl text-primary">Beneficiaries</h1>
+        </div>
+      ),
+      children: [
+        {
+          title: 'Temporary Beneficiaries',
+          icon: <KanbanSquare size={18} strokeWidth={1.5} />,
+          path: paths.dashboard.communitybeneficiary,
+        },
+      ],
+    },
+  ];
+  return menuItems;
+};

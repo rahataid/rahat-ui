c2a25b5b560aca54edbc3c5afa2083ffe62c75ba
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-10 12:08:29 +0545

    clean up campaign page

--- apps/rahat-ui/src/sections/communications/components/communication.summary.tsx
+++ apps/rahat-ui/src/sections/communications/components/communication.summary.tsx
@@ -1,5 +1,5 @@
 import DataCard from 'apps/rahat-ui/src/components/dataCard';
-import { BadgeCent, HeartHandshake, Home, Users } from 'lucide-react';
+import { HeartHandshake, Home, Users } from 'lucide-react';
 
 const CommunicationSummary = (statsData: any, isLoading: boolean) => {
   return (
@@ -8,21 +8,18 @@ const CommunicationSummary = (statsData: any, isLoading: boolean) => {
         className=""
         title="Total Campaign Sent"
         number={statsData?.statsData?.total || 'N/A'}
-        subTitle=""
         Icon={Users}
       />
       <DataCard
         className=""
         title="Successful Campaign"
         number={statsData?.statsData?.success || 'N/A'}
-        subTitle=""
         Icon={Home}
       />
       <DataCard
         className=""
         title="Failed Campaign"
         number={statsData?.statsData?.failed || 'N/A'}
-        subTitle=""
         Icon={HeartHandshake}
       />
     </div>

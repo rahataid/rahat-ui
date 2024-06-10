4d9672dc13dff3ac8e312379d424572f763a9918
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-10 12:07:57 +0545

    campaign upage update

--- apps/rahat-ui/src/sections/communications/components/communication.summary.tsx
+++ apps/rahat-ui/src/sections/communications/components/communication.summary.tsx
@@ -1,27 +1,27 @@
 import DataCard from 'apps/rahat-ui/src/components/dataCard';
 import { BadgeCent, HeartHandshake, Home, Users } from 'lucide-react';
 
-const CommunicationSummary = (statsData:any,isLoading:boolean) => {
+const CommunicationSummary = (statsData: any, isLoading: boolean) => {
   return (
-      <div className=" grid md:grid-cols-3 gap-2 mb-2">
+    <div className=" grid md:grid-cols-3 gap-2 mb-2">
       <DataCard
         className=""
-        title="Total Message Sent"
-        number={statsData?.statsData?.total}
+        title="Total Campaign Sent"
+        number={statsData?.statsData?.total || 'N/A'}
         subTitle=""
         Icon={Users}
       />
       <DataCard
         className=""
-        title="Successful Messages"
-        number={statsData?.statsData?.success}
+        title="Successful Campaign"
+        number={statsData?.statsData?.success || 'N/A'}
         subTitle=""
         Icon={Home}
       />
       <DataCard
         className=""
-        title="Failed Messages"
-        number={statsData?.statsData?.failed}
+        title="Failed Campaign"
+        number={statsData?.statsData?.failed || 'N/A'}
         subTitle=""
         Icon={HeartHandshake}
       />

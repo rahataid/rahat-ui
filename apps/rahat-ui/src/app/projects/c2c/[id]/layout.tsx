0c065d748bb7a2a87659af3fd796bd85a69100e1
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-12 18:44:29 +0545

    chore: Add useProjectSafeWalletSettings hook to layout.tsx files

--- apps/rahat-ui/src/app/projects/c2c/[id]/layout.tsx
+++ apps/rahat-ui/src/app/projects/c2c/[id]/layout.tsx
@@ -3,6 +3,7 @@
 import {
   PROJECT_SETTINGS_KEYS,
   useProjectContractSettings,
+  useProjectSafeWalletSettings,
   useProjectSettingsStore,
   useProjectSubgraphSettings,
 } from '@rahat-ui/query';
@@ -25,6 +26,7 @@ export default function ProjectLayoutRoot({
   const uuid = useParams().id as UUID;
   useProjectSubgraphSettings(uuid);
   useProjectContractSettings(uuid);
+  useProjectSafeWalletSettings(uuid);
 
   const subgraphSettings = useProjectSettingsStore(
     (s) => s.settings?.[uuid]?.[PROJECT_SETTINGS_KEYS.SUBGRAPH]?.url,

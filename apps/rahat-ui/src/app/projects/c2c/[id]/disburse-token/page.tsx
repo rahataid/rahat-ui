418db51a6ea169d78841bd02cc6d52d008942330
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-12 14:49:55 +0545

    disburse flow

---
+++ apps/rahat-ui/src/app/projects/c2c/[id]/disburse-token/page.tsx
@@ -0,0 +1,7 @@
+import DisburseFlowPage from 'apps/rahat-ui/src/sections/projects/c2c/components/disburse-flow/disburse-flow-page';
+
+const DisburseToken = () => {
+  return <DisburseFlowPage selectedBeneficiaries={[]} />;
+};
+
+export default DisburseToken;

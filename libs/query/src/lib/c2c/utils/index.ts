cd4e5294dcc29216055716a654cf7c431ec535db
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-14 11:16:17 +0545

    feat: Refactor mergeTransactions function to merge tokenReceiveds and transferProcesseds arrays

---
+++ libs/query/src/lib/c2c/utils/index.ts
@@ -0,0 +1,41 @@
+export const mergeTransactions = async (transactionsObj: any) => {
+  console.log({ transactionsObj });
+  const { tokenReceiveds, transferProcesseds } = transactionsObj;
+
+  // Combine the arrays
+  const mergedTransactions: any = [
+    ...tokenReceiveds.map((transaction: any) => ({
+      ...transaction,
+      topic: 'Received',
+      date: new Date(
+        parseInt(transaction.blockTimestamp) * 1000,
+      ).toLocaleDateString('en-US', {
+        timeZone: 'UTC',
+      }),
+    })),
+    ...transferProcesseds.map((transaction: any) => ({
+      ...transaction,
+      to: transaction._beneficiary,
+      amount: transaction._amount,
+      token: transaction._tokenAddress,
+      topic: 'Disbursed',
+      date: new Date(
+        parseInt(transaction.blockTimestamp) * 1000,
+      ).toLocaleDateString('en-US', {
+        timeZone: 'UTC',
+      }),
+    })),
+  ];
+
+  // Optionally, sort by timestamp if needed
+  mergedTransactions.sort(
+    (a: any, b: any) =>
+      new Date(a.blockTimestamp).getTime() -
+      new Date(b.blockTimestamp).getTime(),
+  );
+  const cleanedTransactions = mergedTransactions.map(
+    ({ blockTimestamp, __typename, ...rest }) => rest,
+  );
+
+  return cleanedTransactions;
+};

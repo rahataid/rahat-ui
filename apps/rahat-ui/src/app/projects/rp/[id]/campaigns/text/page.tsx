7f6d0befa98dbcf17f35185a2764bd89831632c3
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-18 20:47:05 +0545

    ```text feat: Add RP-related files and exports
    This commit adds new files and exports related to the RP (Resource Provider) functionality. The following changes were made:
    
    - Added new files in the `libs/query/src/lib/rp` directory: `project-actions/index.ts`, `subgraph/stores/index.ts`, `index.ts`
    - Added new files in the `libs/query/src/lib/rp/project-actions` directory: `actions.query.ts`
    - Added new files in the `libs/query/src/lib/rp/subgraph` directory: `stores/rp-project.store.ts`
    - Added new files in the `libs/query/src/lib/rp/contracts/hooks` directory: `index.ts`
    - Added new files in the `libs/query/src/lib/rp/contracts/generated-hooks` directory: `index.ts`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp` directory: `vendors/index.tsx`, `index.ts`, `beneficiary/index.tsx`, `transactions/index.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/campaigns` directory: `add/index.ts`, `text/text.view.tsx`, `index.tsx`, `voice/voice.view.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/vendors` directory: `vendors.view.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/beneficiary` directory: `beneficiary.view.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/transactions` directory: `transactions.view.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/campaigns/voice` directory: `voice.view.tsx`
    - Added new files in the `apps/rahat-ui/src/sections/projects/rp/campaigns/text` directory: `text.view.tsx`
    - Added new files in the `apps/rahat-ui/src/app/projects/rp/[id]/vendors` directory: `page.tsx`
    - Added new files in the `apps/rahat-ui/src/app/projects/rp/[id]/beneficiary` directory: `page.tsx`
    
    These changes are necessary to support the RP functionality in the project.

---
+++ apps/rahat-ui/src/app/projects/rp/[id]/campaigns/text/page.tsx
@@ -0,0 +1,7 @@
+import { RPCampaignTextView } from 'apps/rahat-ui/src/sections/projects/rp/campaigns';
+
+const Page = () => {
+  return <RPCampaignTextView />;
+};
+
+export default Page;
---
+++ apps/rahat-ui/src/sections/projects/rp/beneficiary/beneficiary.view.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import BeneficiaryTable from './beneficiary.table';
+
+export default function BeneficiaryView() {
+  return <BeneficiaryTable />;
+}

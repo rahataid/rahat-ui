338def3df63746b2d9f9a823a9c9f34185e9f845
raghav-rumsan <raghav.kattel@rumsan.net>
2024-06-20 15:24:11 +0545

    action: changes

--- apps/rahat-ui/src/sections/dashboard/dashboard.view.tsx
+++ apps/rahat-ui/src/sections/dashboard/dashboard.view.tsx
@@ -17,6 +17,7 @@ export default function DashboardView() {
       data: `${process.env.NEXT_PUBLIC_API_HOST_URL}/v1/beneficiaries/stats`,
     },
   ];
+
   return (
     <div className="bg-secondary">
       <Tabs defaultValue="list">

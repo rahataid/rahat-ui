5be7046fc6600f2e74e8d7d3c0b831aba60bef3d
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-27 16:12:13 +0545

    adding voice recording

--- apps/rahat-ui/src/sections/projects/rp/campaigns/voice/campaign.voice.add.tsx
+++ apps/rahat-ui/src/sections/projects/rp/campaigns/voice/campaign.voice.add.tsx
@@ -10,8 +10,23 @@ import {
   DrawerTrigger,
 } from '@rahat-ui/shadcn/src/components/ui/drawer';
 import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
-import { Card, CardContent } from '@rahat-ui/shadcn/src/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@rahat-ui/shadcn/src/components/ui/card';
 import { Plus } from 'lucide-react';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/tabs';
+import { Label } from '@rahat-ui/shadcn/src/components/ui/label';
+import { Input } from '@rahat-ui/shadcn/src/components/ui/input';
+import { AudioRecorder } from '@rahat-ui/shadcn/src/components/ui/audioRecorder';
 
 const VoiceCampaignAddDrawer = () => {
   return (
@@ -25,18 +40,56 @@ const VoiceCampaignAddDrawer = () => {
           </CardContent>
         </Card>
       </DrawerTrigger>
-      <DrawerContent className="">
-        <div className="mx-auto">
+      <DrawerContent className="min-h-[600px]">
+        <div className="mx-auto my-auto w-[600px]">
           <DrawerHeader>
-            <DrawerTitle>Move Goal</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+            <DrawerTitle>Add Voice</DrawerTitle>
+            {/* <DrawerDescription>Set your daily activity goal.</DrawerDescription> */}
           </DrawerHeader>
-
-          <DrawerFooter>
-            <Button>Submit</Button>
+          <DrawerDescription>
+            <Tabs defaultValue="upload" className="w-[600px]">
+              <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="upload">Upload</TabsTrigger>
+                <TabsTrigger value="record">Record</TabsTrigger>
+              </TabsList>
+              <TabsContent value="upload">
+                <Card className="min-h-72">
+                  <CardHeader>
+                    <CardTitle>Upload</CardTitle>
+                    <CardDescription>
+                      Choose a voice file to upload.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <Label htmlFor="audio">Audio</Label>
+                      <Input id="audio" type="file" />
+                    </div>
+                  </CardContent>
+                </Card>
+              </TabsContent>
+              <TabsContent value="record">
+                <Card className="min-h-72">
+                  <CardHeader>
+                    <CardTitle>Record</CardTitle>
+                    <CardDescription>Record audio and submit.</CardDescription>
+                  </CardHeader>
+                  <CardContent className="space-y-2">
+                    <div className="my-10">
+                      <AudioRecorder />
+                    </div>
+                  </CardContent>
+                </Card>
+              </TabsContent>
+            </Tabs>
+          </DrawerDescription>
+          <DrawerFooter className="flex items-center justify-between">
             <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button className="w-full" variant="outline">
+                Cancel
+              </Button>
             </DrawerClose>
+            <Button className="w-full">Submit</Button>
           </DrawerFooter>
         </div>
       </DrawerContent>

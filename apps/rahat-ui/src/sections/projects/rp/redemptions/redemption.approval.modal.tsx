bc77ea809079d763a1414ba2e20c1638aa4836e7
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-06-28 16:17:07 +0545

    adding approve modal and history button in redemptions

---
+++ apps/rahat-ui/src/sections/projects/rp/redemptions/redemption.approval.modal.tsx
@@ -0,0 +1,53 @@
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/dialog';
+import { Input } from '@rahat-ui/shadcn/src/components/ui/input';
+import { Label } from '@rahat-ui/shadcn/src/components/ui/label';
+
+export function RedemptionApprovalModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="h-6 w-14 text-xs p-2">Approve</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[500px]">
+        <DialogHeader>
+          <DialogTitle>Approve</DialogTitle>
+          <DialogDescription>Approve redemptions.</DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Approve</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}

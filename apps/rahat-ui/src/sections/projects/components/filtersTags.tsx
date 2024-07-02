2660c269ffb0e9cec41532af27a9a875b21d4e31
Your Name <your.email@example.com>
2024-07-02 12:43:49 +0545

    dashboard map and rp filter

--- apps/rahat-ui/src/sections/projects/components/filtersTags.tsx
+++ apps/rahat-ui/src/sections/projects/components/filtersTags.tsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import { RxCrossCircled } from "react-icons/rx";
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import React from 'react';
+import { RxCrossCircled } from 'react-icons/rx';
 
-const FiltersTags = ({filters, setFilters, total}:any) => {
+const FiltersTags = ({ filters, setFilters, total }: any) => {
+  const filterArray = Object.entries(filters).map(([key, value]) => {
+    return { key, value };
+  });
 
-    const filterArray = Object.entries(filters).map(([key, value]) => {
-        return { key, value };
-      });
-
-    const handleFilterArrayChange = (key: string, value: string) => {
-        const { [key]: _, ...rest } = filters;
-        setFilters(rest);
-    }
-
+  const handleFilterArrayChange = (key: string, value: string) => {
+    const { [key]: _, ...rest } = filters;
+    setFilters(rest);
+  };
 
   return (
-    <div className="rounded-md border bg-card flex gap-8 py-2 px-4 text-sm mb-2 items-center">
-          <span className='text-primary'>{total} results found</span>
-          {
-            filterArray.map(
-            (filter) => <div className='flex items-center gap-2'>
-            {filter.key}: <span  onClick={() => handleFilterArrayChange(filter.key, filter.value)} className='cursor-pointer bg-primary py-1 px-2 text-white rounded text-xs flex items-center gap-2'>{filter.value} <RxCrossCircled /></span> 
-            </div>
-            )
-          }
-          
-        </div>
-  )
-}
+    <div className="rounded-md border bg-card flex py-2 px-4 text-sm mb-2 justify-between">
+      <div className="flex items-center  gap-8">
+        <span className="text-primary">{total} results found</span>
+        {filterArray.map((filter) => (
+          <div className="flex items-center gap-2">
+            {filter.key}:{' '}
+            <span
+              onClick={() => handleFilterArrayChange(filter.key, filter.value)}
+              className="cursor-pointer bg-primary py-1 px-2 text-white rounded text-xs flex items-center gap-2"
+            >
+              {filter.value} <RxCrossCircled />
+            </span>
+          </div>
+        ))}
+      </div>
+      <Button onClick={() => setFilters({})}>Clear filter</Button>
+    </div>
+  );
+};
 
-export default FiltersTags
+export default FiltersTags;

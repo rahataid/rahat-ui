cc12711b5b489badb53257b75b16eaf06ae721c8
amrit-rumsan <amrit@rumsan.net>
2024-07-03 15:38:42 +0545

    update : Edit User Profile

---
+++ apps/rahat-ui/src/sections/profile/edit.profile.info.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useUserEdit, useUserStore } from '@rumsan/react-query';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@rahat-ui/shadcn/src/components/ui/form';
+import { Input } from '@rahat-ui/shadcn/src/components/ui/input';
+import { PhoneInput } from '@rahat-ui/shadcn/src/components/ui/phone-input';
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import { isValidPhoneNumber } from 'react-phone-number-input';
+import { Wallet } from 'lucide-react';
+import { toast } from 'react-toastify';
+
+export default function EditUserProfile() {
+  const user = useUserStore((state) => state.user);
+  const userInfo = React.useMemo(() => user.data, [user]);
+
+  const router = useRouter();
+
+  const FormSchema = z.object({
+    name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
+    wallet: z.string().optional(),
+    phone: z
+      .string()
+      .optional()
+      .refine((val) => !val || isValidPhoneNumber(val), {
+        message: 'Invalid phone number',
+      }),
+    email: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: userInfo?.name || '',
+      wallet: userInfo?.wallet || '',
+      email: userInfo?.email,
+      phone: userInfo?.phone || '',
+    },
+  });
+
+  const editUser = useUserEdit();
+
+  const handleEditUserProfile = async (data: z.infer<typeof FormSchema>) => {
+    const payload = { uuid: userInfo?.uuid, data: data };
+    try {
+      await editUser.mutateAsync(payload);
+    } catch (e) {
+      console.error('Error updating user profile:', e);
+      toast.error('Error updating user profile');
+    }
+  };
+
+  React.useEffect(() => {
+    if (editUser.isSuccess) {
+      toast.success('User updated successfully');
+      setTimeout(() => {
+        router.push('/profile');
+      }, 1000);
+    }
+  }, [editUser.isSuccess]);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleEditUserProfile)}>
+        <div className="shadow-md p-6 rounded-sm bg-card">
+          <h1 className="text-lg font-semibold mb-6">Edit : User Profile</h1>
+          <div className="grid grid-cols-3 gap-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Phone</FormLabel>
+                    <FormControl>
+                      <PhoneInput placeholder="Phone" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input disabled placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="wallet"
+              render={({ field }) => {
+                return (
+                  <FormItem className="col-span-3">
+                    <FormLabel>Wallet Address</FormLabel>
+                    <FormControl>
+                      <div className="relative w-full">
+                        <Wallet className="absolute right-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                        <Input
+                          type="text"
+                          placeholder="Wallet Address"
+                          {...field}
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </div>
+          <div className="flex justify-end mt-8">
+            <Button>Update User Profile</Button>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+}
--- apps/rahat-ui/src/sections/profile/profileView.tsx
+++ apps/rahat-ui/src/sections/profile/profileView.tsx
@@ -6,35 +6,44 @@ import {
   CardTitle,
 } from '@rahat-ui/shadcn/src/components/ui/card';
 import { useUserStore } from '@rumsan/react-query';
+import EditButton from '../projects/components/edit.btn';
 
 export default function ProfileView() {
   const user = useUserStore((state) => state.user);
   return (
-    <div className="mt-8 bg-card">
-      <p className="text-3xl font-semibold">Profile Details</p>
-      <Card className="mt-8 p-4 shadow-md bg-secondary">
-        <CardHeader>
-          <CardTitle className="text-primary">
-            {user?.data?.name || 'N/A'}
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-between flex-wrap p-2">
-            <div>
-              <p>{user?.data?.email || 'N/A'}</p>
-              <p className="text-sm font-medium text-primary">Email</p>
-            </div>
-            <div>
-              <p>{user?.data?.phone || 'N/A'}</p>
-              <p className="text-sm font-medium text-primary">Phone Number</p>
-            </div>
-            <div>
-              <p>{user?.data?.wallet || 'N/A'}</p>
-              <p className="text-sm font-medium text-primary">Address</p>
-            </div>
+    <Card className="shadow-md">
+      <CardHeader className="pb-4">
+        <CardTitle>
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">User Profile</h1>
+            <EditButton path="/profile/edit" />
           </div>
-        </CardContent>
-      </Card>
-    </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex justify-between flex-wrap p-2">
+          <div>
+            <p>{user?.data?.name || 'N/A'}</p>
+            <p className="text-sm font-medium text-muted-foreground">Name</p>
+          </div>
+          <div>
+            <p>{user?.data?.email || 'N/A'}</p>
+            <p className="text-sm font-medium text-muted-foreground">Email</p>
+          </div>
+          <div>
+            <p>{user?.data?.phone || 'N/A'}</p>
+            <p className="text-sm font-medium text-muted-foreground">
+              Phone Number
+            </p>
+          </div>
+          <div>
+            <p>{user?.data?.wallet || 'N/A'}</p>
+            <p className="text-sm font-medium text-muted-foreground">
+              Wallet Address
+            </p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 }

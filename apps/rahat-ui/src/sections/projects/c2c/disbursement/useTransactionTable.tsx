6b2643395d73b139f6105c60df7aa5c0dd0b6d69
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-07-23 15:55:21 +0545

    transaction table update

--- apps/rahat-ui/src/sections/projects/c2c/disbursement/useTransactionTable.tsx
+++ apps/rahat-ui/src/sections/projects/c2c/disbursement/useTransactionTable.tsx
@@ -1,33 +1,7 @@
-import { Checkbox } from '@rahat-ui/shadcn/src/components/ui/checkbox';
 import { ColumnDef } from '@tanstack/react-table';
-import { useParams, useRouter } from 'next/navigation';
 
 export const useTransactionTable = () => {
-  const { id } = useParams();
-  const router = useRouter();
   const columns: ColumnDef<any>[] = [
-    {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
     {
       accessorKey: 'from',
       header: 'From',

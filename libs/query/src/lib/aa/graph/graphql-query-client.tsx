559b48c9ff7a9ea042374447b73c6d5ea5cded21
avash-rumsan <avash.ghimire@rumsan.net>
2024-08-05 18:51:40 +0545

    subgraph from project db

--- libs/query/src/lib/aa/graph/graphql-query-client.tsx
+++ libs/query/src/lib/aa/graph/graphql-query-client.tsx
@@ -1,13 +1,27 @@
 'use client';
 import { cacheExchange, Client, fetchExchange, Provider } from 'urql';
 import React from 'react';
+import { useProjectSettingsStore } from '../../projects';
+import { PROJECT_SETTINGS_KEYS } from 'libs/query/src/config';
+import { useParams } from 'next/navigation';
+import { UUID } from 'crypto';
 
 const GarphQlProvider = ({ children }: any) => {
-  const QueryURL = process.env['NEXT_PUBLIC_AA_SUBGRAPH_URL'] as string;
+  const backupSubgraphURL = process.env[
+    'NEXT_PUBLIC_AA_SUBGRAPH_URL'
+  ] as string;
+
+  const { id: uuid } = useParams();
+
+  const subgraphURL = useProjectSettingsStore(
+    (s) => s.settings?.[uuid as UUID]?.[PROJECT_SETTINGS_KEYS.SUBGRAPH]?.url,
+  );
+
   const client = new Client({
-    url: QueryURL,
+    url: subgraphURL || backupSubgraphURL,
     exchanges: [cacheExchange, fetchExchange],
   });
+
   return <Provider value={client}>{children}</Provider>;
 };
 

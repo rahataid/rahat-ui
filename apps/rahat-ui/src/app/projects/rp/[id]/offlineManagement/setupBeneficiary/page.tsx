3615dc0f382f453e5247e96bf116151aae420831
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2024-08-08 17:01:17 +0545

    update

--- apps/rahat-ui/src/app/projects/rp/[id]/offlineManagement/setupBeneficiary/page.tsx
+++ apps/rahat-ui/src/app/projects/rp/[id]/offlineManagement/setupBeneficiary/page.tsx
@@ -1,7 +1,9 @@
+import SetupBeneficiaryPage from 'apps/rahat-ui/src/sections/projects/rp/offlineManagement/setupBeneficiary/page';
+
 const page = () => {
   return (
     <div className="bg-secondary h-[calc(100vh-60px)] overflow-auto">
-      Setup Beneficiary
+      <SetupBeneficiaryPage />
     </div>
   );
 };
---
+++ apps/rahat-ui/src/sections/projects/rp/offlineManagement/setupBeneficiary/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SetupBeneficiaryPage = () => {
+  return <div className="bg-card rounded-lg m-4 p-6">Page</div>;
+};
+
+export default SetupBeneficiaryPage;

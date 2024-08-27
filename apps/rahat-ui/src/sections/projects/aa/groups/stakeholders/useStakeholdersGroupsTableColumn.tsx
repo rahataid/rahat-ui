fed1f105826f3237a9af43471e51fd6ae780a047
amrit-rumsan <amrit@rumsan.net>
2024-08-27 15:50:30 +0545

    groups cleanup

--- apps/rahat-ui/src/sections/projects/aa/groups/stakeholders/useStakeholdersGroupsTableColumn.tsx
+++ apps/rahat-ui/src/sections/projects/aa/groups/stakeholders/useStakeholdersGroupsTableColumn.tsx
@@ -1,6 +1,5 @@
 import { useParams, useRouter } from 'next/navigation';
 import { ColumnDef } from '@tanstack/react-table';
-import { Checkbox } from '@rahat-ui/shadcn/src/components/ui/checkbox';
 import { Eye } from 'lucide-react';
 import { setPaginationToLocalStorage } from '../../prev.pagination.storage';
 
@@ -15,28 +14,6 @@ export default function useStakeholdersGroupsTableColumn() {
   };
 
   const columns: ColumnDef<any>[] = [
-    // {
-    //   id: 'select',
-    //   header: ({ table }) => (
-    //     <Checkbox
-    //       checked={
-    //         table.getIsAllPageRowsSelected() ||
-    //         (table.getIsSomePageRowsSelected() && 'indeterminate')
-    //       }
-    //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-    //       aria-label="Select all"
-    //     />
-    //   ),
-    //   cell: ({ row }) => (
-    //     <Checkbox
-    //       checked={row.getIsSelected()}
-    //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-    //       aria-label="Select row"
-    //     />
-    //   ),
-    //   enableSorting: false,
-    //   enableHiding: false,
-    // },
     {
       accessorKey: 'name',
       header: 'Group Name',
@@ -50,20 +27,6 @@ export default function useStakeholdersGroupsTableColumn() {
         return <div>{row?.original?._count?.stakeholders}</div>;
       },
     },
-    // {
-    //   accessorKey: 'stakeholders',
-    //   header: 'Member',
-    //   cell: ({ row }) => (
-    //     <div>
-    //       {row.original?.stakeholders?.map((member: any, index: number) => (
-    //         <span key={member?.id}>
-    //           {member?.name}
-    //           {index !== row.original.stakeholders.length - 1 && ', '}
-    //         </span>
-    //       ))}
-    //     </div>
-    //   ),
-    // },
     {
       id: 'actions',
       enableHiding: false,

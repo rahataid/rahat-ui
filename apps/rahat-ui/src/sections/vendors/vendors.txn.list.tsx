e5fdab1999ea384d67fdb5a752b61965b97ae206
bipinparajuli <bipinprjl@gmail.com>
2024-09-06 12:16:57 +0545

    fix:vendor txn pagination

--- apps/rahat-ui/src/sections/vendors/vendors.txn.list.tsx
+++ apps/rahat-ui/src/sections/vendors/vendors.txn.list.tsx
@@ -27,6 +27,15 @@ import { useVendorTransaction } from '../../hooks/el/subgraph/querycall';
 import { truncateEthAddress } from '@rumsan/sdk/utils';
 import TableLoader from '../../components/table.loader';
 import { useRPVendorTransactions } from '@rahat-ui/query';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@rahat-ui/shadcn/src/components/ui/select';
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
 
 // export type Payment = {
 //   id: string;
@@ -73,9 +82,7 @@ interface VendorTxnListProps {
 }
 
 export default function VendorTxnList({ walletAddress }: VendorTxnListProps) {
-  const { data: txns, isFetching } = useRPVendorTransactions(
-    walletAddress,
-  );
+  const { data: txns, isFetching } = useRPVendorTransactions(walletAddress);
 
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
@@ -158,6 +165,51 @@ export default function VendorTxnList({ walletAddress }: VendorTxnListProps) {
             </TableBody>
           </ScrollArea>
         </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex items-center gap-2">
+          <div className="text-sm font-medium">Rows per page</div>
+          <Select
+            defaultValue="10"
+            onValueChange={(value) => table.setPageSize(Number(value))}
+          >
+            <SelectTrigger className="w-16">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="5">5</SelectItem>
+                <SelectItem value="10">10</SelectItem>
+                <SelectItem value="20">20</SelectItem>
+                <SelectItem value="30">30</SelectItem>
+                <SelectItem value="40">40</SelectItem>
+                <SelectItem value="50">50</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex-1 text-sm text-muted-foreground">
+          Page {table.getState().pagination.pageIndex + 1} of{' '}
+          {table.getPageCount()}
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </>
   );

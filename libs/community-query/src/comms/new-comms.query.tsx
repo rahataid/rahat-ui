eba216604a0c8b86d1e860606cb8ce8501b138a4
karun-rumsan <gyawalikarrun@gmail.com>
2024-09-06 17:49:38 +0545

    initial phase of communication query integration

--- libs/community-query/src/comms/new-comms.query.tsx
+++ libs/community-query/src/comms/new-comms.query.tsx
@@ -1,6 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { TAGS } from '../config';
 import { useCommunicationService } from './comms.helpers';
+import { useRSQuery } from '@rumsan/react-query';
+import { getBeneficiaryCommsClient } from '@rahataid/community-tool-sdk/clients';
+import Swal from 'sweetalert2';
+import { Pagination } from '@rumsan/sdk/types';
 
 export const useListAllTransports = () => {
   const newCommunicationService = useCommunicationService();
@@ -10,4 +14,72 @@ export const useListAllTransports = () => {
   });
 
   return query;
+};
+
+export const useListTransports = () => {
+  const { queryClient, rumsanService } = useRSQuery();
+  const commsClient = getBeneficiaryCommsClient(rumsanService.client);
+
+  return useQuery(
+    {
+      queryKey: [TAGS.COMMS_LIST_TRANSPORTS],
+      queryFn: () => commsClient.listTransport(),
+    },
+    queryClient,
+  );
+};
+
+export const useCreateBeneficiaryComms = () => {
+  const { queryClient, rumsanService } = useRSQuery();
+  const commsClient = getBeneficiaryCommsClient(rumsanService.client);
+  return useMutation({
+    mutationFn: (payload: any) => commsClient.create(payload),
+    mutationKey: [TAGS.CREATE_BENEFICIARY_COMMS],
+    onSuccess: () => {
+      Swal.fire('Created Successfully', '', 'success');
+      queryClient.invalidateQueries({
+        queryKey: [TAGS.LIST_BENEFICIARIES_COMMS],
+      });
+    },
+  });
+};
+
+export const useListBeneficiariesComms = (
+  payload: Pagination & { [key: string]: string },
+) => {
+  const { queryClient, rumsanService } = useRSQuery();
+  const commsClient = getBeneficiaryCommsClient(rumsanService.client);
+  return useQuery(
+    {
+      queryKey: [TAGS.LIST_BENEFICIARIES_COMMS, payload],
+      queryFn: () => commsClient.listBenefComms(payload),
+    },
+    queryClient,
+  );
+};
+
+export const useLisBeneficiaryComm = () => {
+  const { queryClient, rumsanService } = useRSQuery();
+  const commsClient = getBeneficiaryCommsClient(rumsanService.client);
+  return useQuery(
+    {
+      queryKey: [TAGS.LIST_BENEFICIARY_COMM],
+      queryFn: () => commsClient.listBenefCommsByID,
+    },
+    queryClient,
+  );
+};
+
+export const useTriggerCommunication = () => {
+  const { queryClient, rumsanService } = useRSQuery();
+  const commsClient = getBeneficiaryCommsClient(rumsanService.client);
+  return useMutation({
+    mutationFn: commsClient.triggerCommunication,
+    mutationKey: [TAGS.TRIGGER_COMMUNICATION],
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [TAGS.LIST_BENEFICIARIES_COMMS],
+      });
+    },
+  });
 };

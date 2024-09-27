49bc2242c36a9292a4a1b40484b6c4bf1d19ffff
karun-rumsan <gyawalikarrun@gmail.com>
2024-09-27 11:16:43 +0545

    beneificary cambodia ui pages

---
+++ apps/rahat-ui/src/sections/projects/cambodia/beneficiary/index.ts
@@ -0,0 +1,4 @@
+export { default as ELCambodiaBeneficiaryView } from './beneficiary.view';
+export { default as ELCambodiaBeneficiaryAddView } from './beneficiary.add';
+export { default as ELCambodiaBeneficiaryDetailView } from './beneficiary.detail';
+export { default as ElCambodiaDiscardedBeneficiaryView } from './discarded.beneficiary.view';
---
+++ apps/rahat-ui/src/sections/projects/cambodia/beneficiary/use.beneficiary.table.columns.tsx
@@ -0,0 +1,52 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { Eye } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+
+export const useCambodiaBeneficiaryTableColumns = () => {
+  const { id } = useParams();
+  const router = useRouter();
+  const columns: ColumnDef<any>[] = [
+    {
+      accessorKey: 'name',
+      header: 'Name',
+      cell: ({ row }) => <div>{row.getValue('name')}</div>,
+    },
+    {
+      accessorKey: 'type',
+      header: 'Type',
+      cell: ({ row }) => <div>{row.getValue('voucherType')}</div>,
+    },
+    {
+      accessorKey: 'phone',
+      header: 'Phone',
+      cell: ({ row }) => <div>{row.getValue('phone')}</div>,
+    },
+    {
+      accessorKey: 'gender',
+      header: 'Gender',
+      cell: ({ row }) => <div>{row.getValue('gender')}</div>,
+    },
+    {
+      id: 'actions',
+      header: 'Actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center gap-2">
+            <Eye
+              className="hover:text-primary cursor-pointer"
+              size={16}
+              strokeWidth={1.5}
+              onClick={() =>
+                router.push(
+                  `/projects/el-cambodia/${id}/beneficiary/${row.original.uuid}`,
+                )
+              }
+            />
+          </div>
+        );
+      },
+    },
+  ];
+  return columns;
+};

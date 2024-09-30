3a09aa2ff5d2dda6061cb6d242473e612912cbe8
anupama-rumsan <anupama@rumsan.net>
2024-09-30 11:53:16 +0545

    contract hooks added for kenya interaction

--- libs/query/src/lib/el-kenya/contracts/hooks/el-kenya.ts
+++ libs/query/src/lib/el-kenya/contracts/hooks/el-kenya.ts
@@ -3,8 +3,79 @@ import Swal from "sweetalert2";
 import { useMutation } from "@tanstack/react-query";
 import { encodeFunctionData } from "viem";
 import { useReadRahatTokenDecimals } from "../generated-hooks/rahatToken";
-import {  useWriteRahatCvaKenyaMulticall,rahatCvaKenyaAbi } from "../generated-hooks";
+import {  useWriteRahatCvaKenyaMulticall,
+  rahatCvaKenyaAbi, 
+  useWriteRahatTreasuryCreateToken, 
+  useReadRahatCvaKenyaTokenAllocations, 
+  useWriteRahatTreasuryTransferToken, 
+  useWriteRedemptionsRedeemToken } from "../generated-hooks";
+import { useRouter } from "next/navigation";
 
+export const useTokenCreate = () => {
+  const { queryClient } = useRSQuery();
+  const treasuryCreateToken = useWriteRahatTreasuryCreateToken();
+
+  const alert = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+  });
+
+  return useMutation(
+    {
+      onSuccess: async () => {
+        alert.fire({
+          icon: 'success',
+          title: 'Token Created Successfully',
+        });
+      },
+
+      onError: (error) => {
+        console.log('error', error.message);
+        alert.fire({
+          icon: 'error',
+          title: 'Error minting and approving token',
+          text: 'Error Creating Token',
+        });
+      },
+
+      mutationFn: async ({
+        name,
+        symbol,
+        description,
+        decimals,
+        manager,
+        rahatTreasuryAddress,
+        initialSupply,
+      }: {
+        name: string;
+        symbol: string;
+        description: string;
+        decimals: number;
+        manager: `0x${string}`;
+        rahatTreasuryAddress: `0x${string}`;
+        initialSupply: string;
+      }) => {
+        const value = await treasuryCreateToken.writeContractAsync({
+          args: [
+            name,
+            symbol,
+            description,
+            decimals,
+            BigInt(initialSupply),
+            rahatTreasuryAddress,
+            manager,
+          ],
+          address: rahatTreasuryAddress,
+        });
+
+        return value;
+      },
+    },
+    queryClient,
+  );
+};
 
 export const useBulkAllocateTokens = (tokenAddress: any) => {
   const multi = useWriteRahatCvaKenyaMulticall();
@@ -89,3 +160,135 @@ export const useBulkAllocateTokens = (tokenAddress: any) => {
     queryClient, 
   );
 };
+
+export const useGetTokenAllocations = (
+  projectAddress: `0x${string}`,
+  tokenAddress: `0x${string}`,
+) => {
+  const decimals = useReadRahatTokenDecimals({
+    query: {
+      enabled: !!tokenAddress,
+    },
+    address: tokenAddress,
+  });
+  console.log('decimals.', decimals.data);
+  return useReadRahatCvaKenyaTokenAllocations({
+    address: projectAddress,
+    query: {
+      enabled: !!projectAddress,
+      select(data) {
+        console.log('data', data);
+        return data ? formatUnits(data, decimals.data as number) : 0;
+      },
+    },
+  });
+};
+
+export const useSendFundToProject = () => {
+  const sendFundProject = useWriteRahatTreasuryTransferToken();
+
+  const { queryClient } = useRSQuery();
+  // const decimals = useReadRahatTokenDecimals({
+  //   address: tokenAddress,
+  // });
+  const router = useRouter();
+
+  const alert = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-primary',
+      cancelButton: 'btn btn-secondary',
+    },
+    buttonsStyling: false,
+  });
+
+  return useMutation(
+    {
+      onError: (error) => {
+        alert.fire({
+          icon: 'error',
+          title: 'Error sending funds',
+          text: error.message,
+        });
+      },
+      onSuccess: (d, { projectAddress }) => {
+        alert.fire({
+          icon: 'success',
+          title: 'Funds sent successfully',
+        });
+        router.push('/treasury');
+
+        // queryClient.invalidateQueries({
+        //   queryKey: ['ProjectDetails', projectAddress],
+        // });
+        // console.log('success', d);
+      },
+      mutationFn: async ({
+        amount,
+        projectAddress,
+        tokenAddress,
+        treasuryAddress,
+      }: {
+        projectAddress: `0x${string}`;
+        amount: string;
+        tokenAddress: `0x${string}`;
+        treasuryAddress: `0x${string}`;
+      }) => {
+        return sendFundProject.writeContractAsync({
+          // @ts-ignore
+          args: [tokenAddress, projectAddress, formatUnits(amount, 0)],
+          address: treasuryAddress,
+        });
+      },
+    },
+    queryClient,
+  );
+};
+
+// export const useContractRedeem = (projectUUID: UUID) => {
+//   const redemptionContract = useWriteRedemptionsRedeemToken();
+
+//   const updateRedemption = useRedeemToken(projectUUID);
+//   const alert = Swal.mixin({
+//     customClass: {
+//       confirmButton: 'btn btn-primary',
+//       cancelButton: 'btn btn-secondary',
+//     },
+//     buttonsStyling: false,
+//   });
+
+//   return useMutation({
+//     onError: (error) => {
+//       alert.fire({
+//         icon: 'error',
+//         title: 'Error redeeming token',
+//         text: error.message,
+//       });
+//     },
+//     onSuccess: async (d, variables) => {
+//       await updateRedemption.mutateAsync({ uuid: variables?.uuid });
+//       alert.fire({
+//         icon: 'success',
+//         title: 'Token redeemed successfully',
+//       });
+//     },
+//     mutationFn: async ({
+//       amount,
+//       tokenAddress,
+//       redemptionAddress,
+//       senderAddress,
+//       uuid,
+//     }: {
+//       amount: number;
+//       tokenAddress: `0x${string}`;
+//       redemptionAddress: `0x${string}`;
+//       senderAddress: `0x${string}`;
+//       uuid: string;
+//     }) => {
+//       return redemptionContract.writeContractAsync({
+//         args: [tokenAddress, senderAddress, BigInt(amount)],
+//         address: redemptionAddress,
+//       });
+//     },
+//   });
+// };
+

33500a2451876cf34ed326788c4d4eab74acecb2
karun-rumsan <gyawalikarrun@gmail.com>
2024-10-08 18:13:29 +0545

    refactor and integration  of sub graph in transaction

--- apps/rahat-ui/src/sections/projects/cambodia/transactions/transactions.view.tsx
+++ apps/rahat-ui/src/sections/projects/cambodia/transactions/transactions.view.tsx
@@ -1,6 +1,15 @@
-import { usePagination } from '@rahat-ui/query';
 import {
+  useCambodiaProjectSubgraphStore,
+  useCambodiaProjectTransactions,
+  usePagination,
+} from '@rahat-ui/query';
+import {
+  ColumnFiltersState,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
   VisibilityState,
 } from '@tanstack/react-table';
@@ -12,42 +21,47 @@ import CambodiaTable from '../table.component';
 import { useTableColumns } from './use.table.columns';
 import CustomPagination from 'apps/rahat-ui/src/components/customPagination';
 import ViewColumns from '../../components/view.columns';
+import Pagination from 'apps/rahat-ui/src/components/pagination';
 
 export default function TransactionsView() {
   const { id } = useParams() as { id: UUID };
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
 
-  const {
-    pagination,
-    filters,
-    setFilters,
-    setNextPage,
-    setPrevPage,
-    setPerPage,
-    selectedListItems,
-    setSelectedListItems,
-    resetSelectedListItems,
-  } = usePagination();
-
+  const { projectTransactions } = useCambodiaProjectSubgraphStore();
+  const tableData: any = React.useMemo(() => {
+    if (projectTransactions) return projectTransactions;
+    else return [];
+  }, [projectTransactions]);
+  console.log(tableData);
   const columns = useTableColumns();
   const table = useReactTable({
-    manualPagination: true,
-    data: [
-      { walletAddress: '123', topic: 'A1' },
-      { walletAddress: '456', topic: 'B1' },
-    ],
+    data: tableData || [],
     columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
     state: {
+      sorting,
+      columnFilters,
       columnVisibility,
+      rowSelection,
     },
   });
   return (
     <>
       <div className="p-4">
-        <div className="mb-4">
+        <div className="mb-2">
           <h1 className="font-semibold text-2xl mb-">Transactions</h1>
           <p className="text-muted-foreground">
             These transactions are documented on the blockchain.
@@ -57,23 +71,29 @@ export default function TransactionsView() {
           <div className="flex justify-between space-x-2 mb-2">
             <SearchInput
               className="w-full"
-              name="transaction"
-              onSearch={() => {}}
+              name="topic"
+              value={
+                (table.getColumn('topic')?.getFilterValue() as string) ?? ''
+              }
+              onSearch={(event) =>
+                table.getColumn('topic')?.setFilterValue(event.target.value)
+              }
             />
             <ViewColumns table={table} />
           </div>
-          <CambodiaTable table={table} tableHeight="h-[calc(100vh-294px)]" />
+          <CambodiaTable table={table} tableHeight="h-[calc(100vh-300px)]" />
         </div>
+
+        <Pagination
+          pageIndex={table.getState().pagination.pageIndex}
+          pageCount={table.getPageCount()}
+          setPageSize={table.setPageSize}
+          canPreviousPage={table.getCanPreviousPage()}
+          previousPage={table.previousPage}
+          canNextPage={table.getCanNextPage()}
+          nextPage={table.nextPage}
+        />
       </div>
-      <CustomPagination
-        currentPage={pagination.page}
-        handleNextPage={setNextPage}
-        handlePrevPage={setPrevPage}
-        handlePageSizeChange={setPerPage}
-        meta={{ total: 0, currentPage: 0 }}
-        perPage={pagination?.perPage}
-        total={0}
-      />
     </>
   );
 }

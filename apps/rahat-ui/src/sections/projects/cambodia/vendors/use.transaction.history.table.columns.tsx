87456dc30ae9f4f9fa1f958c1a3313fac2e230e3
karun-rumsan <gyawalikarrun@gmail.com>
2024-10-21 18:04:57 +0545

    cleanup and feedback

--- apps/rahat-ui/src/sections/projects/cambodia/vendors/use.transaction.history.table.columns.tsx
+++ apps/rahat-ui/src/sections/projects/cambodia/vendors/use.transaction.history.table.columns.tsx
@@ -1,6 +1,20 @@
+'use client';
 import { ColumnDef } from '@tanstack/react-table';
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/tooltip';
+import { useState } from 'react';
+import { Copy, CopyCheck } from 'lucide-react';
 export const useTransactionHistoryTableColumns = () => {
+  const [walletAddressCopied, setWalletAddressCopied] = useState<number>();
+
+  const clickToCopy = (walletAddress: string, index: number) => {
+    navigator.clipboard.writeText(walletAddress);
+    setWalletAddressCopied(index);
+  };
   const columns: ColumnDef<any>[] = [
     {
       accessorKey: 'topic',
@@ -10,12 +24,43 @@ export const useTransactionHistoryTableColumns = () => {
     {
       accessorKey: 'beneficiary',
       header: 'Beneficiary',
-      cell: ({ row }) => <div>{row?.original?.beneficiary}</div>,
+      cell: ({ row }) => (
+        <p className="truncate w-16">{row?.original?.beneficiary}</p>
+      ),
     },
     {
       accessorKey: 'txHash',
       header: 'TxHash',
-      cell: ({ row }) => <div>{row?.original?.txHash}</div>,
+      cell: ({ row }) => (
+        <>
+          {row?.original?.txHash ? (
+            <TooltipProvider delayDuration={100}>
+              <Tooltip>
+                <TooltipTrigger
+                  className="flex gap-3 cursor-pointer"
+                  onClick={() => clickToCopy(row?.original?.txHash, row.index)}
+                >
+                  <p className="truncate w-24">{row?.original?.txHash}</p>
+                  {walletAddressCopied === row.index ? (
+                    <CopyCheck size={20} strokeWidth={1.5} />
+                  ) : (
+                    <Copy size={20} strokeWidth={1.5} />
+                  )}
+                </TooltipTrigger>
+                <TooltipContent className="bg-secondary" side="bottom">
+                  <p className="text-xs font-medium">
+                    {walletAddressCopied === row.index
+                      ? 'copied'
+                      : 'click to copy'}
+                  </p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ) : (
+            'N/A'
+          )}
+        </>
+      ),
     },
     {
       accessorKey: 'timeStamp',

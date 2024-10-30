ce9c8a91d3737921e4076258b2beff6f36c7ed72
karun-rumsan <gyawalikarrun@gmail.com>
2024-10-30 11:09:31 +0545

    fix the columns table and passing the true paramas

--- apps/rahat-ui/src/sections/projects/cambodia/beneficiary/use.beneficiary.table.columns.tsx
+++ apps/rahat-ui/src/sections/projects/cambodia/beneficiary/use.beneficiary.table.columns.tsx
@@ -1,13 +1,10 @@
 import { ColumnDef } from '@tanstack/react-table';
-import { formatUnderScoredString } from 'apps/rahat-ui/src/utils/string';
 import { Eye } from 'lucide-react';
-import { useParams, usePathname, useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 export const useCambodiaBeneficiaryTableColumns = () => {
   const { id } = useParams();
-  const pathName = usePathname();
   const router = useRouter();
-  const isDiscardedBeneficiary = pathName.includes('discardedbenificary');
   const columns: ColumnDef<any>[] = [
     {
       accessorKey: 'name',
@@ -35,12 +32,11 @@ export const useCambodiaBeneficiaryTableColumns = () => {
       accessorKey: 'healthWorker',
       header: 'Health Worker',
       cell: ({ row }) => {
-        return <div>{row?.original?.healthWorker?.name || '-'}</div>;
+        return <div>{row?.original?.healthWorker || '-'}</div>;
       },
     },
-  ];
-  if (!isDiscardedBeneficiary) {
-    columns.push({
+
+    {
       id: 'actions',
       header: 'Actions',
       enableHiding: false,
@@ -60,7 +56,42 @@ export const useCambodiaBeneficiaryTableColumns = () => {
           </div>
         );
       },
-    });
-  }
+    },
+  ];
+
+  return columns;
+};
+
+export const useDiscardedCambodiaBeneficiaryTableColumns = () => {
+  const columns: ColumnDef<any>[] = [
+    {
+      accessorKey: 'name',
+      header: 'Name',
+      cell: ({ row }) => <div>{row.getValue('name')}</div>,
+    },
+    {
+      accessorKey: 'type',
+      header: 'Type',
+      cell: ({ row }) => {
+        return <div>{row?.original?.extras?.type || 'UNKNOWN'}</div>;
+      },
+    },
+    {
+      accessorKey: 'phone',
+      header: 'Phone',
+      cell: ({ row }) => <div>{row.getValue('phone')}</div>,
+    },
+    {
+      accessorKey: 'gender',
+      header: 'Gender',
+      cell: ({ row }) => <div>{row.getValue('gender')}</div>,
+    },
+    {
+      accessorKey: 'healthWorker',
+      header: 'Health Worker',
+      cell: ({ row }) => <div>{row?.original?.healthWorker?.name || ''}</div>,
+    },
+  ];
+
   return columns;
 };

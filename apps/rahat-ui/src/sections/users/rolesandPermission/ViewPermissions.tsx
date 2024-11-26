00ff5f04684a00ff9f584f3ce229c0be0f7d8170
amrit-rumsan <amrit@rumsan.net>
2024-11-26 11:06:29 +0545

    update: Roles & Permissions -> crud

--- apps/rahat-ui/src/sections/users/rolesandPermission/ViewPermissions.tsx
+++ apps/rahat-ui/src/sections/users/rolesandPermission/ViewPermissions.tsx
@@ -4,21 +4,20 @@ import { ShieldCheck } from 'lucide-react';
 
 export default function ViewPermissions({ subject, existingActions }: any) {
   return (
-    <div className="mt-4 divide-y divide-gray-200 dark:divide-gray-700">
-      <h3>
-        <strong>{capitalizeFirstLetter(subject)}</strong>
-      </h3>
-      <div className="flex flex-wrap">
-        {PERMISSIONS.map((d) => (
-          <div key={d.id} className="flex items-center mt-2">
-            {existingActions.includes(d.id) && (
-              <>
-                <ShieldCheck className="ml-2" color="green" size={18} />
-                <label>{d.label}</label>
-              </>
-            )}
-          </div>
-        ))}
+    <div className="mt-2 pb-1 border-b">
+      <p className="font-medium">{capitalizeFirstLetter(subject)}</p>
+      <div className="flex flex-wrap space-x-4">
+        {PERMISSIONS.map(
+          (d) =>
+            existingActions.includes(d.id) && (
+              <div key={d.id} className="flex items-center mt-1">
+                <>
+                  <ShieldCheck className="" color="green" size={18} />
+                  <label>{d.label}</label>
+                </>
+              </div>
+            ),
+        )}
       </div>
     </div>
   );

c5e7fa1b457e5d49fbedea7974a46349aed359ab
bipinparajuli <bipinprjl@gmail.com>
2024-12-23 16:42:39 +0545

    fix:added groups

---
+++ apps/rahat-ui/src/app/projects/c2c/[id]/beneficiary/group/add/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { ELKenyaSelectGroupCreate } from 'apps/rahat-ui/src/sections/projects/el-kenya';
+
+const Page = () => {
+  return <ELKenyaSelectGroupCreate />;
+};
+
+export default Page;

0a5b55273d5e7eb63cfe409c437e4f68d77ce838
amrit-rumsan <amrit@rumsan.net>
2025-03-24 08:55:57 +0545

    fixes & clleanups

--- apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/main.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/main.tsx
@@ -1,10 +1,17 @@
 import { Heading } from 'apps/rahat-ui/src/common';
 import { TriggersListCard, TriggersPhaseCard } from './components';
 import { useParams, useRouter } from 'next/navigation';
+import { useActivitiesPhase, useActivitiesStore } from '@rahat-ui/query';
+import { UUID } from 'crypto';
 
 export default function TriggerStatementView() {
   const router = useRouter();
-  const { id } = useParams();
+  const params = useParams();
+  const projectId = params.id as UUID;
+
+  useActivitiesPhase(projectId);
+  const phases = useActivitiesStore((state) => state.phases);
+
   return (
     <div className="p-4">
       <Heading
@@ -12,38 +19,33 @@ export default function TriggerStatementView() {
         description="Track all the trigger reports here"
       />
       <div className="grid grid-cols-3 gap-4">
-        <TriggersPhaseCard
-          title="Readiness"
-          subtitle="Overview of readiness phase"
-          handleAddTrigger={() => {
-            router.push(`/projects/aa/${id}/trigger-statements/add`);
-          }}
-          chartLabels={['Mandatory', 'Optional']}
-          chartSeries={[10, 2]}
-          mandatoryTriggers={10}
-          optionalTriggers={2}
-          triggeredMandatoryTriggers={8}
-          triggeredOptionalTriggers={1}
-          handleViewDetails={() => {
-            router.push(`/projects/aa/${id}/trigger-statements/111`);
-          }}
-        />
-        <TriggersPhaseCard
-          title="Activation"
-          subtitle="Overview of activation phase"
-          handleAddTrigger={() => {
-            router.push(`/projects/aa/${id}/trigger-statements/add`);
-          }}
-          chartLabels={['Mandatory', 'Optional']}
-          chartSeries={[10, 2]}
-          mandatoryTriggers={10}
-          optionalTriggers={2}
-          triggeredMandatoryTriggers={8}
-          triggeredOptionalTriggers={1}
-          handleViewDetails={() => {
-            router.push(`/projects/aa/${id}/trigger-statements/111`);
-          }}
-        />
+        {phases
+          .filter((p) => p.name !== 'PREPAREDNESS')
+          .map((d) => (
+            <TriggersPhaseCard
+              key={d.id}
+              title={d.name}
+              subtitle={`Overview of ${d.name.toLowerCase()} phase`}
+              handleAddTrigger={() => {
+                localStorage.setItem(
+                  'selectedPhase',
+                  JSON.stringify({ id: d.uuid, name: d.name }),
+                );
+                router.push(`/projects/aa/${projectId}/trigger-statements/add`);
+              }}
+              chartLabels={['Mandatory', 'Optional']}
+              chartSeries={[10, 2]}
+              mandatoryTriggers={10}
+              optionalTriggers={2}
+              triggeredMandatoryTriggers={8}
+              triggeredOptionalTriggers={1}
+              handleViewDetails={() => {
+                router.push(
+                  `/projects/aa/${projectId}/trigger-statements/${d.uuid}`,
+                );
+              }}
+            />
+          ))}
         <TriggersListCard />
       </div>
     </div>

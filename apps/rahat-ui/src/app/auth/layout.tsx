c97cfd30523c26625f54cfe1ab774e100676c38f
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-04-01 12:35:11 +0545

    bug fix

--- apps/rahat-ui/src/app/auth/layout.tsx
+++ apps/rahat-ui/src/app/auth/layout.tsx
@@ -11,17 +11,19 @@ export default function AuthLayout({
 }) {
   return (
     <GuestGuard>
-      <div className="h-screen flex">
-        <div className="w-1/2 bg-login bg-cover bg-center">
-          <div className="w-full h-full bg-black bg-opacity-60 p-8 flex flex-col justify-between">
+      <div className="h-screen flex flex-col md:flex-row">
+        {/* Left Section (Logo + Text) */}
+        <div className="w-full md:w-1/2 bg-login bg-cover bg-center">
+          <div className="w-full h-full bg-black bg-opacity-60 p-6 flex flex-col justify-between">
             <Image
               src="/svg/rahat-logo-white.png"
               alt="rahat-logo-white"
-              height={150}
-              width={150}
+              height={100}
+              width={100}
+              className="mx-auto md:mx-0"
             />
-            <div>
-              <p className="text-white w-4/5">
+            <div className="text-center md:text-left">
+              <p className="text-white text-sm md:text-base w-full md:w-4/5 mx-auto">
                 Rahat, an open-source blockchain-based financial access platform
                 to support vulnerable communities. Our mission is to break the
                 poverty cycle by providing immediate financial access, building
@@ -31,7 +33,11 @@ export default function AuthLayout({
             </div>
           </div>
         </div>
-        <div className="w-1/2">{children}</div>
+
+        {/* Right Section (Auth Form) */}
+        <div className="w-full md:w-1/2 flex justify-center items-center p-6">
+          {children}
+        </div>
       </div>
     </GuestGuard>
   );

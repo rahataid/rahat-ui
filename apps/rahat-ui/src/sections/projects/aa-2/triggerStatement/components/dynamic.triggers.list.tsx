4b6ada53ce7ff408a6d6927195309b2fec24c0cc
amrit-rumsan <amrit@rumsan.net>
2025-04-03 13:34:18 +0545

    fixes

--- apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/components/dynamic.triggers.list.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/components/dynamic.triggers.list.tsx
@@ -18,9 +18,9 @@ export default function DynamicTriggersList({ projectId, triggers }: IProps) {
             type={t?.triggerStatement?.type || 'N/A'}
             isTriggered={t?.isTriggered}
             title={t?.title || 'N/A'}
-            dataSource={t?.source?.source || 'N/A'}
-            riverBasin={t?.source?.riverBasin || 'N/A'}
-            time={t?.createdAt?.toLocaleString()}
+            dataSource={t?.phase?.source?.source || 'N/A'}
+            riverBasin={t?.phase?.source?.riverBasin || 'N/A'}
+            time={new Date(t?.createdAt)?.toLocaleString()}
           />
         ))}
         {/* <TriggerCard

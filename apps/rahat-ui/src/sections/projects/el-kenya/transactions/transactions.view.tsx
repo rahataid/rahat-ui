7a7dfb73a866420ad84724ac12ad8c1d7636be15
bipinparajuli <bipinprjl@gmail.com>
2025-04-03 10:23:12 +0545

    fix:fixed transaction sorting

--- apps/rahat-ui/src/sections/projects/el-kenya/transactions/transactions.view.tsx
+++ apps/rahat-ui/src/sections/projects/el-kenya/transactions/transactions.view.tsx
@@ -8,7 +8,7 @@ import { UUID } from 'crypto';
 import { useParams } from 'next/navigation';
 import { useElkenyaTransactionsTableColumns } from './use.transactions.table.columns';
 import { useKenyaProjectTransactions } from '@rahat-ui/query';
-import React from 'react';
+import React, { useState } from 'react';
 import ElkenyaTable from '../table.component';
 import ClientSidePagination from '../../components/client.side.pagination';
 
@@ -17,8 +17,11 @@ export default function TransactionsView() {
   const { data, error, isLoading } = useKenyaProjectTransactions();
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
+  const [sorting, setSorting] = useState([{ id: 'timeStamp', desc: true }]);
 
-  const columns = useElkenyaTransactionsTableColumns();
+  const columns = useElkenyaTransactionsTableColumns({
+    setSorting: setSorting,
+  });
   const table = useReactTable({
     data: data || [],
     columns,
@@ -26,6 +29,7 @@ export default function TransactionsView() {
     getPaginationRowModel: getPaginationRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
     state: {
+      sorting,
       columnVisibility,
     },
   });

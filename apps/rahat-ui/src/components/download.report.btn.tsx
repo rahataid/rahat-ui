4740b647c1082b3ba2009d71497bd26e5192cc50
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-04-04 16:23:18 +0545

    hide shadow download report btn

--- apps/rahat-ui/src/components/download.report.btn.tsx
+++ apps/rahat-ui/src/components/download.report.btn.tsx
@@ -1,20 +1,15 @@
-import { Button } from "@rahat-ui/shadcn/src/components/ui/button";
-import { CloudDownload } from "lucide-react";
+import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
+import { CloudDownload } from 'lucide-react';
 
 type IProps = {
-    handleDownload: VoidFunction;
-}
+  handleDownload: VoidFunction;
+};
 
 export default function DownloadReportBtn({ handleDownload }: IProps) {
-    return (
-        <Button
-            type="button"
-            variant="outline"
-            className="shadow-md"
-            onClick={handleDownload}
-        >
-            <CloudDownload size={18} className="mr-1" />
-            Download Report
-        </Button>
-    )
+  return (
+    <Button type="button" variant="outline" onClick={handleDownload}>
+      <CloudDownload size={18} className="mr-1" />
+      Download Report
+    </Button>
+  );
 }

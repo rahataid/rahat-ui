dd4f5a4d355ed80c9de45a8df52a42f94859e1a6
amrit-rumsan <amrit@rumsan.net>
2025-04-10 08:11:22 +0545

    stakeholder : edit & delete feature integration

--- apps/rahat-ui/src/sections/projects/aa-2/stakeholders/stakeholders.details.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/stakeholders/stakeholders.details.tsx
@@ -1,17 +1,32 @@
 'use client';
 import { Button } from '@rahat-ui/shadcn/src/components/ui/button';
-import { HeaderWithBack } from 'apps/rahat-ui/src/common';
+import {
+  DeleteButton,
+  EditButton,
+  HeaderWithBack,
+} from 'apps/rahat-ui/src/common';
 import { UUID } from 'crypto';
 import { Edit2, Trash2 } from 'lucide-react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import StakeHolderInfo from './staholders.info';
-import { useStakeholderDetails } from '@rahat-ui/query';
+import { useDeleteStakeholders, useStakeholderDetails } from '@rahat-ui/query';
 const StakeholdersDetail = () => {
+  const router = useRouter();
   const params = useParams();
   const projectId = params.id as UUID;
   const stakeholderId = params.stakeholdersId as UUID;
 
   const details = useStakeholderDetails(projectId, { uuid: stakeholderId });
+
+  const removeStakeholder = useDeleteStakeholders();
+
+  const handleDelete = async () => {
+    await removeStakeholder.mutateAsync({
+      projectUUID: projectId,
+      stakeholderPayload: { uuid: stakeholderId },
+    });
+    router.push(`/projects/aa/${projectId}/stakeholders`);
+  };
   return (
     <div className="p-4 ">
       <div className="flex justify-between items-center p-4 ">
@@ -21,22 +36,38 @@ const StakeholdersDetail = () => {
           path={`/projects/aa/${projectId}/stakeholders`}
         />
         <div className=" flex flex-end justify-end gap-3 mt-auto">
-          <Button
+          {/* <Button
             variant="outline"
             style={{ borderColor: 'red' }}
             className="flex items-center gap-3 rounded-md w-36 text-red-600"
           >
             <Trash2 size={16} />
             <span className="text-lg font-thin">Delete</span>
-          </Button>
+          </Button> */}
 
-          <Button
+          <DeleteButton
+            className="rounded flex gap-1 items-center text-sm font-medium"
+            name="stakeholder"
+            label="Delete"
+            handleContinueClick={handleDelete}
+          />
+          <EditButton
+            className="rounded flex gap-1 items-center text-sm font-medium"
+            label="Edit"
+            onFallback={() =>
+              router.push(
+                `/projects/aa/${projectId}/stakeholders/${stakeholderId}/edit`,
+              )
+            }
+          />
+
+          {/* <Button
             variant="outline"
             className="flex  rounded-md  items-center gap-3 w-36"
           >
             <Edit2 size={16} />
             <span className="text-lg font-thin">Edit</span>
-          </Button>
+          </Button> */}
         </div>
       </div>
 

c986111afe245ee931c136913a0334660e6f60e6
karun-rumsan <gyawalikarrun@gmail.com>
2025-06-30 22:23:55 +0545

    Show label when voice messages fails

--- apps/rahat-ui/src/sections/projects/aa-2/communicationLog/table/useCommsLogsTableColumns.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/communicationLog/table/useCommsLogsTableColumns.tsx
@@ -1,7 +1,13 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { Badge } from '@rahat-ui/shadcn/src/components/ui/badge';
 import { BroadcastStatus } from '@rumsan/connect/src/types';
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@rahat-ui/shadcn/src/components/ui/tooltip';
+import { TriangleAlertIcon } from 'lucide-react';
 export default function useCommsLogsTableColumns() {
   const columns: ColumnDef<any>[] = [
     {
@@ -37,7 +43,41 @@ export default function useCommsLogsTableColumns() {
     {
       accessorKey: 'timeStamp',
       header: 'Timestamp',
-      cell: ({ row }) => <div>{renderDateTime(row?.original?.createdAt)}</div>,
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center space-x-2 gap-2">
+            {renderDateTime(row?.original?.createdAt)}
+            {row?.original?.status === 'FAIL' && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild className="hover:cursor-pointer py-0">
+                    <TriangleAlertIcon
+                      className="w-6 h-6 xl:w-4 xl:h-4  text-red-500"
+                      strokeWidth={2.5}
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="left"
+                    className="w-96 rounded-sm p-4 max-h-60 overflow-auto"
+                  >
+                    <div className="flex space-x-2 items-center">
+                      <TriangleAlertIcon
+                        size={16}
+                        strokeWidth={1.5}
+                        color="red"
+                      />
+                      <span className="font-semibold text-sm/6">Fail</span>
+                    </div>
+                    <p className="text-gray-500 text-sm mt-1 break-words">
+                      {row.original?.disposition?.disposition}
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </div>
+        );
+      },
     },
   ];
   return columns;

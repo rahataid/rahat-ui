842395f559d77f7f093dd5692e13bed0a85165cb
subash-rumsan <subhash.parajuli@rumsan.net>
2025-08-01 18:19:23 +0545

    fix: Source Reflected in Select Field While Editing Automated Trigger

--- apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/edit.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/edit.tsx
@@ -12,12 +12,16 @@ import {
 import { useParams, useRouter } from 'next/navigation';
 import { UUID } from 'crypto';
 import { capitalizeFirstLetter } from 'apps/rahat-ui/src/utils';
+import LoaderRahat from 'apps/rahat-ui/src/components/LoaderRahat';
 
 export default function EditTrigger() {
   const router = useRouter();
   const params = useParams();
   const projectId = params.id as UUID;
-  const triggerRepeatKey = window.location.href.split('/').slice(-2, -1)[0];
+  // const triggerRepeatKey = window.location.href.split('/').slice(-2, -1)[0];
+  const pathSegments = new URL(window.location.href).pathname.split('/');
+  const triggerIndex = pathSegments.indexOf('trigger-statements');
+  const triggerRepeatKey = pathSegments[triggerIndex + 1];
 
   const triggerDetailPage = `/projects/aa/${projectId}/trigger-statements/${triggerRepeatKey}`;
   const version = false;
@@ -211,18 +215,18 @@ export default function EditTrigger() {
   const automatedForm = useForm<z.infer<typeof AutomatedFormSchema>>({
     resolver: zodResolver(AutomatedFormSchema),
     defaultValues: {
-      title: '',
-      description: '',
-      source: '',
-      maxLeadTimeDays: '',
-      minLeadTimeDays: '',
-      probability: '',
-      isMandatory: false,
-      warningLevel: '',
-      dangerLevel: '',
-      forecast: '',
-      daysToConsiderPrior: '',
-      forecastStatus: '',
+      title: trigger?.title || '',
+      description: trigger?.description || '',
+      source: trigger?.source || '',
+      maxLeadTimeDays: trigger?.triggerStatement?.maxLeadTimeDays || '',
+      minLeadTimeDays: trigger?.triggerStatement?.minLeadTimeDays || '',
+      probability: trigger?.triggerStatement?.probability || '',
+      isMandatory: !trigger?.isMandatory || false,
+      warningLevel: trigger?.triggerStatement?.warningLevel || '',
+      dangerLevel: trigger?.triggerStatement?.dangerLevel || '',
+      forecast: trigger?.triggerStatement?.forecast || '',
+      daysToConsiderPrior: trigger?.triggerStatement?.daysToConsiderPrior || '',
+      forecastStatus: trigger?.triggerStatement?.forecastStatus || '',
     },
   });
 
@@ -289,7 +293,7 @@ export default function EditTrigger() {
         source: trigger?.source,
         isMandatory: !trigger?.isMandatory,
         maxLeadTimeDays: trigger?.triggerStatement?.maxLeadTimeDays,
-        minLeadTimeDays: trigger?.triggerStatement?.maxLeadTimeDays,
+        minLeadTimeDays: trigger?.triggerStatement?.minLeadTimeDays,
         description: trigger?.description || '',
         probability: trigger?.triggerStatement?.probability,
         warningLevel: trigger?.triggerStatement?.warningLevel,
@@ -301,61 +305,61 @@ export default function EditTrigger() {
     }
   }, [trigger]);
 
-  // return isLoading ? (
-  //   <TableLoader />
-  // ) :
   return (
-    <div className="p-4">
-      <Back />
-      <Heading
-        title={`Edit ${capitalizeFirstLetter(triggerType || '')} Trigger`}
-        description=""
-      />
-      <div className="px-4 pb-4 border rounded shadow">
-        {triggerType === 'automated' ? (
-          <AutomatedTriggerAddForm
-            form={automatedForm}
-            phase={{
-              name: trigger?.phase?.name,
-              riverBasin: trigger?.phase?.source?.riverBasin,
-            }}
-          />
-        ) : (
-          <ManualTriggerAddForm
-            form={manualForm}
-            phase={{
-              name: trigger?.phase?.name,
-              riverBasin: trigger?.phase?.source?.riverBasin,
-            }}
-          />
-        )}
-        <div className="flex justify-end mt-4">
-          <Button
-            type="button"
-            variant="outline"
-            className="w-40 mr-2"
-            onClick={() => {
-              if (triggerType === 'automated') {
-                automatedForm.reset();
-              } else {
-                manualForm.reset();
-              }
+    <>
+      {isLoading && <LoaderRahat />}
+      <div className={`p-4 ${isLoading && 'hidden'}`}>
+        <Back />
+        <Heading
+          title={`Edit ${capitalizeFirstLetter(triggerType || '')} Trigger`}
+          description=""
+        />
+        <div className="px-4 pb-4 border rounded shadow">
+          {triggerType === 'automated' ? (
+            <AutomatedTriggerAddForm
+              form={automatedForm}
+              phase={{
+                name: trigger?.phase?.name,
+                riverBasin: trigger?.phase?.source?.riverBasin,
+              }}
+            />
+          ) : (
+            <ManualTriggerAddForm
+              form={manualForm}
+              phase={{
+                name: trigger?.phase?.name,
+                riverBasin: trigger?.phase?.source?.riverBasin,
+              }}
+            />
+          )}
+          <div className="flex justify-end mt-4">
+            <Button
+              type="button"
+              variant="outline"
+              className="w-40 mr-2"
+              onClick={() => {
+                if (triggerType === 'automated') {
+                  automatedForm.reset();
+                } else {
+                  manualForm.reset();
+                }
 
-              // router.push(triggerDetailPage);
-            }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="submit"
-            className="w-40 mr-2"
-            onClick={handleEditTriggers}
-            disabled={updateTrigger?.isPending}
-          >
-            Update
-          </Button>
+                // router.push(triggerDetailPage);
+              }}
+            >
+              Reset
+            </Button>
+            <Button
+              type="submit"
+              className="w-40 mr-2"
+              onClick={handleEditTriggers}
+              disabled={updateTrigger?.isPending}
+            >
+              Update
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

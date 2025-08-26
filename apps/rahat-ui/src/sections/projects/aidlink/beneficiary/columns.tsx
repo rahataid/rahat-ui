efbaadd8fc2a54027eab2ff48b11a3b62531e1b8
amrit-rumsan <amrit@rumsan.net>
2025-08-26 08:11:25 +0545

    aidlink: project beneficiary list view

---
+++ apps/rahat-ui/src/sections/projects/aidlink/beneficiary/columns.tsx
@@ -0,0 +1,159 @@
+'use client';
+import * as React from 'react';
+import { useState } from 'react';
+import { ColumnDef } from '@tanstack/react-table';
+import { Copy, CopyCheck, Eye } from 'lucide-react';
+
+import { truncateEthAddress } from '@rumsan/sdk/utils/string.utils';
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from '@rahat-ui/shadcn/src/components/ui/tooltip';
+import { useParams, useRouter } from 'next/navigation';
+import useCopy from 'apps/rahat-ui/src/hooks/useCopy';
+
+export const useProjectBeneficiaryTableColumns = () => {
+  const router = useRouter();
+  const { id } = useParams();
+
+  const { clickToCopy, copyAction } = useCopy();
+
+  const columns: ColumnDef<any>[] = [
+    {
+      accessorKey: 'name',
+      header: 'Name',
+      cell: ({ row }) => <div> {row.getValue('name')}</div>,
+    },
+    {
+      accessorKey: 'phone',
+      header: 'Phone Number',
+      cell: ({ row }) => <div> {row.getValue('phone')}</div>,
+    },
+    {
+      accessorKey: 'wallet',
+      header: 'Wallet Address',
+      cell: ({ row }) => (
+        <TooltipProvider delayDuration={100}>
+          <Tooltip>
+            <TooltipTrigger
+              className="flex items-center gap-3 cursor-pointer"
+              onClick={() =>
+                clickToCopy(row?.original?.walletAddress, row?.original?.uuid)
+              }
+            >
+              <p>{truncateEthAddress(row?.original?.walletAddress)}</p>
+              {copyAction === row?.original?.uuid ? (
+                <CopyCheck size={15} strokeWidth={1.5} />
+              ) : (
+                <Copy className="text-slate-500" size={15} strokeWidth={1.5} />
+              )}
+            </TooltipTrigger>
+            <TooltipContent className="bg-secondary" side="bottom">
+              <p className="text-xs font-medium">
+                {copyAction === row?.original?.uuid
+                  ? 'copied'
+                  : 'click to copy'}
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ),
+    },
+    {
+      accessorKey: 'disbursedAmount',
+      header: 'Disbursed Amount',
+      cell: ({ row }) => <div> {row.getValue('disbursedAmount') || 'N/A'}</div>,
+    },
+    {
+      id: 'actions',
+      header: 'Action',
+      enableHiding: false,
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center gap-2">
+            <Eye
+              className="hover:text-primary cursor-pointer"
+              size={16}
+              strokeWidth={1.5}
+              onClick={() =>
+                router.push(
+                  `/projects/aa/${id}/beneficiary/${row.original.uuid}`,
+                )
+              }
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return columns;
+};
+
+export const useProjectBeneficiaryGroupDetailsTableColumns = () => {
+  const router = useRouter();
+  const { id, groupId } = useParams();
+
+  const { clickToCopy, copyAction } = useCopy();
+
+  const columns: ColumnDef<any>[] = [
+    {
+      accessorKey: 'walletAddress',
+      header: 'Wallet',
+      cell: ({ row }) => (
+        <TooltipProvider delayDuration={100}>
+          <Tooltip>
+            <TooltipTrigger
+              className="flex items-center gap-3 cursor-pointer"
+              onClick={() =>
+                clickToCopy(row?.original?.walletAddress, row?.original?.uuid)
+              }
+            >
+              <p>{truncateEthAddress(row?.original?.walletAddress)}</p>
+              {copyAction === row?.original?.uuid ? (
+                <CopyCheck size={15} strokeWidth={1.5} />
+              ) : (
+                <Copy className="text-slate-500" size={15} strokeWidth={1.5} />
+              )}
+            </TooltipTrigger>
+            <TooltipContent className="bg-secondary" side="bottom">
+              <p className="text-xs font-medium">
+                {copyAction === row?.original?.uuid
+                  ? 'copied'
+                  : 'click to copy'}
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ),
+    },
+
+    {
+      id: 'actions',
+      header: 'Action',
+      enableHiding: false,
+      cell: ({ row }) => {
+        console.log(row);
+
+        return (
+          <div className="flex items-center gap-2">
+            <Eye
+              className="hover:text-primary cursor-pointer"
+              size={16}
+              strokeWidth={1.5}
+              onClick={() =>
+                router.push(
+                  `/projects/aa/${id}/beneficiary/${row?.original?.benefId}?groupId=${groupId}`,
+                )
+              }
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return columns;
+};

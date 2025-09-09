01f3decb1acdd967a99ab9c716aa6d731dd8020c
anupama-rumsan <anupama@rumsan.net>
2025-09-09 14:47:51 +0545

    graph query updated and utils added for time conversion

--- libs/query/src/lib/c2c/subgraph/graph.query.ts
+++ libs/query/src/lib/c2c/subgraph/graph.query.ts
@@ -30,6 +30,18 @@ export const TransactionDetails = `
 }
 `;
 
+export const FilteredTransaction = `
+query FilteredTransaction($contractAddress: String, fromDate: String, toDate: String) {
+  transfers(where: {from: $contractAddress, blockTimestamp_gte: $fromDate, blockTimestamp_lte: $toDate}) {
+    blockTimestamp
+    from
+    id
+    to
+    transactionHash
+    value
+  }
+}`;
+
 export const ReceivedTransactionDetails = `
   query ReceivedTransactionDetails($contractAddress: String) {
     transfers(where: {to: $contractAddress}) {

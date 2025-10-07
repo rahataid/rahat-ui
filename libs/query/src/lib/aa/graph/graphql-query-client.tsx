fe2ea00df8bbc33eaaff450d55be36aac9757524
subash-rumsan <subhash.parajuli@rumsan.net>
2025-10-07 12:24:24 +0545

    update graphQl provide config

--- libs/query/src/lib/aa/graph/graphql-query-client.tsx
+++ libs/query/src/lib/aa/graph/graphql-query-client.tsx
@@ -1,16 +1,34 @@
 'use client';
-import { cacheExchange, Client, fetchExchange, Provider } from 'urql';
+import {
+  cacheExchange,
+  Client,
+  fetchExchange,
+  Provider,
+  ssrExchange,
+} from 'urql';
 import React from 'react';
 import { useProjectSettingsStore } from '../../projects';
 import { PROJECT_SETTINGS_KEYS } from 'libs/query/src/config';
 import { useParams } from 'next/navigation';
 import { UUID } from 'crypto';
 
+declare global {
+  interface Window {
+    __URQL_DATA__?: any;
+  }
+}
+
 const GarphQlProvider = ({ children }: any) => {
   const backupSubgraphURL =
     (process.env['NEXT_PUBLIC_AA_SUBGRAPH_URL'] as string) || '';
 
   const { id: uuid } = useParams();
+
+  const isServerSide = typeof window === 'undefined';
+  const ssr = ssrExchange({
+    isClient: !isServerSide,
+    initialState: !isServerSide ? window.__URQL_DATA__ : undefined,
+  });
 
   const subgraphURL =
     useProjectSettingsStore(
@@ -19,7 +37,7 @@ const GarphQlProvider = ({ children }: any) => {
 
   const client = new Client({
     url: subgraphURL || backupSubgraphURL,
-    exchanges: [cacheExchange, fetchExchange],
+    exchanges: [cacheExchange, fetchExchange, ssr],
   });
 
   return <Provider value={client}>{children}</Provider>;

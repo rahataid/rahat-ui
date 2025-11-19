17ff87b7f151c9a71d177ec6b48af16d27ec479f
subash-rumsan <subhash.parajuli@rumsan.net>
2025-11-19 11:19:22 +0545

    fix: updated dateFormate and refactor code of glofas.section component

--- apps/rahat-ui/src/sections/projects/aa-2/dataSources/components/glofas/glofas.section.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/dataSources/components/glofas/glofas.section.tsx
@@ -9,6 +9,7 @@ import { UUID } from 'crypto';
 import React from 'react';
 import { ScrollArea } from '@rahat-ui/shadcn/src/components/ui/scroll-area';
 import { NoResult, TableLoader } from 'apps/rahat-ui/src/common';
+import GlofasPeriodDataTable from './glofas.periodData.table';
 
 export function GlofasSection() {
   const params = useParams();
@@ -57,19 +58,6 @@ export function GlofasSection() {
     [glofasData],
   );
 
-  const getCellColor = (cell: string) => {
-    const cellValue = cell ? Number(cell) : 0;
-    if (cellValue >= 30 && cellValue < 50) {
-      return 'bg-yellow-100';
-    }
-    if (cellValue >= 50 && cellValue < 70) {
-      return 'bg-yellow-200';
-    }
-    if (cellValue >= 70) {
-      return 'bg-yellow-300';
-    }
-    return 'bg-green-50';
-  };
   if (isLoading) {
     return <TableLoader />;
   }
@@ -91,146 +79,23 @@ export function GlofasSection() {
         />
       </div>
 
-      <div className="bg-card p-4 rounded-sm border shadow mt-4">
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="font-semibold text-lg">{'ECMWF-ENS > 2 yr RP'}</h1>
-        </div>
-        <div className="overflow-auto">
-          <table className="min-w-full bg-white border border-gray-200">
-            <thead>
-              <tr>
-                {returnPeriodHeaders2yr?.map(
-                  (header: string, index: number) => (
-                    <th
-                      className="p-2 border border-gray-300 bg-gray-100 text-center text-xs font-semibold text-gray-600"
-                      key={index}
-                    >
-                      {header}
-                    </th>
-                  ),
-                )}
-              </tr>
-            </thead>
-            <tbody>
-              {returnPeriodData2yr?.map((row: string[], rowIndex: number) => (
-                <tr key={rowIndex}>
-                  {row.map((cell: string, cellIndex: number) => {
-                    let bgColor;
-                    if (cellIndex > 0) {
-                      bgColor = getCellColor(cell);
-                    }
+      <GlofasPeriodDataTable
+        headerData={returnPeriodHeaders2yr}
+        bodyData={returnPeriodData2yr}
+        title="ECMWF-ENS > 2 yr RP"
+      />
 
-                    return (
-                      <td
-                        className={`p-2 border border-gray-200 text-center text-sm text-gray-700 ${bgColor}`}
-                        key={cellIndex}
-                      >
-                        {cell}
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <GlofasPeriodDataTable
+        headerData={returnPeriodHeaders5yr}
+        bodyData={returnPeriodData5yr}
+        title="ECMWF-ENS > 5 yr RP"
+      />
 
-      {returnPeriodHeaders5yr && returnPeriodData5yr && (
-        <div className="bg-card p-4 rounded-sm border shadow mt-4">
-          <div className="flex justify-between items-center mb-4">
-            <h1 className="font-semibold text-lg">{'ECMWF-ENS > 5 yr RP'}</h1>
-          </div>
-          <div className="overflow-auto">
-            <table className="min-w-full bg-white border border-gray-200">
-              <thead>
-                <tr>
-                  {returnPeriodHeaders5yr?.map(
-                    (header: string, index: number) => (
-                      <th
-                        className="p-2 border border-gray-300 bg-gray-100 text-center text-xs font-semibold text-gray-600"
-                        key={index}
-                      >
-                        {header}
-                      </th>
-                    ),
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {returnPeriodData5yr?.map((row: string[], rowIndex: number) => (
-                  <tr key={rowIndex}>
-                    {row.map((cell: string, cellIndex: number) => {
-                      let bgColor;
-                      if (cellIndex > 0) {
-                        bgColor = getCellColor(cell);
-                      }
-
-                      return (
-                        <td
-                          className={`p-2 border border-gray-200 text-center text-sm text-gray-700 ${bgColor}`}
-                          key={cellIndex}
-                        >
-                          {cell}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
-
-      {returnPeriodHeaders20yr && returnPeriodData20yr && (
-        <div className="bg-card p-4 rounded-sm border shadow mt-4">
-          <div className="flex justify-between items-center mb-4">
-            <h1 className="font-semibold text-lg">{'ECMWF-ENS > 20 yr RP'}</h1>
-          </div>
-          <div className="overflow-auto">
-            <table className="min-w-full bg-white border border-gray-200">
-              <thead>
-                <tr>
-                  {returnPeriodHeaders20yr?.map(
-                    (header: string, index: number) => (
-                      <th
-                        className="p-2 border border-gray-300 bg-gray-100 text-center text-xs font-semibold text-gray-600"
-                        key={index}
-                      >
-                        {header}
-                      </th>
-                    ),
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {returnPeriodData20yr?.map(
-                  (row: string[], rowIndex: number) => (
-                    <tr key={rowIndex}>
-                      {row.map((cell: string, cellIndex: number) => {
-                        let bgColor;
-                        if (cellIndex > 0) {
-                          bgColor = getCellColor(cell);
-                        }
-
-                        return (
-                          <td
-                            className={`p-2 border border-gray-200 text-center text-sm text-gray-700 ${bgColor}`}
-                            key={cellIndex}
-                          >
-                            {cell}
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  ),
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
+      <GlofasPeriodDataTable
+        headerData={returnPeriodHeaders20yr}
+        bodyData={returnPeriodData20yr}
+        title="ECMWF-ENS > 20 yr RP"
+      />
     </ScrollArea>
   );
 }

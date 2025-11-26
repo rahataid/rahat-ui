8fb70aba06c54640fb4d8573483ad92e94df66f4
amrit-rumsan <amrit@rumsan.net>
2025-11-26 19:07:02 +0545

    fix: triggers forecast detail

--- apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/components/detail.forecast.data.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/triggerStatement/components/detail.forecast.data.tsx
@@ -1,5 +1,8 @@
+import { Badge } from '@rahat-ui/shadcn/src/components/ui/badge';
 import { Heading } from 'apps/rahat-ui/src/common';
 import { TriangleAlert } from 'lucide-react';
+import { SOURCE_CONFIG } from '../trigger.statement.schema';
+import { capitalizeFirstLetter } from 'apps/rahat-ui/src/utils';
 
 type IProps = {
   source: string;
@@ -12,14 +15,38 @@ export function ForecastDataSection({
   phase,
   triggerStatement,
 }: IProps) {
+  const {
+    value,
+    source: triggerSource,
+    operator,
+    expression,
+    sourceSubType,
+  } = triggerStatement;
+  const sourceSubTypeLabel =
+    SOURCE_CONFIG[triggerSource as keyof typeof SOURCE_CONFIG]?.sourceSubType;
+  const unit = sourceSubTypeLabel?.match(/\((.*?)\)/)?.[1] || '';
+  const formattedSourceSubType = sourceSubType?.replace('_', ' ');
+
   return (
     <div className="p-4 border rounded-sm shadow">
       <Heading
         title="Forecast Data"
         titleStyle="text-lg/7"
-        description={`Source: ${source}`}
+        description={`Source: ${source} - ${sourceSubTypeLabel}`}
       />
-      {source === 'GLOFAS' && (
+      <div className="p-3 text-center border rounded">
+        <p className="font-semibold text-3xl/10 text-primary">
+          {value} {unit}
+        </p>
+        <p className="font-medium text-sm/6 flex justify-center items-center gap-2">
+          <TriangleAlert size={16} strokeWidth={2.5} color="red" />
+          {capitalizeFirstLetter(formattedSourceSubType || '')}
+        </p>
+        <Badge className="font-normal">
+          {formattedSourceSubType} {operator} {value} {unit}
+        </Badge>
+      </div>
+      {/* {source === 'GLOFAS' && (
         <div className="grid grid-cols-3 gap-4">
           <div className="p-3 text-center border rounded">
             <p className="font-semibold text-3xl/10 text-primary">
@@ -89,7 +116,7 @@ export function ForecastDataSection({
             <p className="font-medium text-sm/6">Forecast Status</p>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }

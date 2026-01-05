8cc3a13348477ef49b6243f9f48a5c258be97338
subash-rumsan <subhash.parajuli@rumsan.net>
2026-01-05 10:28:00 +0545

    fix: resolved grievances list window while change the tab

--- apps/rahat-ui/src/sections/projects/aa-2/grievances/list/grievances.tabs.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/grievances/list/grievances.tabs.tsx
@@ -7,12 +7,26 @@ import {
 } from 'libs/shadcn/src/components/ui/tabs';
 import GrievanceOverview from './grievances.overview';
 import GrievancesTable from './grievances.table';
+import { useSecondPanel } from 'apps/rahat-ui/src/providers/second-panel-provider';
 
 export default function GrievancesTabs() {
+  const { closeSecondPanel } = useSecondPanel();
   const { activeTab, setActiveTab } = useActiveTab('overview');
+
+  const handleTabChange = (value: string) => {
+    if (value === 'overview') {
+      closeSecondPanel();
+    }
+    setActiveTab(value);
+  };
+
   return (
     <div>
-      <Tabs defaultValue={activeTab} onValueChange={setActiveTab}>
+      <Tabs
+        defaultValue={activeTab}
+        value={activeTab}
+        onValueChange={handleTabChange}
+      >
         <TabsList className="border bg-secondary rounded pb-2">
           <TabsTrigger
             className="w-full data-[state=active]:bg-white"

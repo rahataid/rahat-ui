6083cecbeaaf406ef726ef92caa9508d03cbe72d
Akash-rumsan <akash.sunar@rumsan.net>
2026-03-10 15:10:32 +0545

    tabs continue

--- apps/rahat-ui/src/sections/projects/aa-2/payout/main.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/payout/main.tsx
@@ -20,6 +20,7 @@ import {
   TabsTrigger,
 } from '@rahat-ui/shadcn/src/components/ui/tabs';
 import { useActiveTab } from 'apps/rahat-ui/src/utils/useActivetab';
+import PayoutTransactionList from './table/payoutTransactionList';
 
 export default function PayoutView() {
   const params = useParams();
@@ -58,33 +59,12 @@ export default function PayoutView() {
 
   return (
     <div className="p-4 ">
-      <Heading title="Payout" description="Track all the payout reports here" />
-      <div className="flex justify-between items-center space-x-4">
-        <Tabs defaultValue={activeTab} onValueChange={setActiveTab}>
-          <TabsList className="border bg-secondary rounded">
-            <TabsTrigger
-              className="w-full data-[state=active]:bg-white"
-              value="vendorList"
-            >
-              Payout
-            </TabsTrigger>
-            <TabsTrigger
-              className="w-full data-[state=active]:bg-white"
-              value="vendorRedemptionList"
-            >
-              Payout List
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="payout">
-            {/* <VendorList id={id} /> */}
-            <h1>Hello from Payout</h1>
-          </TabsContent>
-          <TabsContent value="payoutList">
-            {/* <VendorRedemptionList id={id} /> */}
-            <h1>Hello from Payout List</h1>
-          </TabsContent>
-        </Tabs>
-        <div className="flex flex-end gap-2">
+      <div className="flex justify-between">
+        <Heading
+          title="Payout"
+          description="Track all the payout reports here"
+        />
+        <div className="flex flex-end gap-2 items-center">
           <RoleAuth
             roles={[AARoles.ADMIN, AARoles.Municipality]}
             hasContent={false}
@@ -101,63 +81,101 @@ export default function PayoutView() {
           </RoleAuth>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
-        {payoutStats.map((stat) => {
-          return (
-            <DataCard
-              key={stat.label}
-              title={stat.label}
-              number={stat.value as string}
-              className="rounded-sm h-32"
-              infoIcon={stat.infoIcon}
-              infoTooltip={stat.infoTooltip}
-            />
-          );
-        })}
-      </div>
-      <div className="flex flex-wrap mt-4 gap-4">
-        <div className="flex-1 border rounded-sm p-4">
-          <h1 className="text-lg font-medium mb-4">Payout Type</h1>
-          <div className="w-full aspect-square">
-            <DynamicPieChart
-              pieData={[
-                {
-                  label: 'FSP',
-                  value: statsPayout?.payoutOverview?.payoutTypes?.FSP || 0,
-                },
-                {
-                  label: 'CVA',
-                  value: statsPayout?.payoutOverview?.payoutTypes?.VENDOR || 0,
-                },
-              ]}
-              colors={['#F4A462', '#2A9D90']}
-            />
-          </div>
-        </div>
+      <div className="flex justify-between items-center space-x-4 rounded-md p-4 border">
+        <Tabs defaultValue={activeTab} onValueChange={setActiveTab}>
+          <TabsList className="border bg-secondary rounded">
+            <TabsTrigger
+              className="w-full data-[state=active]:bg-white"
+              value="payout"
+            >
+              Payout
+            </TabsTrigger>
+            <TabsTrigger
+              className="w-full data-[state=active]:bg-white"
+              value="payoutList"
+            >
+              Payout List
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="payout">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
+              {payoutStats.map((stat) => {
+                return (
+                  <DataCard
+                    key={stat.label}
+                    title={stat.label}
+                    number={stat.value as string}
+                    className="rounded-sm h-32"
+                    infoIcon={stat.infoIcon}
+                    infoTooltip={stat.infoTooltip}
+                  />
+                );
+              })}
+            </div>
+            <div className="flex flex-wrap mt-4 gap-4">
+              <div className="flex-1 border rounded-sm p-4">
+                <h1 className="text-lg font-medium mb-4">Payout Type</h1>
+                <div className="w-full aspect-square">
+                  <DynamicPieChart
+                    pieData={[
+                      {
+                        label: 'FSP',
+                        value:
+                          statsPayout?.payoutOverview?.payoutTypes?.FSP || 0,
+                      },
+                      {
+                        label: 'CVA',
+                        value:
+                          statsPayout?.payoutOverview?.payoutTypes?.VENDOR || 0,
+                      },
+                    ]}
+                    colors={['#F4A462', '#2A9D90']}
+                  />
+                </div>
+              </div>
 
-        <div className="flex-1 border rounded-sm p-4">
-          <h1 className="text-lg font-medium mb-4">Payout Status</h1>
-          <div className="w-full aspect-square">
-            <DynamicPieChart
-              pieData={[
-                {
-                  label: 'Success',
-                  value:
-                    statsPayout?.payoutOverview?.payoutStatus?.SUCCESS || 0,
-                },
-                {
-                  label: 'Failed',
-                  value: statsPayout?.payoutOverview?.payoutStatus?.FAILED || 0,
-                },
-              ]}
-              colors={['#2A9D90', '#DC3545']}
-            />
-          </div>
-        </div>
+              <div className="flex-1 border rounded-sm p-4">
+                <h1 className="text-lg font-medium mb-4">Payout Status</h1>
+                <div className="w-full aspect-square">
+                  <DynamicPieChart
+                    pieData={[
+                      {
+                        label: 'Success',
+                        value:
+                          statsPayout?.payoutOverview?.payoutStatus?.SUCCESS ||
+                          0,
+                      },
+                      {
+                        label: 'Failed',
+                        value:
+                          statsPayout?.payoutOverview?.payoutStatus?.FAILED ||
+                          0,
+                      },
+                    ]}
+                    colors={['#2A9D90', '#DC3545']}
+                  />
+                </div>
+              </div>
 
-        <div className="flex-[2] border rounded-sm p-4">
-          <RecentPayout payouts={payouts?.data} />
-        </div>
+              <div className="flex-[2] border rounded-sm p-4">
+                <RecentPayout payouts={payouts?.data} />
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="payoutList">
+            {/* <VendorRedemptionList id={id} /> */}
+            <RoleAuth
+              roles={[
+                AARoles.ADMIN,
+                AARoles.MANAGER,
+                AARoles.Municipality,
+                AARoles.UNICEFNepalCO,
+              ]}
+            >
+              <PayoutTransactionList />
+            </RoleAuth>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );

ba6b0cccb073cf53d8cdaa75111a39b6bc3910e0
mohit-rumsan <mohit.rajbhandari@rumsan.net>
2026-03-24 15:00:26 +0545

    [#1900] [INKIND-MANAGEMENT] Render inkind management UI and data flow (#1905)
    * Feat: Create new route for inkind handling with assignment and register
    
    * Redesign allocation list
    
    * Integrate inkind api and fix api value render
    
    * Fix grammers
    
    * Optimize code and implement clean code policy
    
    * Remove unncessary comments
    
    * Add comments
    
    * Add zod validation
    
    * Fix import error
    
    * Fix modal
    
    * Fix UI colors and add validations
    
    * Add tag for transaction flow
    
    * Review ui with theme changes as per QA
    
    * Add similar tags, make ui semantic across the page and rename btn texts
    
    * Add onValue in type form to save context on back press
    
    * Resolve quantity validation for assign, and cancel redirection
    
    * Add truncation to table data, add data cards as requirements
    
    * Add tooltip for assign inkinds btn and fix transactions error texts
    
    * Final text changes

---
+++ apps/rahat-ui/src/sections/projects/aa-2/inkindManagement/components/index.ts
@@ -0,0 +1,8 @@
+export { default as InkindList } from './inkind.list';
+export { default as InkindOverview } from './inkind.overview';
+export { default as InkindTabs } from './inkind.tabs';
+export { default as InkindUpdateSheet } from './inkind.update.sheet';
+export { default as InkindAllocationList } from './inkind.allocation.list';
+export { default as InkindAllocationDetail } from './inkind.allocation.detail';
+export { default as InkindTransactionDetail } from './inkind.transaction.detail';
+export { default as InkindInfoItem } from './inkind.info.item';
--- apps/rahat-ui/src/sections/projects/aa-2/stakeholders/component/TruncatedCell.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/stakeholders/component/TruncatedCell.tsx
@@ -36,7 +36,7 @@ export function TruncatedCell({
           </span>
         </TooltipTrigger>
         <TooltipContent className={cn('rounded-sm', className)}>
-          <p className=" break-words ">{text}</p>
+          <p className="whitespace-pre-line flex wrap break-words max-w-[20vw]">{text}</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>

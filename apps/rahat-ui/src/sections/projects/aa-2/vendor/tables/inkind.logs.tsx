e3d6498f6b2587c5c76bd97b81e60e86bbec09c6
Akash-rumsan <akash.sunar@rumsan.net>
2026-04-09 14:33:23 +0545

    integration in inkind logs

--- apps/rahat-ui/src/sections/projects/aa-2/vendor/tables/inkind.logs.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/vendor/tables/inkind.logs.tsx
@@ -9,66 +9,90 @@ import { SearchInput } from 'apps/rahat-ui/src/common/search.input';
 import { CustomPagination, Heading } from 'apps/rahat-ui/src/common';
 import { usePagination } from '@rahat-ui/query/utils/use-pagination';
 import CopyTooltip from 'apps/rahat-ui/src/common/copyTooltip';
+import { useParams } from 'next/navigation';
+import { useLogsDetailsByVendor } from '@rahat-ui/query';
+import { UUID } from 'crypto';
+import Loader from 'apps/community-tool-ui/src/components/Loader';
+import { TruncatedCell } from '../../stakeholders/component/TruncatedCell';
+import { dateFormat } from 'apps/rahat-ui/src/utils/dateFormate';
+import { useDebounce } from '@rahat-ui/shadcn/src/components/custom/multi-select';
 
-const mockData = {
-  success: true,
-  data: [
-    {
-      uuid: '1',
-      walletAddress: '1A1zP1eP5Q...Gefi2',
-      groupName: 'Aashman Group',
-      inKindName: 'Aashman In-kind',
-      inKindQuantity: '1,000',
-      txHash: '1A1zP1eP5Q...Gefi2',
-      timestamp: '2024-01-01 12:00:00',
-    },
-    {
-      uuid: '2',
-      walletAddress: '1B1zP1eP5Q...Gefi2',
-      groupName: 'Akash Group',
-      inKindName: 'Akash In-kind',
-      inKindQuantity: '1,000',
-      txHash: '1B1zP1eP5Q...Gefi2',
-      timestamp: '2024-01-01 12:00:00',
-    },
-  ],
-  meta: {
-    total: 2,
-    lastPage: 1,
-    currentPage: 1,
-    perPage: 10,
-    prev: null,
-    next: null,
-  },
-};
+interface InKindLog {
+  uuid: UUID;
+  beneficiaryWallet: string;
+  groupInkindId: UUID;
+  quantity: number;
+  redeemedAt: string;
+  txHash: string | null;
+  vendorUid: UUID;
+  beneficiary: {
+    uuid: UUID;
+    walletAddress: string;
+    phone: string | null;
+    extras: {
+      phone: string;
+      validPhoneNumber: boolean;
+    };
+  };
+  groupInkind: {
+    uuid: UUID;
+    inkind: {
+      uuid: UUID;
+      name: string;
+      type: string;
+    };
+    group: {
+      uuid: UUID;
+      name: string;
+    };
+  };
+}
 
-const columns: ColumnDef<any>[] = [
+const columns: ColumnDef<InKindLog>[] = [
   {
     accessorKey: 'groupName',
     header: 'Group Name',
+    cell: ({ row }) => (
+      <TruncatedCell
+        text={row.original.groupInkind.group.name}
+        maxLength={20}
+      />
+    ),
   },
 
   {
-    accessorKey: 'walletAddress',
+    accessorKey: 'beneficiaryWallet',
     header: 'Beneficiary Wallet Address',
     cell: ({ row }) => (
       <div className="flex gap-2 items-center">
-        {row.original.walletAddress}
+        <TruncatedCell text={row.original.beneficiaryWallet} maxLength={10} />
         <CopyTooltip
-          value={row.getValue('walletAddress')}
+          value={row.getValue('beneficiaryWallet')}
           uniqueKey={row.original?.uuid}
         />
       </div>
     ),
   },
-  { accessorKey: 'inKindName', header: 'In-kind Name' },
-  { accessorKey: 'inKindQuantity', header: 'In-kind Quantity' },
+  {
+    accessorKey: 'inKindName',
+    header: 'In-kind Name',
+    cell: ({ row }) => (
+      <TruncatedCell
+        text={row.original.groupInkind.inkind.name}
+        maxLength={15}
+      />
+    ),
+  },
+  {
+    accessorKey: 'quantity',
+    header: 'In-kind Quantity',
+  },
   {
     accessorKey: 'txHash',
     header: 'TxHash',
     cell: ({ row }) => (
       <div className="flex gap-2 items-center">
-        {row.original.txHash}
+        <TruncatedCell text={row.original.txHash || 'N/A'} maxLength={10} />
         <CopyTooltip
           value={row.getValue('txHash')}
           uniqueKey={row.original?.uuid}
@@ -76,10 +100,23 @@ const columns: ColumnDef<any>[] = [
       </div>
     ),
   },
-  { accessorKey: 'timestamp', header: 'Timestamp' },
+  {
+    accessorKey: 'redeemedAt',
+    header: 'Timestamp',
+    cell: ({ row }) => (
+      <TruncatedCell
+        text={
+          row?.original?.redeemedAt ? dateFormat(row?.original?.redeemedAt) : ''
+        }
+        maxLength={10}
+      />
+    ),
+  },
 ];
 
 export default function InKindLogs() {
+  const { id, vendorId }: { id: UUID; vendorId: UUID } = useParams();
+
   const {
     pagination,
     setNextPage,
@@ -87,19 +124,30 @@ export default function InKindLogs() {
     setPerPage,
     filters,
     setFilters,
-    setPagination,
   } = usePagination();
 
-  const [search, setSearch] = React.useState('');
-  const filteredData = React.useMemo(
-    () =>
-      mockData.data.filter((row) =>
-        row.walletAddress.toLowerCase().includes(search.toLowerCase()),
-      ),
-    [search],
+  const debounceSearch = useDebounce(filters, 500);
+
+  const { data: logsData, isLoading } = useLogsDetailsByVendor({
+    projectUuid: id,
+    vendorId: vendorId,
+    walletAddress: debounceSearch.walletAddress,
+    page: pagination.page,
+    perPage: pagination.perPage,
+    search: debounceSearch.walletAddress,
+  });
+
+  const handleSearch = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement> | null, key: string) => {
+      const value = event?.target?.value ?? '';
+      setFilters({ ...filters, [key]: value });
+    },
+    [filters],
   );
+
   const table = useReactTable({
-    data: filteredData,
+    manualPagination: true,
+    data: logsData?.data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
@@ -111,22 +159,43 @@ export default function InKindLogs() {
         description="List of all in-kind transactions"
       />
 
-      <SearchInput
-        name="walletAddress"
-        value={search}
-        onSearch={(e) => setSearch(e.target.value)}
-        placeholder="Search wallet"
-      />
-      <DemoTable table={table} />
-      <CustomPagination
-        currentPage={pagination.page}
-        handleNextPage={setNextPage}
-        handlePrevPage={setPrevPage}
-        handlePageSizeChange={setPerPage}
-        meta={mockData.meta || { total: 0, currentPage: 0 }}
-        perPage={pagination?.perPage}
-        total={mockData.meta?.total || 0}
-      />
+      {isLoading ? (
+        <div className="w-full">
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <SearchInput
+            name="walletAddress"
+            onSearch={(e) => handleSearch(e, 'walletAddress')}
+            value={filters?.walletAddress || ''}
+            placeholder="Search wallet"
+          />
+          <DemoTable
+            table={table}
+            tableHeight={'h-[calc(400px)]'}
+            loading={isLoading}
+          />
+          <CustomPagination
+            currentPage={pagination.page}
+            handleNextPage={setNextPage}
+            handlePrevPage={setPrevPage}
+            handlePageSizeChange={setPerPage}
+            meta={
+              (logsData?.meta as any) || {
+                total: 0,
+                lastPage: 0,
+                currentPage: 0,
+                perPage: 0,
+                prev: null,
+                next: null,
+              }
+            }
+            perPage={pagination?.perPage}
+            total={logsData?.meta?.total || 0}
+          />
+        </>
+      )}
     </div>
   );
 }

2b6218578c6815327dd372e9a217a1d48cd3343f
Akash-rumsan <akash.sunar@rumsan.net>
2026-04-22 18:12:00 +0545

    transaction histoty column  added

---
+++ apps/rahat-ui/src/sections/projects/aa-2/fundManagement/columns/useTokenTransactionHistory.tsx
@@ -0,0 +1,67 @@
+import { Hash } from 'lucide-react';
+import { TruncatedCell } from '../../stakeholders/component/TruncatedCell';
+import CopyTooltip from 'apps/rahat-ui/src/common/copyTooltip';
+import { ColumnDef } from '@tanstack/react-table';
+
+interface TokenTransaction {
+  uuid: string;
+  transactionHash: string;
+  from: string;
+  to: string;
+  blockNumber: number;
+  value: string;
+  blockTimestamp: string;
+}
+export const useTokenTransactionHistory = () => {
+
+  const columns: ColumnDef<TokenTransaction>[] = [
+    {
+      header: 'Transaction Hash',
+      accessorKey: 'transactionHash',
+      cell: ({ row }) => (
+        <div className="flex items-center gap-2">
+          <TruncatedCell text={row.original?.transactionHash} maxLength={15} />
+          <CopyTooltip
+            value={row.original?.transactionHash}
+            uniqueKey={row.original?.uuid}
+          />
+        </div>
+      ),
+    },
+    {
+      header: 'From',
+      accessorKey: 'from',
+      cell: ({ row }) => (
+        <div className="flex items-center gap-2">
+          <TruncatedCell text={row.original?.from} maxLength={15} />
+          <CopyTooltip
+            value={row.original?.from}
+            uniqueKey={row.original?.uuid}
+          />
+        </div>
+      ),
+    },
+    {
+      header: 'To',
+      accessorKey: 'to',
+      cell: ({ row }) => (
+        <div className="flex items-center gap-2">
+          <TruncatedCell text={row.original?.to} maxLength={15} />
+          <CopyTooltip
+            value={row.original?.to}
+            uniqueKey={row.original?.uuid}
+          />
+        </div>
+      ),
+    },
+    {
+      header: 'Amount',
+      accessorKey: 'value',
+    },
+    {
+      header: 'Date',
+      accessorKey: 'blockTimestamp',
+    },
+  ];
+  return columns;
+};

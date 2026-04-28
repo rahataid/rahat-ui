72b4f4b99dc3bdcce7249aedb0179926de195a2a
mohit-rumsan <mohit.rajbhandari@rumsan.net>
2026-04-28 15:01:28 +0545

    Fix query refetch issue

--- apps/rahat-ui/src/providers/query-provider.tsx
+++ apps/rahat-ui/src/providers/query-provider.tsx
@@ -3,23 +3,27 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider } from 'connectkit';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface QueryProviderProps {
   children: React.ReactNode;
 }
 
 export const QueryProvider = ({ children }: QueryProviderProps) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        retry: false,
-      },
-      mutations: {
-        retry: false,
-      },
-    },
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+          },
+          mutations: {
+            retry: false,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>

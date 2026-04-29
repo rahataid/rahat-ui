4f8f2f18245b275290d27def935244c984f5d8f1
Akash-rumsan <akash.sunar@rumsan.net>
2026-04-29 16:33:22 +0545

    redirect to sepolia integrated

--- apps/rahat-ui/src/sections/projects/aa-2/fundManagement/columns/useTokenTransactionHistory.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/fundManagement/columns/useTokenTransactionHistory.tsx
@@ -1,8 +1,14 @@
-import { Hash } from 'lucide-react';
 import { TruncatedCell } from '../../stakeholders/component/TruncatedCell';
 import CopyTooltip from 'apps/rahat-ui/src/common/copyTooltip';
 import { ColumnDef } from '@tanstack/react-table';
 import { dateFormat } from 'apps/rahat-ui/src/utils/dateFormate';
+import { getExplorerUrl } from 'apps/rahat-ui/src/utils';
+import {
+  PROJECT_SETTINGS_KEYS,
+  useProjectSettingsStore,
+} from '@rahat-ui/query';
+import { useParams } from 'next/navigation';
+import { UUID } from 'crypto';
 
 interface TokenTransaction {
   uuid: string;
@@ -14,19 +20,39 @@ interface TokenTransaction {
   blockTimeStamp: string;
 }
 export const useTokenTransactionHistory = () => {
+  const params = useParams();
+  const projectId = params.id as UUID;
+
+  const { settings } = useProjectSettingsStore((s) => ({
+    settings: s.settings,
+  }));
+
   const columns: ColumnDef<TokenTransaction>[] = [
     {
       header: 'Transaction Hash',
       accessorKey: 'transactionHash',
-      cell: ({ row }) => (
-        <div className="flex items-center gap-2">
-          <TruncatedCell text={row.original?.transactionHash} maxLength={15} />
-          <CopyTooltip
-            value={row.original?.transactionHash}
-            uniqueKey={row.original?.uuid}
-          />
-        </div>
-      ),
+      cell: ({ row }) => {
+        const txnUrl = getExplorerUrl({
+          chainSettings:
+            settings?.[projectId]?.[PROJECT_SETTINGS_KEYS.CHAIN_SETTINGS],
+          target: 'tx',
+          value: row.original?.transactionHash,
+        });
+        return (
+          <div className="flex items-center gap-2">
+            <a href={txnUrl || '#'} target="_blank" rel="noopener noreferrer">
+              <TruncatedCell
+                text={row.original?.transactionHash}
+                maxLength={15}
+              />
+            </a>
+            <CopyTooltip
+              value={row.original?.transactionHash}
+              uniqueKey={row.original?.uuid}
+            />
+          </div>
+        );
+      },
     },
     {
       header: 'From',

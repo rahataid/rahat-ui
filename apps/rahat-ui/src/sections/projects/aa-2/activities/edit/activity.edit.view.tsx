1cabd0d7fa8b67a4d6da2cb4062d9a745369141d
nabina-rumsan <nabina.rumsan@gmail.com>
2026-06-30 17:11:41 +0545

    fix(activity): prevent activity update until communication changes are saved

--- apps/rahat-ui/src/sections/projects/aa-2/activities/edit/activity.edit.view.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/activities/edit/activity.edit.view.tsx
@@ -124,8 +124,9 @@ export default function EditActivity() {
 
   const redirectUpdatePath = redirectTo
     ? `/projects/aa/${projectID}/activities/${activityID}`
-    : `/projects/aa/${projectID}/activities/${activityID}${backFrom ? `?from=${backFrom}` : ''
-    }`;
+    : `/projects/aa/${projectID}/activities/${activityID}${
+        backFrom ? `?from=${backFrom}` : ''
+      }`;
 
   const { FormSchema, form, communicationForm, defaultCommunicationValues } =
     useActivityForm(appTransports);
@@ -415,7 +416,9 @@ export default function EditActivity() {
                         updateActivity?.isPending ||
                         uploadFile?.isPending ||
                         audioUploading ||
-                        open
+                        open ||
+                        editCommunicationForm.value ||
+                        !!form.formState.errors.responsibility
                       }
                     >
                       Update
@@ -698,7 +701,7 @@ export default function EditActivity() {
                               className="bg-white shadow-sm rounded-xl p-4 border border-gray-200 flex items-center gap-3 hover:cursor-pointer hover:bg-gray-100"
                             >
                               {uploadFile.isPending &&
-                                uploadingFileName === file.fileName ? (
+                              uploadingFileName === file.fileName ? (
                                 <LoaderCircle className="text-green-600 animate-spin w-9 h-9" />
                               ) : (
                                 <FileCheck className="w-9 h-9 text-green-600" />

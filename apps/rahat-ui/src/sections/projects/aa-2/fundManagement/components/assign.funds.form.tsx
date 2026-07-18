0e22246e1fe9de18b6bf1528f14d6da99dc2a712
subash-rumsan <subhash.parajuli@rumsan.net>
2026-07-18 21:29:17 +0545

    display warning message if there any fiatRedeemNotCompleted data avalible

--- apps/rahat-ui/src/sections/projects/aa-2/fundManagement/components/assign.funds.form.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/fundManagement/components/assign.funds.form.tsx
@@ -133,6 +133,16 @@ export default function AssignFundsForm({
       return;
     }
 
+    if (validationResult?.fiatRedeemNotCompleted?.length > 0) {
+      setErrorData(validationResult);
+      errorModule.onTrue();
+      return;
+    }
+
+    proceedToNextStep(data);
+  };
+
+  const proceedToNextStep = (data: FundAssignmentFormValues) => {
     const selectedGroup = benGroups?.data.find(
       (group) => group.uuid === data.beneficiaryGroupId,
     );
@@ -325,7 +335,14 @@ export default function AssignFundsForm({
           </div>
         </div>
       </form>
-      <ErrorInfoPopupModel validateModal={errorModule} errorData={errorData} />
+      <ErrorInfoPopupModel
+        validateModal={errorModule}
+        errorData={errorData}
+        onContinue={() => {
+          errorModule.onFalse();
+          proceedToNextStep(form.getValues());
+        }}
+      />
     </Form>
   );
 }

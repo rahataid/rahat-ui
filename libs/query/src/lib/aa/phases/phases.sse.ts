917a0ef2f2c1b96e513dd41196c1ac5879196ed8
Akash-rumsan <akash.rumsan@gmail.com>
2026-07-25 23:26:38 +0545

    sse implemented

--- libs/query/src/lib/aa/phases/phases.sse.ts
+++ libs/query/src/lib/aa/phases/phases.sse.ts
@@ -1,27 +1,31 @@
 'use client';
 
-import { useSSE } from 'libs/query/src/utils/use-sse';
+import { useQueryClient } from '@tanstack/react-query';
+import { useAuthStore } from '@rumsan/react-query';
+// import { useSSE } from '../../utils/use-sse';
 import { usePhasesStore } from './phases.store';
+import { useSSE } from 'libs/query/src/utils/use-sse';
 
 export function usePhaseSSE(projectId: string) {
+  const qc = useQueryClient();
   const addPhase = usePhasesStore((s) => s.addPhase);
   const updatePhase = usePhasesStore((s) => s.updatePhase);
 
   useSSE({
-    url: `http://localhost:5500/v1/events/phases`,
+    url: `${process.env.NEXT_PUBLIC_API_HOST_URL}/v1/events/phases`,
+    // getToken: () => useAuthStore.getState().token,
     onMessage: (event) => {
       const { event: type, data } = event;
-
+      console.log('type:', type);
       if (type === 'phase.created') {
-        console.log('new phase added');
         addPhase(data);
       }
 
       if (type === 'phase.updated') {
-        console.log('phase edited');
-
         updatePhase(data.uuid, data);
       }
+
+      qc.invalidateQueries({ queryKey: ['phases', projectId] });
     },
   });
 

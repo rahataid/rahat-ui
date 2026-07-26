84eedef0bf51a46f09f9abe86b0f7c5539e1b1a1
Akash-rumsan <akash.rumsan@gmail.com>
2026-07-26 23:22:02 +0545

    sse working

--- libs/query/src/lib/aa/phases/phases.sse.ts
+++ libs/query/src/lib/aa/phases/phases.sse.ts
@@ -7,7 +7,7 @@ export function usePhaseSSE(projectId: string) {
   const qc = useQueryClient();
 
   useSSE({
-    url: `${process.env.NEXT_PUBLIC_API_HOST_URL}/v1/events/phases`,
+    url: `${process.env['NEXT_PUBLIC_API_HOST_URL']}/v1/events/phases`,
     onMessage: (event) => {
       // const { event: type, data } = event;
 

48bd4e486e89a4c8bd51bf4b4cca0a1e77ebb4d4
Akash-rumsan <akash.rumsan@gmail.com>
2026-07-26 22:28:15 +0545

    code cleanup

--- libs/query/src/lib/aa/phases/phases.sse.ts
+++ libs/query/src/lib/aa/phases/phases.sse.ts
@@ -1,29 +1,15 @@
 'use client';
 
 import { useQueryClient } from '@tanstack/react-query';
-import { useAuthStore } from '@rumsan/react-query';
-// import { useSSE } from '../../utils/use-sse';
-import { usePhasesStore } from './phases.store';
 import { useSSE } from 'libs/query/src/utils/use-sse';
 
 export function usePhaseSSE(projectId: string) {
   const qc = useQueryClient();
-  const addPhase = usePhasesStore((s) => s.addPhase);
-  const updatePhase = usePhasesStore((s) => s.updatePhase);
 
   useSSE({
     url: `${process.env.NEXT_PUBLIC_API_HOST_URL}/v1/events/phases`,
-    // getToken: () => useAuthStore.getState().token,
     onMessage: (event) => {
-      const { event: type, data } = event;
-      console.log('type:', type);
-      if (type === 'phase.created') {
-        addPhase(data);
-      }
-
-      if (type === 'phase.updated') {
-        updatePhase(data.uuid, data);
-      }
+      // const { event: type, data } = event;
 
       qc.invalidateQueries({ queryKey: ['phases', projectId] });
     },

f8632d259d8f10c4d8d6d508040c4d9edc998ee1
Akash-rumsan <akash.rumsan@gmail.com>
2026-08-26 10:36:43 +0545

    text soze reduced

--- apps/rahat-ui/src/sections/projects/aa-2/communicationLog/table/useCommsLogsTableColumns.tsx
+++ apps/rahat-ui/src/sections/projects/aa-2/communicationLog/table/useCommsLogsTableColumns.tsx
@@ -52,7 +52,7 @@ export default function useCommsLogsTableColumns(transportName: string) {
       header: 'Timestamp',
       cell: ({ row }) => {
         return (
-          <div className="flex  text-xs items-center space-x-2 gap-2">
+          <div className="flex  text-[10px] items-center space-x-2 gap-2">
             <div className="flex flex-col ">
               <span>{dateFormat(row?.original?.createdAt)}</span>
               <span>{dateFormat(row?.original?.updatedAt)}</span>

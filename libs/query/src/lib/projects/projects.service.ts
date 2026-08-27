06d3fc2cd04e012564f64434aa8ed710447d77c8
mohit-rumsan <mohit.rajbhandari@rumsan.net>
2026-08-27 17:05:29 +0545

    Add project image configurable

--- libs/query/src/lib/projects/projects.service.ts
+++ libs/query/src/lib/projects/projects.service.ts
@@ -662,20 +662,20 @@ export const useProjectBeneficiaries = (payload: GetProjectBeneficiaries) => {
         ...query.data,
         data: query.data?.data?.length
           ? query.data.data.map((row: any) => ({
-            ...row,
-            uuid: row?.uuid?.toString(),
-            walletAddress: row?.walletAddress?.toString(),
-            voucherClaimStatus: row?.claimStatus,
-            name: row?.piiData?.name || '',
-            email: row?.piiData?.email || '',
-            gender: row?.projectData?.gender?.toString() || '',
-            phone: row?.piiData?.phone || 'N/A',
-            type: row?.type?.toString() || 'N/A',
-            phoneStatus: row?.projectData?.phoneStatus || '',
-            bankedStatus: row?.projectData?.bankedStatus || '',
-            internetStatus: row?.projectData?.internetStatus || '',
-            benTokens: row?.benTokens || 'N/A',
-          }))
+              ...row,
+              uuid: row?.uuid?.toString(),
+              walletAddress: row?.walletAddress?.toString(),
+              voucherClaimStatus: row?.claimStatus,
+              name: row?.piiData?.name || '',
+              email: row?.piiData?.email || '',
+              gender: row?.projectData?.gender?.toString() || '',
+              phone: row?.piiData?.phone || 'N/A',
+              type: row?.type?.toString() || 'N/A',
+              phoneStatus: row?.projectData?.phoneStatus || '',
+              bankedStatus: row?.projectData?.bankedStatus || '',
+              internetStatus: row?.projectData?.internetStatus || '',
+              benTokens: row?.benTokens || 'N/A',
+            }))
           : [],
       };
     }, [query.data]),
@@ -935,12 +935,57 @@ export const useProjectClose = () => {
         });
       },
       mutationKey: ['projectClose'],
-      mutationFn: async ({ uuid, data }: { uuid: UUID; data: { status: string } }) => {
+      mutationFn: async ({
+        uuid,
+        data,
+      }: {
+        uuid: UUID;
+        data: { status: string };
+      }) => {
         const res = await rumsanService.client.patch(
           `/projects/${uuid}/status`,
           data,
         );
         return res;
+      },
+    },
+    queryClient,
+  );
+};
+
+export const useProjectImageUpdate = () => {
+  const { queryClient } = useRSQuery();
+  const alert = useSwal();
+  const toast = alert.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+  });
+  return useMutation(
+    {
+      onSuccess: () => {
+        toast.fire({
+          title: 'Project image updated successfully',
+          icon: 'success',
+        });
+        queryClient.invalidateQueries({
+          queryKey: [TAGS.GET_PROJECT_DETAILS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [TAGS.GET_ALL_PROJECTS],
+        });
+      },
+      onError: () => {
+        toast.fire({
+          title: 'Error while updating project image.',
+          icon: 'error',
+        });
+      },
+      mutationKey: ['projectImageUpdate'],
+      mutationFn: async ({ uuid, url }: { uuid: UUID; url: string }) => {
+        const res = await api.patch(`projects/${uuid}/image`, { url });
+        return res.data;
       },
     },
     queryClient,
